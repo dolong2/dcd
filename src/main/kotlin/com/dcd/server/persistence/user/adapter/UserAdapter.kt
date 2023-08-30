f5025834645e86283e4b4472c8edb26c5df015ad
jeayoung <k01066624566@gmail.com>
2023-08-30 09:34:58 +0900

    add User Adapter

---
+++ src/main/kotlin/com/dcd/server/persistence/user/adapter/UserAdapter.kt
@@ -0,0 +1,22 @@
+package com.dcd.server.persistence.user.adapter
+
+import com.dcd.server.core.domain.user.model.User
+import com.dcd.server.persistence.user.entity.UserJpaEntity
+
+fun User.toEntity(): UserJpaEntity =
+    UserJpaEntity(
+        id = this.id,
+        email = this.email,
+        name = this.name,
+        password = this.password,
+        roles = this.roles
+    )
+
+fun UserJpaEntity.toDomain(): User =
+    User(
+        id = this.id,
+        email = this.email,
+        name = this.name,
+        password = this.password,
+        roles = this.roles
+    )
