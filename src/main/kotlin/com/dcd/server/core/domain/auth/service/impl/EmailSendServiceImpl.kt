999544438df135b808e05128560a55305cd0d2eb
jeayoung <k01066624566@gmail.com>
2023-09-03 15:51:46 +0900

    add EmailSendService

---
+++ src/main/kotlin/com/dcd/server/core/domain/auth/service/impl/EmailSendServiceImpl.kt
@@ -0,0 +1,26 @@
+package com.dcd.server.core.domain.auth.service.impl
+
+import com.dcd.server.core.domain.auth.model.EmailAuth
+import com.dcd.server.core.domain.auth.service.EmailSendService
+import com.dcd.server.core.domain.auth.spi.CommandEmailAuthPort
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.stereotype.Service
+
+@Service
+class EmailSendServiceImpl(
+    private val commandEmailAuthPort: CommandEmailAuthPort,
+    private val emailSender: JavaMailSender,
+) : EmailSendService{
+    override fun sendEmail(email: String) {
+        val emailAuth = EmailAuth(email = email)
+        val code = emailAuth.code
+        val message = emailSender.createMimeMessage()
+        val messageHelper = MimeMessageHelper(message, "UTF-8")
+        messageHelper.setTo(email)
+        messageHelper.setSubject("[DCD] 메일인증 코드")
+        messageHelper.setText("$code \n위의 코드를 입력해주세요!")
+        emailSender.send(message)
+        commandEmailAuthPort.save(emailAuth)
+    }
+}
