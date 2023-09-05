d10f15a1a6b0b02a375a47c3d94b74a51cb80c7a
jeayoung <k01066624566@gmail.com>
2023-09-05 10:19:12 +0900

    add certificateMail method to AuthWebAdapter

--- src/main/kotlin/com/dcd/server/presentation/domain/auth/data/exetension/EmailAuthRequestExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/auth/data/exetension/EmailAuthRequestExtension.kt
@@ -1,9 +1,17 @@
 package com.dcd.server.presentation.domain.auth.data.exetension
 
+import com.dcd.server.core.domain.auth.dto.request.CertificateMailRequestDto
 import com.dcd.server.core.domain.auth.dto.request.EmailSendRequestDto
+import com.dcd.server.presentation.domain.auth.data.request.CertificateMailRequest
 import com.dcd.server.presentation.domain.auth.data.request.EmailSendRequest
 
 fun EmailSendRequest.toDto(): EmailSendRequestDto =
     EmailSendRequestDto(
         email = this.email
+    )
+
+fun CertificateMailRequest.toDto(): CertificateMailRequestDto =
+    CertificateMailRequestDto(
+        email = this.email,
+        code = this.code
     )
--- src/test/kotlin/com/dcd/server/presentation/domain/auth/AuthWebAdapterTest.kt
+++ src/test/kotlin/com/dcd/server/presentation/domain/auth/AuthWebAdapterTest.kt
@@ -1,7 +1,9 @@
 package com.dcd.server.presentation.domain.auth
 
 import com.dcd.server.core.domain.auth.usecase.AuthMailSendUseCase
+import com.dcd.server.core.domain.auth.usecase.AuthenticateMailUseCase
 import com.dcd.server.core.domain.auth.usecase.SignUpUseCase
+import com.dcd.server.presentation.domain.auth.data.request.CertificateMailRequest
 import com.dcd.server.presentation.domain.auth.data.request.EmailSendRequest
 import com.dcd.server.presentation.domain.auth.data.request.SignUpRequest
 import io.kotest.core.spec.style.BehaviorSpec
@@ -15,7 +17,8 @@ import org.springframework.http.ResponseEntity
 class AuthWebAdapterTest : BehaviorSpec({
     val authMailSendUseCase = mockk<AuthMailSendUseCase>()
     val signUpUseCase = mockk<SignUpUseCase>()
-    val authWebAdapter = AuthWebAdapter(authMailSendUseCase, signUpUseCase)
+    val authenticateMailUseCase = mockk<AuthenticateMailUseCase>()
+    val authWebAdapter = AuthWebAdapter(authMailSendUseCase, signUpUseCase, authenticateMailUseCase)
 
     given("EmailSendRequest가 주어지고") {
         val testEmail = "testEmail"
@@ -42,6 +45,20 @@ class AuthWebAdapterTest : BehaviorSpec({
             then("문제가 없다면 usecase가 실행되어야함") {
                 verify { signUpUseCase.execute(any()) }
                 result shouldBe ResponseEntity(HttpStatus.CREATED)
+            }
+        }
+    }
+
+    given("CertificateMailRequest가 주어지고") {
+        val testEmail = "testEmail"
+        val testCode = "testCode"
+        val request = CertificateMailRequest(testEmail, testCode)
+        `when`("certificateMail 메서드를 실행할때") {
+            every { authenticateMailUseCase.execute(any()) } returns Unit
+            val result = authWebAdapter.certificateEmail(request)
+            then("문제가 없다면 usecase가 실행되어야함") {
+                verify { authenticateMailUseCase.execute(any()) }
+                result shouldBe ResponseEntity.ok().build()
             }
         }
     }
