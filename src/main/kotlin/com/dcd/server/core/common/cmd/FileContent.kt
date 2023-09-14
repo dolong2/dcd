d7a9bdc72b2347fb4de18a1d6abf8f60cedb2ef5
jeayoung <k01066624566@gmail.com>
2023-09-14 14:00:15 +0900

    fix docker file content

--- src/main/kotlin/com/dcd/server/core/common/cmd/FileContent.kt
+++ src/main/kotlin/com/dcd/server/core/common/cmd/FileContent.kt
@@ -4,8 +4,21 @@ object FileContent {
     fun getSpringBootDockerFileContent(name: String, javaVersion: Int): String =
         "FROM openjdk:${javaVersion}-jdk\n" +
         "WORKDIR /usr/src/app\n" +
-        "COPY $name/build/libs/$name-0.0.1-SNAPSHOT.jar $name/build/libs/app-0.0.1-SNAPSHOT.jar\n" +
+        "COPY build/libs/$name-0.0.1-SNAPSHOT.jar build/libs/app-0.0.1-SNAPSHOT.jar\n" +
         "EXPOSE 8080\n" +
         "CMD [\"java\",\"-jar\",\"$name/build/libs/app-0.0.1-SNAPSHOT.jar\"]"
+
+    fun getBuildGradleKtsFileContent(name: String): String =
+        "tasks {\n" +
+        "\tval customBootJarName = \"$name-\$version.jar\"\n" +
+        "\tnamed<org.springframework.boot.gradle.tasks.bundling.BootJar>(\"bootJar\") {\n" +
+        "\t\tarchiveFileName.set(customBootJarName)\n" +
+        "\t}\n" +
+        "}"
+
+    fun getBuildGradleFileContent(name: String): String =
+        "bootJar {\n" +
+        "\tarchiveFileName = '$name.jar'\n" +
+        "}"
 
 }
