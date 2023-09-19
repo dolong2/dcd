a38123e2d945a5dd19d2ad8ce7aa3e5f3dc24d1b
jeayoung <k01066624566@gmail.com>
2023-09-19 10:03:29 +0900

    fix docker image build command

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/BuildDockerImageServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/BuildDockerImageServiceImpl.kt
@@ -17,7 +17,7 @@ class BuildDockerImageServiceImpl(
             ?: throw ApplicationNotFoundException())
         val name = application.name
         commandPort.executeShellCommand("cd ./$name && ./gradlew clean build")
-        commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:v0.0.1 .")
+        commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
     }
 
     override fun buildImageByApplication(application: Application) {
