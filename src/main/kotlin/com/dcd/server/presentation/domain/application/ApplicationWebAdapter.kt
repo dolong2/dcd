e2cee217ce294437de0226d1b3b12782d59536cd
jeayoung <k01066624566@gmail.com>
2023-09-21 17:30:09 +0900

    add RunApplication method to webAdapter

--- src/main/kotlin/com/dcd/server/presentation/domain/application/ApplicationWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/ApplicationWebAdapter.kt
@@ -1,23 +1,29 @@
 package com.dcd.server.presentation.domain.application
 
 import com.dcd.server.core.domain.application.usecase.CreateApplicationUseCase
+import com.dcd.server.core.domain.application.usecase.RunApplicationUseCase
 import com.dcd.server.presentation.domain.application.data.exetension.toDto
 import com.dcd.server.presentation.domain.application.data.request.CreateApplicationRequest
+import com.dcd.server.presentation.domain.application.data.request.RunApplicationRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/application")
 class ApplicationWebAdapter(
-    private val createApplicationUseCase: CreateApplicationUseCase
+    private val createApplicationUseCase: CreateApplicationUseCase,
+    private val runApplicationUseCase: RunApplicationUseCase
 ) {
     @PostMapping
     fun createApplication(@Validated @RequestBody createApplicationRequest: CreateApplicationRequest): ResponseEntity<Void> =
         createApplicationUseCase.execute(createApplicationRequest.toDto())
             .run { ResponseEntity(HttpStatus.CREATED) }
+
+    @PostMapping("/{id}/run")
+    fun runApplication(@PathVariable id: String, @RequestBody runApplicationRequest: RunApplicationRequest): ResponseEntity<Void> =
+        runApplicationUseCase.execute(id, runApplicationRequest.toDto())
+            .run { ResponseEntity.ok().build() }
+
 }
--- src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/CreateApplicationDataExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/CreateApplicationDataExtension.kt
@@ -1,7 +1,9 @@
 package com.dcd.server.presentation.domain.application.data.exetension
 
 import com.dcd.server.core.domain.application.dto.request.CreateApplicationReqDto
+import com.dcd.server.core.domain.application.dto.request.RunApplicationReqDto
 import com.dcd.server.presentation.domain.application.data.request.CreateApplicationRequest
+import com.dcd.server.presentation.domain.application.data.request.RunApplicationRequest
 
 fun CreateApplicationRequest.toDto(): CreateApplicationReqDto =
     CreateApplicationReqDto(
@@ -9,4 +11,12 @@ fun CreateApplicationRequest.toDto(): CreateApplicationReqDto =
         description = this.description,
         githubUrl = this.githubUrl,
         applicationType = this.applicationType
+    )
+
+fun RunApplicationRequest.toDto(): RunApplicationReqDto =
+    RunApplicationReqDto(
+        langVersion = this.langVersion,
+        dbTypes = this.dbTypes,
+        rootPassword = this.rootPassword,
+        dataBaseName = this.dataBaseName
     )
