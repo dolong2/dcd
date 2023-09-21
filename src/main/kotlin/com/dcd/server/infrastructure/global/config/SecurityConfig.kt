633034da244e9e5ece4f48982f18e2299b2290a3
jeayoung <k01066624566@gmail.com>
2023-09-21 17:32:11 +0900

    add SecurityConfig for run application

--- src/main/kotlin/com/dcd/server/infrastructure/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/dcd/server/infrastructure/global/config/SecurityConfig.kt
@@ -50,6 +50,7 @@ class SecurityConfig(
 
                 //application
                 .requestMatchers(HttpMethod.POST, "/application").authenticated()
+                .requestMatchers(HttpMethod.POST, "/application/{id}/run").authenticated()
 
                 //when url not set
                 .anyRequest().denyAll()
