8a4e244e03dc0674b60e1a43f06e51dfa27a5e0d
jeayoung <k01066624566@gmail.com>
2023-09-25 12:03:00 +0900

    renaming RunApplicationUseCase

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/SpringApplicationRunUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/SpringApplicationRunUseCase.kt
@@ -2,7 +2,7 @@ package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.common.spi.CompareUserPort
-import com.dcd.server.core.domain.application.dto.request.RunApplicationReqDto
+import com.dcd.server.core.domain.application.dto.request.SpringApplicationRunReqDto
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.exception.ApplicationRunOptionNotValidException
 import com.dcd.server.core.domain.application.model.Application
@@ -13,7 +13,7 @@ import com.dcd.server.core.domain.user.service.GetCurrentUserService
 import kotlin.RuntimeException
 
 @UseCase
-class RunApplicationUseCase(
+class SpringApplicationRunUseCase(
     private val cloneApplicationByUrlService: CloneApplicationByUrlService,
     private val modifyGradleService: ModifyGradleService,
     private val createDockerFileService: CreateDockerFileService,
@@ -24,32 +24,26 @@ class RunApplicationUseCase(
     private val queryApplicationPort: QueryApplicationPort,
     private val compareUserPort: CompareUserPort
 ) {
-    fun execute(id: String, runApplicationReqDto: RunApplicationReqDto) {
+    fun execute(id: String, runApplicationReqDto: SpringApplicationRunReqDto) {
         val application = (queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException())
         if (compareUserPort.compareTwoUserEntity(currentUserService.getCurrentUser(), application.owner))
             throw RuntimeException()
         cloneApplicationByUrlService.cloneByApplication(application)
         when(application.applicationType){
-            ApplicationType.SPRING_BOOT -> runSpringBootApplication(application, runApplicationReqDto)
+            ApplicationType.SPRING_BOOT -> {
+                modifyGradleService.modifyGradleByApplication(application)
+                val version = runApplicationReqDto.langVersion
+                val dbTypes = runApplicationReqDto.dbTypes
+                val rootPassword = runApplicationReqDto.rootPassword
+                    ?: throw ApplicationRunOptionNotValidException()
+                val dataBaseName = runApplicationReqDto.dataBaseName
+                    ?: throw ApplicationRunOptionNotValidException()
+                createDockerFileService.createFileToApplication(application, version)
+                buildDockerImageService.buildImageByApplication(application)
+                createDockerComposeFileService.createDockerComposeYml(application, dbTypes, rootPassword, dataBaseName)
+                createContainerService.createContainer(application)
+            }
         }
-    }
-
-    private fun runSpringBootApplication(
-        application: Application,
-        runApplicationReqDto: RunApplicationReqDto,
-    ) {
-        modifyGradleService.modifyGradleByApplication(application)
-        val version = runApplicationReqDto.langVersion
-        val dbTypes = runApplicationReqDto.dbTypes
-            ?: throw ApplicationRunOptionNotValidException()
-        val rootPassword = runApplicationReqDto.rootPassword
-            ?: throw ApplicationRunOptionNotValidException()
-        val dataBaseName = runApplicationReqDto.dataBaseName
-            ?: throw ApplicationRunOptionNotValidException()
-        createDockerFileService.createFileToApplication(application, version)
-        buildDockerImageService.buildImageByApplication(application)
-        createDockerComposeFileService.createDockerComposeYml(application, dbTypes, rootPassword, dataBaseName)
-        createContainerService.createContainer(application)
     }
 }
