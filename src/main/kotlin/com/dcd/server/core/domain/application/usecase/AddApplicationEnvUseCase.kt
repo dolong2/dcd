237b6b8e6d9bf054bac3511d92aea6ee84160db4
jeayoung <k01066624566@gmail.com>
2023-09-30 17:30:05 +0900

    add AddApplicationEnvUseCase

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/AddApplicationEnvUseCase.kt
@@ -0,0 +1,23 @@
+package com.dcd.server.core.domain.application.usecase
+
+import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.domain.application.dto.request.AddApplicationEnvReqDto
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.spi.CommandApplicationPort
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+
+@UseCase
+class AddApplicationEnvUseCase(
+    private val queryApplicationPort: QueryApplicationPort,
+    private val commandApplicationPort: CommandApplicationPort,
+) {
+    fun execute(id: String, addApplicationEnvReqDto: AddApplicationEnvReqDto) {
+        val application = (queryApplicationPort.findById(id)
+            ?: throw ApplicationNotFoundException())
+        val envMutable = application.env.toMutableMap()
+        addApplicationEnvReqDto.envList.forEach {
+            envMutable[it.key] = it.value
+        }
+        commandApplicationPort.save(application.copy(env = envMutable))
+    }
+}
