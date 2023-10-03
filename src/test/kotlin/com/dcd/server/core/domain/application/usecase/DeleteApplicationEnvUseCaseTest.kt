196766aa3c24525e03d7ac2062817feb40e8b892
jeayoung <k01066624566@gmail.com>
2023-10-03 13:44:22 +0900

    add DeleteApplicationEnvUseCase test

---
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/DeleteApplicationEnvUseCaseTest.kt
@@ -0,0 +1,62 @@
+package com.dcd.server.core.domain.application.usecase
+
+import com.dcd.server.core.domain.application.exception.ApplicationEnvNotFoundException
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.spi.CommandApplicationPort
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.auth.model.Role
+import com.dcd.server.core.domain.user.model.User
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class DeleteApplicationEnvUseCaseTest : BehaviorSpec({
+    val queryApplicationPort = mockk<QueryApplicationPort>()
+    val commandApplicationPort = mockk<CommandApplicationPort>()
+    val deleteApplicationEnvUseCase = DeleteApplicationEnvUseCase(queryApplicationPort, commandApplicationPort)
+
+    given("애플리케이션 Id와 삭제할 key가 주어지고") {
+        val applicationId = "testId"
+        val key = "testKey"
+        val user =
+            User(email = "email", password = "password", name = "testName", roles = mutableListOf(Role.ROLE_USER))
+        val application = Application(
+            id = applicationId,
+            name = "test",
+            description = "test",
+            applicationType = ApplicationType.SPRING_BOOT,
+            env = mapOf(Pair(key, "testValue")),
+            githubUrl = "testUrl",
+            owner = user
+        )
+        `when`("usecase를 실행할때") {
+            every { queryApplicationPort.findById(applicationId) } returns application
+            every { commandApplicationPort.save(any()) } answers { callOriginal() }
+            deleteApplicationEnvUseCase.execute(applicationId, key)
+            then("commandApplicationPort의 save메서드가 실행되어야함") {
+                verify { commandApplicationPort.save(any()) }
+            }
+        }
+        `when`("해당 환경변수가 없을떄") {
+            every { queryApplicationPort.findById(applicationId) } returns application
+            val notExistsKey = "not exists"
+            then("ApplicationEnvNotFoundException이 발생해야함") {
+                shouldThrow<ApplicationEnvNotFoundException> {
+                    deleteApplicationEnvUseCase.execute(applicationId, notExistsKey)
+                }
+            }
+        }
+        `when`("해당 애플리케이션이 없을때") {
+            every { queryApplicationPort.findById(applicationId) } returns null
+            then("ApplicationNotFoundException이 발생해야함") {
+                shouldThrow<ApplicationNotFoundException> {
+                    deleteApplicationEnvUseCase.execute(applicationId, key)
+                }
+            }
+        }
+    }
+})
