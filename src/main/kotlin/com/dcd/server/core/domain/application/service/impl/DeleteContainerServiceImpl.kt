e1ef60b21492d1fce0175faa6c225d1db0139301
jeayoung <k01066624566@gmail.com>
2023-10-05 15:31:32 +0900

    add DeleteContainerService

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DeleteContainerServiceImpl.kt
@@ -0,0 +1,17 @@
+package com.dcd.server.core.domain.application.service.impl
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.service.DeleteContainerService
+import org.springframework.stereotype.Service
+
+@Service
+class DeleteContainerServiceImpl(
+    private val commandPort: CommandPort
+) : DeleteContainerService {
+    override fun deleteContainer(application: Application) {
+        val name = application.name
+        commandPort.executeShellCommand("docker stop $name && docker rm $name")
+    }
+
+}
