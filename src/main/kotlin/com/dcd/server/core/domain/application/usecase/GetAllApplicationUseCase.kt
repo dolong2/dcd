3fa91272ba301db114eb512937ab960e6f415b4d
jeayoung <k01066624566@gmail.com>
2023-10-06 10:00:24 +0900

    apply ReadOnlyUseCase

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCase.kt
@@ -1,12 +1,13 @@
 package com.dcd.server.core.domain.application.usecase
 
+import com.dcd.server.core.common.annotation.ReadOnlyUseCase
 import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.domain.application.dto.extenstion.toDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationListResponseDto
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import com.dcd.server.core.domain.user.service.GetCurrentUserService
 
-@UseCase
+@ReadOnlyUseCase
 class GetAllApplicationUseCase(
     private val queryApplicationPort: QueryApplicationPort,
     private val getCurrentUserService: GetCurrentUserService
