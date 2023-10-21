61eeba30e3eef094093920882aafb8ac9685f388
jeayoung <k01066624566@gmail.com>
2023-10-21 15:21:04 +0900

    add DeleteApplicationUseCaseTest

---
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/DeleteApplicationUseCaseTest.kt
@@ -0,0 +1,63 @@
+package com.dcd.server.core.domain.application.usecase
+
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.spi.CommandApplicationPort
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.auth.model.Role
+import com.dcd.server.core.domain.user.model.User
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.lang.RuntimeException
+
+class DeleteApplicationUseCaseTest : BehaviorSpec({
+    val getCurrentUserService = mockk<GetCurrentUserService>()
+    val commandApplicationPort = mockk<CommandApplicationPort>()
+    val queryApplicationPort = mockk<QueryApplicationPort>()
+    val deleteApplicationUseCase =
+        DeleteApplicationUseCase(getCurrentUserService, commandApplicationPort, queryApplicationPort)
+    given("애플리케이션 id가 주어지고") {
+        val applicationId = "testId"
+        val user =
+            User(email = "email", password = "password", name = "testName", roles = mutableListOf(Role.ROLE_USER))
+        val application = Application(
+            id = applicationId,
+            name = "test",
+            description = "test",
+            applicationType = ApplicationType.SPRING_BOOT,
+            env = mapOf(),
+            githubUrl = "testUrl",
+            owner = user
+        )
+        every { getCurrentUserService.getCurrentUser() } returns user
+        `when`("usecase를 실행할때") {
+            every { commandApplicationPort.delete(application) } returns Unit
+            every { queryApplicationPort.findById(applicationId) } returns application
+            deleteApplicationUseCase.execute(applicationId)
+            then("commandApplicationPort의 delete메서드가 실행되어야함") {
+                verify { commandApplicationPort.delete(application) }
+            }
+        }
+        `when`("application을 찾을 수 없을때") {
+            every { queryApplicationPort.findById(applicationId) } returns null
+            then("applicationNotFoundException이 발생해야함") {
+                shouldThrow<ApplicationNotFoundException> {
+                    deleteApplicationUseCase.execute(applicationId)
+                }
+            }
+        }
+        `when`("현재 유저가 소유자가 아닐때") {
+            every { getCurrentUserService.getCurrentUser() } returns user.copy(id = "otherUser")
+            then("RuntimeException이 발생해야함") {
+                shouldThrow<RuntimeException> {
+                    deleteApplicationUseCase.execute(applicationId)
+                }
+            }
+        }
+    }
+})
