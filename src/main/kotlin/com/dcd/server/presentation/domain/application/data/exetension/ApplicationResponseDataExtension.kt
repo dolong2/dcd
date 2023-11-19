3f2915c1230dc6130b83b0d91da2739cf83166c6
dolong2 <k01066624566@gmail.com>
2023-11-19 11:20:11 +0900

    진행사항 병합

---
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/ApplicationResponseDataExtension.kt
@@ -0,0 +1,21 @@
+package com.dcd.server.presentation.domain.application.data.exetension
+
+import com.dcd.server.core.domain.application.dto.response.ApplicationListResponseDto
+import com.dcd.server.core.domain.application.dto.response.ApplicationResponseDto
+import com.dcd.server.presentation.domain.application.data.response.ApplicationListResponse
+import com.dcd.server.presentation.domain.application.data.response.ApplicationResponse
+
+fun ApplicationResponseDto.toResponse(): ApplicationResponse =
+    ApplicationResponse(
+        id = this.id,
+        name = this.name,
+        description = this.description,
+        githubUrl = this.githubUrl,
+        applicationType = this.applicationType,
+        env = this.env
+    )
+
+fun ApplicationListResponseDto.toResponse(): ApplicationListResponse =
+    ApplicationListResponse(
+        list = this.list.map { it.toResponse() }
+    )
