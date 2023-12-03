48e1ddd1df7a840ca57a7bde9ea8485b1efbc33f
dolong2 <k01066624566@gmail.com>
2023-12-03 18:08:06 +0900

    add CreateWorkspaceUseCase

---
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/CreateWorkspaceUseCase.kt
@@ -0,0 +1,22 @@
+package com.dcd.server.core.domain.workspace.usecase
+
+import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.workspace.dto.extension.toEntity
+import com.dcd.server.core.domain.workspace.dto.request.CreateWorkspaceReqDto
+import com.dcd.server.core.domain.workspace.service.CreateNetworkService
+import com.dcd.server.core.domain.workspace.spi.CommandWorkspacePort
+
+@UseCase
+class CreateWorkspaceUseCase(
+    private val commandWorkspacePort: CommandWorkspacePort,
+    private val currentUserService: GetCurrentUserService,
+    private val createNetworkService: CreateNetworkService
+) {
+    fun execute(createWorkspaceReqDto: CreateWorkspaceReqDto) {
+        val currentUser = currentUserService.getCurrentUser()
+        commandWorkspacePort.save(createWorkspaceReqDto.toEntity(currentUser))
+        createNetworkService.createNetwork(createWorkspaceReqDto.title)
+    }
+}
