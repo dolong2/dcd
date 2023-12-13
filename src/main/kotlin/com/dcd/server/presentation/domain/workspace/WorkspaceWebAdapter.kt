4d7b66961673557f3778c34c9d83044a776f5c21
dolong2 <k01066624566@gmail.com>
2023-12-13 16:27:57 +0900

    add get method to WorkspaceWebAdapter

--- src/main/kotlin/com/dcd/server/presentation/domain/workspace/WorkspaceWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/workspace/WorkspaceWebAdapter.kt
@@ -1,21 +1,35 @@
 package com.dcd.server.presentation.domain.workspace
 
 import com.dcd.server.core.domain.workspace.usecase.CreateWorkspaceUseCase
+import com.dcd.server.core.domain.workspace.usecase.GetAllWorkspaceUseCase
+import com.dcd.server.core.domain.workspace.usecase.GetWorkspaceUseCase
 import com.dcd.server.presentation.domain.workspace.data.exetension.toDto
+import com.dcd.server.presentation.domain.workspace.data.exetension.toResponse
 import com.dcd.server.presentation.domain.workspace.data.request.CreateWorkspaceRequest
+import com.dcd.server.presentation.domain.workspace.data.response.WorkspaceListResponse
+import com.dcd.server.presentation.domain.workspace.data.response.WorkspaceResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/workspace")
 class WorkspaceWebAdapter(
-    private val createWorkspaceUseCase: CreateWorkspaceUseCase
+    private val createWorkspaceUseCase: CreateWorkspaceUseCase,
+    private val getAllWorkspaceUseCase: GetAllWorkspaceUseCase,
+    private val getWorkspaceUseCase: GetWorkspaceUseCase
 ) {
     @PostMapping
     fun createWorkspace(@RequestBody createWorkspaceRequest: CreateWorkspaceRequest): ResponseEntity<Void> =
         createWorkspaceUseCase.execute(createWorkspaceRequest.toDto())
             .run { ResponseEntity.ok().build() }
+
+    @GetMapping
+    fun getAllWorkspace(): ResponseEntity<WorkspaceListResponse> =
+        getAllWorkspaceUseCase.execute()
+            .let { ResponseEntity.ok(it.toResponse()) }
+
+    @GetMapping("/{workspaceId}")
+    fun getOneWorkspace(@PathVariable workspaceId: String): ResponseEntity<WorkspaceResponse> =
+        getWorkspaceUseCase.execute(workspaceId)
+            .let { ResponseEntity.ok(it.toResponse()) }
 }
