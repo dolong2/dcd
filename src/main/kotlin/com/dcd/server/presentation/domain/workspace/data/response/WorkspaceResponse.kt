6bc963c55074e8a34baf8a019944f15d28c3fa62
dolong2 <k01066624566@gmail.com>
2023-12-13 16:27:44 +0900

    add WorkspaceResponse

---
+++ src/main/kotlin/com/dcd/server/presentation/domain/workspace/data/response/WorkspaceResponse.kt
@@ -0,0 +1,12 @@
+package com.dcd.server.presentation.domain.workspace.data.response
+
+import com.dcd.server.presentation.domain.application.data.response.ApplicationResponse
+import com.dcd.server.presentation.domain.user.data.response.UserResponse
+
+data class WorkspaceResponse(
+    val id: String,
+    val title: String,
+    val description: String,
+    val applicationList: List<ApplicationResponse>,
+    val owner: UserResponse
+)
