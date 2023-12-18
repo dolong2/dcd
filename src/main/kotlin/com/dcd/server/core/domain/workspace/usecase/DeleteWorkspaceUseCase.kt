b20f1dbfb3565da4221478633bb408c51ad9490b
dolong2 <k01066624566@gmail.com>
2023-12-18 13:47:37 +0900

    [refac] DeleteWorkspaceUseCase에서 ValidateWorkspaceOwnerService를 사용하도록 변경

--- src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/DeleteWorkspaceUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/DeleteWorkspaceUseCase.kt
@@ -4,6 +4,7 @@ import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.domain.user.service.GetCurrentUserService
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
+import com.dcd.server.core.domain.workspace.service.ValidateWorkspaceOwnerService
 import com.dcd.server.core.domain.workspace.spi.CommandWorkspacePort
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 
@@ -11,14 +12,14 @@ import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 class DeleteWorkspaceUseCase(
     private val commandWorkspacePort: CommandWorkspacePort,
     private val queryWorkspacePort: QueryWorkspacePort,
-    private val getCurrentUserService: GetCurrentUserService
+    private val getCurrentUserService: GetCurrentUserService,
+    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService
 ) {
     fun execute(workspaceId: String) {
         val workspace = (queryWorkspacePort.findById(workspaceId)
             ?: throw WorkspaceNotFoundException())
         val currentUser = getCurrentUserService.getCurrentUser()
-        if (currentUser.equals(workspace.owner).not())
-            throw WorkspaceOwnerNotSameException()
+        validateWorkspaceOwnerService.validateOwner(currentUser, workspace)
         commandWorkspacePort.delete(workspace)
     }
 }
