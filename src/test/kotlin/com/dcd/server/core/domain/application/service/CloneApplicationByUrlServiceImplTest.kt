ae65a3deb96c3fdd30a56f00856c4fcc47412f55
dolong2 <k01066624566@gmail.com>
2023-12-23 10:50:59 +0900

    [add] CloneApplicationByUrlService 테스트

---
+++ src/test/kotlin/com/dcd/server/core/domain/application/service/CloneApplicationByUrlServiceImplTest.kt
@@ -0,0 +1,61 @@
+package com.dcd.server.core.domain.application.service
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.service.impl.CloneApplicationByUrlServiceImpl
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.auth.model.Role
+import com.dcd.server.core.domain.user.model.User
+import com.dcd.server.core.domain.workspace.model.Workspace
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.util.*
+
+class CloneApplicationByUrlServiceImplTest : BehaviorSpec({
+    val commandPort = mockk<CommandPort>(relaxUnitFun = true)
+    val queryApplicationPort = mockk<QueryApplicationPort>()
+    val service = CloneApplicationByUrlServiceImpl(queryApplicationPort, commandPort)
+
+    val user =
+        User(email = "email", password = "password", name = "testName", roles = mutableListOf(Role.ROLE_USER))
+    given("애플리케이션id가 주어지고") {
+        val appId = UUID.randomUUID().toString()
+
+        `when`("cloneById 메서드를 실행할때") {
+            val workspace = Workspace(
+                UUID.randomUUID().toString(),
+                title = "test workspace",
+                description = "test workspace description",
+                owner = user
+            )
+            val application = Application(appId, "testName", null, ApplicationType.SPRING_BOOT, "testUrl", mapOf(), workspace, port = 8080)
+            every { queryApplicationPort.findById(appId) } returns application
+
+            service.cloneById(appId)
+            then("commandPort가 실행되어야함") {
+                verify { commandPort.executeShellCommand("git clone ${application.githubUrl} ${application.name}") }
+            }
+        }
+    }
+
+    given("애플리케이션이 주이지고") {
+        val workspace = Workspace(
+            UUID.randomUUID().toString(),
+            title = "test workspace",
+            description = "test workspace description",
+            owner = user
+        )
+        val application = Application(UUID.randomUUID().toString(), "testName", null, ApplicationType.SPRING_BOOT, "testUrl", mapOf(), workspace, port = 8080)
+
+        `when`("cloneByApplication 메서드를 실행할때") {
+            service.cloneByApplication(application)
+
+            then("commandPort가 실행되어야함") {
+                verify { commandPort.executeShellCommand("git clone ${application.githubUrl} ${application.name}") }
+            }
+        }
+    }
+})
