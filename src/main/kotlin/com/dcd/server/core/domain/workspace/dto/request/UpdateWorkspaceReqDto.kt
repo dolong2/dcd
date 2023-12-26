396cb375c39e1b8df4561b8a088a7c97f3c84f74
dolong2 <k01066624566@gmail.com>
2023-12-26 16:19:35 +0900

    [add] UpdateWorkspaceUseCase

---
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/dto/request/UpdateWorkspaceReqDto.kt
@@ -0,0 +1,6 @@
+package com.dcd.server.core.domain.workspace.dto.request
+
+data class UpdateWorkspaceReqDto(
+    val title: String,
+    val description: String,
+)
---
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/UpdateWorkspaceUseCase.kt
@@ -0,0 +1,28 @@
+package com.dcd.server.core.domain.workspace.usecase
+
+import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.workspace.dto.request.UpdateWorkspaceReqDto
+import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
+import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
+import com.dcd.server.core.domain.workspace.spi.CommandWorkspacePort
+import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
+
+@UseCase
+class UpdateWorkspaceUseCase(
+    private val commandWorkspacePort: CommandWorkspacePort,
+    private val queryWorkspacePort: QueryWorkspacePort,
+    private val getCurrentUserService: GetCurrentUserService
+) {
+    fun execute(workspaceId: String, updateWorkspaceReqDto: UpdateWorkspaceReqDto) {
+        val workspace = (queryWorkspacePort.findById(workspaceId)
+            ?: throw WorkspaceNotFoundException())
+        val currentUser = getCurrentUserService.getCurrentUser()
+
+        if (workspace.owner.equals(currentUser).not())
+            throw WorkspaceOwnerNotSameException()
+
+        val updatedWorkspace = workspace.copy(title = updateWorkspaceReqDto.title, description = updateWorkspaceReqDto.description)
+        commandWorkspacePort.save(updatedWorkspace)
+    }
+}
