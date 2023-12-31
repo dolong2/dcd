3464aa5b24f2a2bb27ab918fc73c08958697ecbd
dolong2 <k01066624566@gmail.com>
2023-12-31 15:30:10 +0900

    [add] 워크스페이스 주인이 아닐때 실행하는 테스트 케이스 추가

--- src/test/kotlin/com/dcd/server/core/domain/application/usecase/GetOneApplicationUseCaseTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/GetOneApplicationUseCaseTest.kt
@@ -8,6 +8,7 @@ import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import com.dcd.server.core.domain.auth.model.Role
 import com.dcd.server.core.domain.user.model.User
 import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
 import com.dcd.server.core.domain.workspace.model.Workspace
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
@@ -47,6 +48,19 @@ class GetOneApplicationUseCaseTest : BehaviorSpec({
 
             then("result는 application의 내용이랑 같아야함") {
                 shouldThrow<ApplicationNotFoundException> {
+                    getOneApplicationUseCase.execute(application.id)
+                }
+            }
+        }
+        `when`("현재 유저가 해당 애플리케이션의 워크스페이스 주인이 아닐때") {
+            val another =
+                User(email = "another", password = "password", name = "another", roles = mutableListOf(Role.ROLE_USER))
+
+            every { queryApplicationPort.findById(application.id) } returns application
+            every { getCurrentUserService.getCurrentUser() } returns another
+
+            then("WorkspaceOwnerNotSameException이 발생해야함") {
+                shouldThrow<WorkspaceOwnerNotSameException> {
                     getOneApplicationUseCase.execute(application.id)
                 }
             }
