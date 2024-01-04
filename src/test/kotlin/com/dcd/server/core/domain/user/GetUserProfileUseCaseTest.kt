3b181e08326254da704a5c729d95dc793efde22b
dolong2 <k01066624566@gmail.com>
2024-01-04 11:11:35 +0900

    [add] 프로필 조회시 유저, 워크스페이스, 애플리케이션이 주어진 테스트 케이스 추가

---
+++ src/test/kotlin/com/dcd/server/core/domain/user/GetUserProfileUseCaseTest.kt
@@ -0,0 +1,67 @@
+package com.dcd.server.core.domain.user
+
+import com.dcd.server.core.domain.application.dto.extenstion.toProfileDto
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.auth.model.Role
+import com.dcd.server.core.domain.user.dto.extension.toDto
+import com.dcd.server.core.domain.user.model.User
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.user.usecase.GetUserProfileUseCase
+import com.dcd.server.core.domain.workspace.dto.extension.toProfileDto
+import com.dcd.server.core.domain.workspace.model.Workspace
+import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.util.*
+
+class GetUserProfileUseCaseTest : BehaviorSpec({
+    val getCurrentUserService = mockk<GetCurrentUserService>()
+    val queryWorkspacePort = mockk<QueryWorkspacePort>()
+    val queryApplicationPort = mockk<QueryApplicationPort>()
+
+    val getUserProfileUseCase = GetUserProfileUseCase(getCurrentUserService, queryWorkspacePort, queryApplicationPort)
+
+    given("유저, 애플리케이션, 워크스페이스가 주어지고") {
+        val user =
+            User(email = "email", password = "password", name = "testName", roles = mutableListOf(Role.ROLE_USER))
+        val workspace = Workspace(
+            UUID.randomUUID().toString(),
+            title = "test workspace",
+            description = "test workspace description",
+            owner = user
+        )
+        val application = Application(
+            id = "testId",
+            name = "test",
+            description = "test",
+            applicationType = ApplicationType.SPRING_BOOT,
+            env = mapOf(),
+            githubUrl = "testUrl",
+            workspace = workspace,
+            port = 8080
+        )
+
+        `when`("usecase를 실행할때") {
+            every { getCurrentUserService.getCurrentUser() } returns user
+            every { queryWorkspacePort.findByUser(user) } returns listOf(workspace)
+            every { queryApplicationPort.findAllByWorkspace(workspace) } returns listOf(application)
+
+            val result = getUserProfileUseCase.execute()
+
+            then("워크스페이스와 애플리케이션을 조회해야함") {
+                verify { queryApplicationPort.findAllByWorkspace(workspace) }
+                verify { queryWorkspacePort.findByUser(user) }
+            }
+
+            then("결과값은 user, application, workspace의 값을 담고있어야함") {
+                result.user shouldBe user.toDto()
+                result.workspaces shouldBe listOf(workspace.toProfileDto(listOf(application.toProfileDto())))
+            }
+        }
+    }
+})
