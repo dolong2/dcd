0034e4109a3fdf1e6766fc258e4d734ab1506f9e
dolong2 <k01066624566@gmail.com>
2024-01-06 21:14:55 +0900

    [add] ChangePasswordUseCase pointcut 추가

--- src/main/kotlin/com/dcd/server/core/common/aop/EmailCertificateAspect.kt
+++ src/main/kotlin/com/dcd/server/core/common/aop/EmailCertificateAspect.kt
@@ -1,9 +1,11 @@
 package com.dcd.server.core.common.aop
 
 import com.dcd.server.core.common.aop.exception.NotCertificateEmailException
+import com.dcd.server.core.domain.auth.dto.request.PasswordChangeReqDto
 import com.dcd.server.core.domain.auth.dto.request.SignUpReqDto
 import com.dcd.server.core.domain.auth.spi.CommandEmailAuthPort
 import com.dcd.server.core.domain.auth.spi.QueryEmailAuthPort
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
@@ -13,15 +15,29 @@ import org.springframework.stereotype.Component
 @Component
 class EmailCertificateAspect(
     private val queryEmailAuthPort: QueryEmailAuthPort,
-    private val commandEmailAuthPort: CommandEmailAuthPort
+    private val commandEmailAuthPort: CommandEmailAuthPort,
+    private val getCurrentUserService: GetCurrentUserService
 ) {
     @Pointcut("execution(* com.dcd.server.core.domain.auth.usecase.SignUpUseCase.execute(..)) " + "&& args(signUpReqDto)")
     fun signupUseCasePointcut(signUpReqDto: SignUpReqDto) {
     }
 
+    @Pointcut("execution(* com.dcd.server.core.domain.auth.usecase.ChangePasswordUseCase.execute(..))" + "&& args(passwordChangeReqDto)")
+    fun changePasswordUseCasePointcut(passwordChangeReqDto: PasswordChangeReqDto) {}
+
     @Before("signupUseCasePointcut(signUpReqDto)")
     private fun checkEmailCertificate(signUpReqDto: SignUpReqDto) {
         val emailAuthList = queryEmailAuthPort.findByEmail(signUpReqDto.email)
+            .filter { it.certificate }
+        if (emailAuthList.isEmpty())
+            throw NotCertificateEmailException()
+        commandEmailAuthPort.deleteByCode(emailAuthList[0].code)
+    }
+
+    @Before("changePasswordUseCasePointcut(passwordChangeReqDto)")
+    private fun checkEmailCertificate() {
+        val user = getCurrentUserService.getCurrentUser()
+        val emailAuthList = queryEmailAuthPort.findByEmail(user.email)
             .filter { it.certificate }
         if (emailAuthList.isEmpty())
             throw NotCertificateEmailException()
