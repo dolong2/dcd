f71819bfe47a2798deb5dc48bcdb47cf2cf784c1
dolong2 <k01066624566@gmail.com>
2024-01-11 16:25:46 +0900

    [add] MYSQL 애플리케이션 타입 추가

--- src/main/kotlin/com/dcd/server/core/common/error/ErrorCode.kt
+++ src/main/kotlin/com/dcd/server/core/common/error/ErrorCode.kt
@@ -9,6 +9,7 @@ enum class ErrorCode(
     PASSWORD_NOT_CORRECT("패스워드가 옳바르지 않음", 400),
     ALREADY_USER_EXIST("이미 해당 유저가 존재함", 400),
     APPLICATION_OPTION_NOT_VALID("애플리케이션 옵션이 유효하지 않음", 400),
+    NOT_SUPPORTED_APPLICATION_TYPE("지원되는 애플리케이션 타입이 아님", 400),
 
     UNAUTHORIZED("권한이 없음", 401),
     EXPIRED_TOKEN("토큰이 만료됨", 401),
