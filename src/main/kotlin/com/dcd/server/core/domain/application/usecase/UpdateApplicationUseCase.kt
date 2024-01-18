11647bb31ff2d48bb7c0791f01e78503e7ce676f
dolong2 <k01066624566@gmail.com>
2024-01-18 18:03:44 +0900

    [add] UpdateApplicationUseCase 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/UpdateApplicationUseCase.kt
@@ -0,0 +1,28 @@
+package com.dcd.server.core.domain.application.usecase
+
+import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.domain.application.dto.request.UpdateApplicationReqDto
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.spi.CommandApplicationPort
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+
+@UseCase
+class UpdateApplicationUseCase(
+    private val queryApplicationPort: QueryApplicationPort,
+    private val commandApplicationPort: CommandApplicationPort,
+    private val getCurrentUserService: GetCurrentUserService
+) {
+    fun execute(id: String, updateApplicationReqDto: UpdateApplicationReqDto) {
+        val application = (queryApplicationPort.findById(id)
+            ?: throw ApplicationNotFoundException())
+        val owner = application.workspace.owner
+
+        if (owner.equals(getCurrentUserService.getCurrentUser()).not())
+            throw RuntimeException()
+
+        val updatedApplication =
+            application.copy(name = updateApplicationReqDto.name, description = updateApplicationReqDto.description)
+        commandApplicationPort.save(updatedApplication)
+    }
+}
