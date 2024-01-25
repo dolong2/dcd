9341cb80575d32b09a67af5a4bffaa2f091e079e
dolong2 <k01066624566@gmail.com>
2024-01-25 16:40:22 +0900

    [add] 이용가능 버전 조회 엔드포인트

--- src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/ApplicationResponseDataExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/ApplicationResponseDataExtension.kt
@@ -3,9 +3,11 @@ package com.dcd.server.presentation.domain.application.data.exetension
 import com.dcd.server.core.domain.application.dto.response.ApplicationListResDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationProfileResDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationResDto
+import com.dcd.server.core.domain.application.dto.response.AvailableVersionResDto
 import com.dcd.server.presentation.domain.application.data.response.ApplicationListResponse
 import com.dcd.server.presentation.domain.application.data.response.ApplicationProfileResponse
 import com.dcd.server.presentation.domain.application.data.response.ApplicationResponse
+import com.dcd.server.presentation.domain.application.data.response.AvailableVersionResponse
 
 fun ApplicationResDto.toResponse(): ApplicationResponse =
     ApplicationResponse(
@@ -27,4 +29,9 @@ fun ApplicationProfileResDto.toResponse(): ApplicationProfileResponse =
         id = this.id,
         name = this.name,
         description = this.description
+    )
+
+fun AvailableVersionResDto.toResponse(): AvailableVersionResponse =
+    AvailableVersionResponse(
+        version = this.version
     )
---
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/data/response/AvailableVersionResponse.kt
@@ -0,0 +1,5 @@
+package com.dcd.server.presentation.domain.application.data.response
+
+data class AvailableVersionResponse(
+    val version: List<String>
+)
