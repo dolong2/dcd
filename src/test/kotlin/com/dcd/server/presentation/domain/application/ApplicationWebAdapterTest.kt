6be1a74ed00a54ad531c154326bc1075e8e51ded
dolong2 <k01066624566@gmail.com>
2024-01-26 15:47:45 +0900

    [add] ApplicationWebAdapter의 getAvailableVersion 메서드 테스트 코드

--- src/test/kotlin/com/dcd/server/presentation/domain/application/ApplicationWebAdapterTest.kt
+++ src/test/kotlin/com/dcd/server/presentation/domain/application/ApplicationWebAdapterTest.kt
@@ -3,12 +3,14 @@ package com.dcd.server.presentation.domain.application
 import com.dcd.server.core.domain.application.dto.request.UpdateApplicationReqDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationListResDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationResDto
+import com.dcd.server.core.domain.application.dto.response.AvailableVersionResDto
 import com.dcd.server.core.domain.application.model.enums.ApplicationType
 import com.dcd.server.core.domain.application.usecase.*
 import com.dcd.server.presentation.domain.application.data.exetension.toResponse
 import com.dcd.server.presentation.domain.application.data.request.AddApplicationEnvRequest
 import com.dcd.server.presentation.domain.application.data.request.CreateApplicationRequest
 import com.dcd.server.presentation.domain.application.data.request.UpdateApplicationRequest
+import com.dcd.server.presentation.domain.application.data.response.AvailableVersionResponse
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -162,6 +164,23 @@ class ApplicationWebAdapterTest : BehaviorSpec({
             }
             then("updateApplicationUseCase를 실행해야함") {
                 verify { updateApplicationUseCase.execute(testId, any() as UpdateApplicationReqDto) }
+            }
+        }
+    }
+
+    given("애플리케이션 타입이 주어지고") {
+        val applicationType = ApplicationType.SPRING_BOOT
+
+        `when`("getAvailableVersion 메서드를 실행할때") {
+            val availableVersionResDto = AvailableVersionResDto(version = listOf("11", "17"))
+            every { getAvailableVersionUseCase.execute(applicationType) } returns availableVersionResDto
+
+            val result = applicationWebAdapter.getAvailableVersion(applicationType)
+            then("result의 바디는 availableVersionResDto랑 같아야함") {
+                result.body?.version shouldBe availableVersionResDto.version
+            }
+            then("result는 200 상태코드를 가져야함") {
+                result.statusCode shouldBe HttpStatus.OK
             }
         }
     }
