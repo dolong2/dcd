b08a73c90ea9a6a50819cecb4528cda496734da2
dolong2 <k01066624566@gmail.com>
2024-02-03 18:06:14 +0900

    [fix] CreateApplicationRequest 객체의 url 필드를 nullable로 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/dto/request/CreateApplicationReqDto.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/dto/request/CreateApplicationReqDto.kt
@@ -5,7 +5,7 @@ import com.dcd.server.core.domain.application.model.enums.ApplicationType
 data class CreateApplicationReqDto(
     val name: String,
     val description: String?,
-    val githubUrl: String,
+    val githubUrl: String?,
     val env: Map<String, String>,
     val applicationType: ApplicationType,
     val port: Int,
