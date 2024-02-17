0d37a6d16ae63f9f68312522ff479b1118921c76
dolong2 <k01066624566@gmail.com>
2024-02-17 14:45:04 +0900

    [add] UpdateApplicationReqDto에 applicationType, githubUrl, port 필드 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/dto/request/UpdateApplicationReqDto.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/dto/request/UpdateApplicationReqDto.kt
@@ -1,7 +1,12 @@
 package com.dcd.server.core.domain.application.dto.request
 
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
+
 data class UpdateApplicationReqDto(
     val name: String,
     val description: String?,
-    val version: String
+    val applicationType: ApplicationType,
+    val githubUrl: String?,
+    val version: String,
+    val port: Int
 )
