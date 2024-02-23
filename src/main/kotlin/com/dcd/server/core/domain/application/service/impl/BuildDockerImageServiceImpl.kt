f80b2ca7c6ef40ebdd44aebd4bca4b10f03cc13f
dolong2 <k01066624566@gmail.com>
2024-02-23 21:51:37 +0900

    [add] BuildDockerImageService에서 ExistsValue가 0이 아니면 에러 발생

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/BuildDockerImageServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/BuildDockerImageServiceImpl.kt
@@ -3,6 +3,7 @@ package com.dcd.server.core.domain.application.service.impl
 import com.dcd.server.core.common.command.CommandPort
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
 import com.dcd.server.core.domain.application.service.BuildDockerImageService
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import org.springframework.stereotype.Service
@@ -16,14 +17,31 @@ class BuildDockerImageServiceImpl(
         val application = (queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException())
         val name = application.name
-        commandPort.executeShellCommand("cd ./$name && ./gradlew clean build")
-        commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
+        val exitValue = when (application.applicationType) {
+            ApplicationType.SPRING_BOOT -> {
+                commandPort.executeShellCommand("cd ./$name && ./gradlew clean build")
+                commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
+            }
+
+            else -> {
+                commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
+            }
+        }
+        if (exitValue != 0) throw RuntimeException()
     }
 
     override fun buildImageByApplication(application: Application) {
         val name = application.name
-        commandPort.executeShellCommand("cd ./$name && ./gradlew clean build")
-        commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
+        val exitValue = when(application.applicationType) {
+            ApplicationType.SPRING_BOOT -> {
+                commandPort.executeShellCommand("cd ./$name && ./gradlew clean build")
+                commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
+            }
+            else -> {
+                commandPort.executeShellCommand("cd ./$name && docker build -t ${name.lowercase()}:latest .")
+            }
+        }
+        if (exitValue != 0) throw RuntimeException()
     }
 
 }
