239924d549bf646f240a605eaec1948fcba8ec55
dolong2 <k01066624566@gmail.com>
2024-02-23 20:56:00 +0900

    [del] DockerRunService에서 ExistsPortService 제거

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DockerRunServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DockerRunServiceImpl.kt
@@ -6,14 +6,12 @@ import com.dcd.server.core.domain.application.exception.ApplicationNotFoundExcep
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.model.enums.ApplicationType
 import com.dcd.server.core.domain.application.service.DockerRunService
-import com.dcd.server.core.domain.application.service.ExistsPortService
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import org.springframework.stereotype.Service
 
 @Service
 class DockerRunServiceImpl(
     private val queryApplicationPort: QueryApplicationPort,
-    private val existsPortService: ExistsPortService,
     private val commandPort: CommandPort
 ) : DockerRunService {
     override fun runApplication(id: String, externalPort: Int) {
