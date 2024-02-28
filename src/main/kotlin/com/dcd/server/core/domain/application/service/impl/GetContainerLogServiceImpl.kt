0c0303e5febd915b90b999f3b23f0baf86e9864b
dolong2 <k01066624566@gmail.com>
2024-02-28 17:30:09 +0900

    [add] GetContainerLogService

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/GetContainerLogServiceImpl.kt
@@ -0,0 +1,20 @@
+package com.dcd.server.core.domain.application.service.impl
+
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.service.GetContainerLogService
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+@Service
+class GetContainerLogServiceImpl : GetContainerLogService {
+    override fun getLogs(application: Application): List<String> {
+        val cmd = arrayOf("/bin/sh", "-c", "docker logs ${application.name}")
+        val p = Runtime.getRuntime().exec(cmd)
+        val br = BufferedReader(InputStreamReader(p.inputStream))
+        val result = br.readLines()
+        p.waitFor()
+        p.destroy()
+        return result
+    }
+}
