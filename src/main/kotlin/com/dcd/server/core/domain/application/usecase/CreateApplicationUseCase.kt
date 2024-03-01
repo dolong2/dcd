3db94b1cc920ca13dc33e44cba06bd1b05ed8048
dolong2 <k01066624566@gmail.com>
2024-03-01 16:50:39 +0900

    [fix] 애플리케이션을 생성할때 미리 컨테이너를 생성하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/CreateApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/CreateApplicationUseCase.kt
@@ -3,6 +3,8 @@ package com.dcd.server.core.domain.application.usecase
 import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.domain.application.dto.extenstion.toEntity
 import com.dcd.server.core.domain.application.dto.request.CreateApplicationReqDto
+import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.service.*
 import com.dcd.server.core.domain.application.spi.CommandApplicationPort
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 import com.dcd.server.core.domain.workspace.service.ValidateWorkspaceOwnerService
@@ -12,7 +14,13 @@ import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 class CreateApplicationUseCase(
     private val commandApplicationPort: CommandApplicationPort,
     private val queryWorkspacePort: QueryWorkspacePort,
-    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService
+    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService,
+    private val cloneApplicationByUrlService: CloneApplicationByUrlService,
+    private val modifyGradleService: ModifyGradleService,
+    private val createDockerFileService: CreateDockerFileService,
+    private val getExternalPortService: GetExternalPortService,
+    private val buildDockerImageService: BuildDockerImageService,
+    private val createContainerService: CreateContainerService
 ) {
     fun execute(workspaceId: String, createApplicationReqDto: CreateApplicationReqDto) {
         val workspace = queryWorkspacePort.findById(workspaceId)
@@ -20,5 +28,21 @@ class CreateApplicationUseCase(
         validateWorkspaceOwnerService.validateOwner(workspace)
         val application = createApplicationReqDto.toEntity(workspace)
         commandApplicationPort.save(application)
+
+        val version = application.version
+        val externalPort = getExternalPortService.getExternalPort(application.port)
+        when(application.applicationType){
+            ApplicationType.SPRING_BOOT -> {
+                cloneApplicationByUrlService.cloneByApplication(application)
+                modifyGradleService.modifyGradleByApplication(application)
+                createDockerFileService.createFileToApplication(application, version, externalPort)
+                buildDockerImageService.buildImageByApplication(application)
+                createContainerService.createContainer(application, externalPort)
+            }
+
+            else -> {
+                createContainerService.createContainer(application, version, externalPort)
+            }
+        }
     }
 }
