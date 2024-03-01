4e435f978b578d6ecb20f77ab4f8ab40199686f1
dolong2 <k01066624566@gmail.com>
2024-03-01 17:56:56 +0900

    [add] StopContainerService 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/StopContainerServiceImpl.kt
@@ -0,0 +1,16 @@
+package com.dcd.server.core.domain.application.service.impl
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.service.StopContainerService
+import org.springframework.stereotype.Service
+
+@Service
+class StopContainerServiceImpl(
+    private val commandPort: CommandPort
+) : StopContainerService {
+    override fun stopContainer(application: Application) {
+        val exitValue = commandPort.executeShellCommand("docker stop ${application.name.lowercase()}")
+        if (exitValue != 0) throw RuntimeException()
+    }
+}
