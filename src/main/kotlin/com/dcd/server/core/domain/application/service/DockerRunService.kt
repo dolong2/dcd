f20461c7e73ba07f37e9afc34a9cc1340c8c99a9
dolong2 <k01066624566@gmail.com>
2024-03-01 15:43:56 +0900

    [fix] DockerRunService에선 docker 컨테이너를 실행시키게끔 변경

--- src/main/kotlin/com/dcd/server/core/domain/application/service/DockerRunService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/DockerRunService.kt
@@ -3,12 +3,9 @@ package com.dcd.server.core.domain.application.service
 import com.dcd.server.core.domain.application.model.Application
 
 interface DockerRunService {
-    fun runApplication(id: String, externalPort: Int)
+    fun runApplication(id: String)
 
-    fun runApplication(application: Application, externalPort: Int)
+    fun runApplication(application: Application)
 
-    fun runApplication(id: String, version: String, externalPort: Int)
-
-    fun runApplication(application: Application, version: String, externalPort: Int)
 
 }
--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DockerRunServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DockerRunServiceImpl.kt
@@ -3,6 +3,7 @@ package com.dcd.server.core.domain.application.service.impl
 import com.dcd.server.core.common.command.CommandPort
 import com.dcd.server.core.domain.application.exception.ApplicationEnvNotFoundException
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.exception.ContainerNotCreatedException
 import com.dcd.server.core.domain.application.exception.ContainerNotRunException
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.model.enums.ApplicationType
@@ -15,75 +16,18 @@ class DockerRunServiceImpl(
     private val queryApplicationPort: QueryApplicationPort,
     private val commandPort: CommandPort
 ) : DockerRunService {
-    override fun runApplication(id: String, externalPort: Int) {
+    override fun runApplication(id: String) {
         val application = (queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException())
-        run(application, externalPort = externalPort)
+        run(application)
     }
 
-    override fun runApplication(application: Application, externalPort: Int) {
-        run(application, externalPort = externalPort)
+    override fun runApplication(application: Application) {
+        run(application)
     }
 
-    override fun runApplication(id: String, version: String, externalPort: Int) {
-        val application = (queryApplicationPort.findById(id)
-            ?: throw ApplicationNotFoundException())
-        run(application, version, externalPort)
-    }
-
-    override fun runApplication(application: Application, version: String, externalPort: Int) {
-        run(application, version, externalPort)
-    }
-
-    private fun run(application: Application, version: String = "latest", externalPort: Int) {
-        val exitValue =
-            when (application.applicationType) {
-                ApplicationType.SPRING_BOOT -> {
-                    commandPort.executeShellCommand(
-                        "cd ${application.name} " +
-                        "&& docker run --network ${application.workspace.title.replace(' ', '_')} " +
-                        "--name ${application.name.lowercase()} -d " +
-                        "-p ${externalPort}:${application.port} ${application.name.lowercase()}:$version"
-                    )
-                }
-
-                ApplicationType.MYSQL -> {
-                    val defaultDB =
-                        if (application.env.containsKey("database"))
-                            "-e MYSQL_DATABASE=${application.env["database"]} "
-                        else ""
-                    commandPort.executeShellCommand(
-                        "docker run --network ${application.workspace.title.replace(' ', '_')} " +
-                        "-e MYSQL_ROOT_PASSWORD=${application.env["rootPassword"] ?: throw ApplicationEnvNotFoundException()} " +
-                        defaultDB +
-                        "--name ${application.name.lowercase()} -d " +
-                        "-p ${externalPort}:${application.port} mysql:$version"
-                    )
-                }
-
-                ApplicationType.MARIA_DB -> {
-                    val defaultDB =
-                        if (application.env.containsKey("database"))
-                            "-e MYSQL_DATABASE=${application.env["database"]} "
-                        else ""
-                    commandPort.executeShellCommand(
-                        "docker run --network ${application.workspace.title.replace(' ', '_')} " +
-                        "-e MYSQL_ROOT_PASSWORD=${application.env["rootPassword"] ?: throw ApplicationEnvNotFoundException()} " +
-                        defaultDB +
-                        "--name ${application.name.lowercase()} -d " +
-                        "-p ${externalPort}:${application.port} mariadb:$version"
-                    )
-                }
-
-                ApplicationType.REDIS -> {
-                    commandPort.executeShellCommand(
-                        "docker run --network ${application.workspace.title.replace(' ', '_')} " +
-                        "--name ${application.name.lowercase()} -d " +
-                        "-p ${externalPort}:${application.port} redis:$version"
-                    )
-                }
-            }
-
+    private fun run(application: Application) {
+        val exitValue = commandPort.executeShellCommand("docker start ${application.name.lowercase()}")
         if (exitValue != 0) throw ContainerNotRunException()
     }
 }
