284cf0c8d5a500f939ee93a979da631a5886bef1
dolong2 <k01066624566@gmail.com>
2024-03-02 15:25:26 +0900

    [del] 애플리케이션 실행/정지 유스케이스의 ReadOnly 옵션 제거

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/StopApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/StopApplicationUseCase.kt
@@ -1,18 +1,15 @@
 package com.dcd.server.core.domain.application.usecase
 
-import com.dcd.server.core.common.annotation.ReadOnlyUseCase
+import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.domain.application.exception.AlreadyStoppedException
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
 import com.dcd.server.core.domain.application.service.ChangeApplicationStatusService
-import com.dcd.server.core.domain.application.service.DeleteApplicationDirectoryService
-import com.dcd.server.core.domain.application.service.DeleteContainerService
 import com.dcd.server.core.domain.application.service.StopContainerService
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
-import com.dcd.server.core.domain.user.service.GetCurrentUserService
 import com.dcd.server.core.domain.workspace.service.ValidateWorkspaceOwnerService
 
-@ReadOnlyUseCase
+@UseCase
 class StopApplicationUseCase(
     private val queryApplicationPort: QueryApplicationPort,
     private val stopContainerService: StopContainerService,
