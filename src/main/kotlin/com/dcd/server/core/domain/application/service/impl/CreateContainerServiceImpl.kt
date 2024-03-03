155dd5cb9196a218ebbb6a6a9a74a3b3425f8a77
dolong2 <k01066624566@gmail.com>
2024-03-03 16:29:54 +0900

    [fix] 컨테이너를 생성할때 통일된 명령을 실행하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/CreateContainerServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/CreateContainerServiceImpl.kt
@@ -22,43 +22,9 @@ class CreateContainerServiceImpl(
 
     private fun create(application: Application, version: String = "latest", externalPort: Int) {
         val cmd =
-            when (application.applicationType) {
-                ApplicationType.SPRING_BOOT -> {
-                    "docker create --network ${application.workspace.title.replace(' ', '_')} " +
-                    "--name ${application.name.lowercase()} -d " +
-                    "-p ${externalPort}:${application.port} ${application.name.lowercase()}:$version"
-                }
-
-                ApplicationType.MYSQL -> {
-                    val defaultDB =
-                        if (application.env.containsKey("database"))
-                            "-e MYSQL_DATABASE=${application.env["database"]} "
-                        else ""
-                    "docker create --network ${application.workspace.title.replace(' ', '_')} " +
-                    "-e MYSQL_ROOT_PASSWORD=${application.env["rootPassword"] ?: throw ApplicationEnvNotFoundException()} " +
-                    defaultDB +
-                    "--name ${application.name.lowercase()} -d " +
-                    "-p ${externalPort}:${application.port} mysql:$version"
-                }
-
-                ApplicationType.MARIA_DB -> {
-                    val defaultDB =
-                        if (application.env.containsKey("database"))
-                            "-e MYSQL_DATABASE=${application.env["database"]} "
-                        else ""
-                    "docker create --network ${application.workspace.title.replace(' ', '_')} " +
-                    "-e MYSQL_ROOT_PASSWORD=${application.env["rootPassword"] ?: throw ApplicationEnvNotFoundException()} " +
-                    defaultDB +
-                    "--name ${application.name.lowercase()} -d " +
-                    "-p ${externalPort}:${application.port} mariadb:$version"
-                }
-
-                ApplicationType.REDIS -> {
-                    "docker run --network ${application.workspace.title.replace(' ', '_')} " +
-                    "--name ${application.name.lowercase()} -d " +
-                    "-p ${externalPort}:${application.port} redis:$version"
-                }
-            }
+            "docker create --network ${application.workspace.title.replace(' ', '_')} " +
+            "--name ${application.name.lowercase()} -d " +
+            "-p ${externalPort}:${application.port} ${application.name.lowercase()}:$version"
 
         val exitValue = commandPort.executeShellCommand(cmd)
         if (exitValue != 0) throw ContainerNotCreatedException()
