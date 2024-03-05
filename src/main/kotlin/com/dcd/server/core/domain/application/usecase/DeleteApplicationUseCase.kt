6064ff6efeb19d8a7e0389938c8b3173a66ea834
dolong2 <k01066624566@gmail.com>
2024-03-05 20:05:20 +0900

    [fix] DeleteApplicationUseCase에서 컨테이너와 이미지를 제거하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/DeleteApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/DeleteApplicationUseCase.kt
@@ -2,6 +2,8 @@ package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.service.DeleteContainerService
+import com.dcd.server.core.domain.application.service.DeleteImageService
 import com.dcd.server.core.domain.application.spi.CommandApplicationPort
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import com.dcd.server.core.domain.user.service.GetCurrentUserService
@@ -12,13 +14,19 @@ class DeleteApplicationUseCase(
     private val getCurrentUserService: GetCurrentUserService,
     private val commandApplicationPort: CommandApplicationPort,
     private val queryApplicationPort: QueryApplicationPort,
-    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService
+    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService,
+    private val deleteContainerService: DeleteContainerService,
+    private val deleteImageService: DeleteImageService
 ) {
     fun execute(id: String) {
         val user = getCurrentUserService.getCurrentUser()
         val application = (queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException())
         validateWorkspaceOwnerService.validateOwner(user, application.workspace)
+
+        deleteContainerService.deleteContainer(application)
+        deleteImageService.deleteImage(application)
+
         commandApplicationPort.delete(application)
     }
 }
