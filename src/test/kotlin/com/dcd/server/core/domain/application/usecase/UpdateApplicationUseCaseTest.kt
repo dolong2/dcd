82bd323c962c88338dc1b947e75f206af3f94cf3
dolong2 <k01066624566@gmail.com>
2024-03-09 13:24:21 +0900

    [add] UpdateApplicationUseCase 테스트 코드에서 실행중인 애플리케이션이 주어질때 usecase를 실행하는 테스트 케이스 추가

--- src/test/kotlin/com/dcd/server/core/domain/application/usecase/UpdateApplicationUseCaseTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/UpdateApplicationUseCaseTest.kt
@@ -1,6 +1,7 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.domain.application.dto.request.UpdateApplicationReqDto
+import com.dcd.server.core.domain.application.exception.AlreadyRunningException
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
@@ -68,6 +69,21 @@ class UpdateApplicationUseCaseTest : BehaviorSpec({
 
             then("ApplicationNotFoundException이 발생해야함") {
                 shouldThrow<ApplicationNotFoundException> {
+                    updateApplicationUseCase.execute(applicationId, updateReqDto)
+                }
+            }
+        }
+    }
+
+    given("실행중인 애플리케이션이 주어지고") {
+        val application = ApplicationGenerator.generateApplication(id = applicationId, workspace = workspace, status = ApplicationStatus.RUNNING)
+
+        `when`("usecase를 실행할때") {
+            every { getCurrentUserService.getCurrentUser() } returns user
+            every { queryApplicationPort.findById(applicationId) } returns application
+
+            then("ReqDto의 내용이 반영된 애플리케이션을 저장해야함") {
+                shouldThrow<AlreadyRunningException> {
                     updateApplicationUseCase.execute(applicationId, updateReqDto)
                 }
             }
