8bc663f97909cc43c686af7d32870af78205b7e6
dolong2 <k01066624566@gmail.com>
2024-03-10 15:06:10 +0900

    [fix] GetAvailableVersionUseCase에 ReadOnlyUseCase 적용

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAvailableVersionUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAvailableVersionUseCase.kt
@@ -1,11 +1,11 @@
 package com.dcd.server.core.domain.application.usecase
 
-import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.annotation.ReadOnlyUseCase
 import com.dcd.server.core.domain.application.dto.response.AvailableVersionResDto
 import com.dcd.server.core.domain.application.model.enums.ApplicationType
 import com.dcd.server.core.domain.application.service.GetApplicationVersionService
 
-@UseCase
+@ReadOnlyUseCase
 class GetAvailableVersionUseCase(
     private val getApplicationVersionService: GetApplicationVersionService
 ) {
