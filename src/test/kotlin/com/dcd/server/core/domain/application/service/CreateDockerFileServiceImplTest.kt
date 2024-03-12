f7cd48dee5a2d18d4f5ffdf08e856ee2faf2de95
dolong2 <k01066624566@gmail.com>
2024-03-12 21:19:04 +0900

    [add] 레디스 애플리케이션이 주어졌을때 CreateDockerFileService을 실행하는 테스트 케이스 추가

--- src/test/kotlin/com/dcd/server/core/domain/application/service/CreateDockerFileServiceImplTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/service/CreateDockerFileServiceImplTest.kt
@@ -45,4 +45,30 @@ class CreateDockerFileServiceImplTest : BehaviorSpec({
         commandPort.executeShellCommand("rm -rf ${application.name}") // 실제로 생성된 디렉토리 제거
     }
 
+    given("레디스 애플리케이션이 주어지고") {
+        val application =
+            ApplicationGenerator.generateApplication(applicationType = ApplicationType.REDIS)
+
+        `when`("서비스를 실행할때") {
+            createDockerFileService.createFileToApplication(application, application.version)
+
+            then("애플리케이션의 이름을 가진 디렉토리를 생성해야함") {
+                verify { commandPort.executeShellCommand("mkdir ${application.name}") }
+            }
+            then("실제로 애플리케이션 이름의 디렉토리가 생성되야함") {
+                commandPort.executeShellCommand("test -e ${application.name}") shouldBe 0
+            }
+            then("생성된 DockerFile의 내용은 FileContent의 내용과 같아야함") {
+                val actualFileContent = StringBuilder()
+                File("./${application.name}/Dockerfile").forEachLine {
+                    actualFileContent.append(it + "\n")
+                }
+
+                actualFileContent.toString() shouldBe FileContent.getRedisDockerFileContent(application.version, application.port, application.env)
+            }
+        }
+
+        commandPort.executeShellCommand("rm -rf ${application.name}") // 실제로 생성된 디렉토리 제거
+    }
+
 })
