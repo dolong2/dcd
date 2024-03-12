34a9a2bb63b51f260b61048c81a01992c79b344c
dolong2 <k01066624566@gmail.com>
2024-03-12 20:13:52 +0900

    [add] CreateContainerService를 실행하는 테스트 케이스 추가

---
+++ src/test/kotlin/com/dcd/server/core/domain/application/service/CreateContainerServiceImplTest.kt
@@ -0,0 +1,34 @@
+package com.dcd.server.core.domain.application.service
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.exception.ContainerNotCreatedException
+import com.dcd.server.core.domain.application.service.impl.CreateContainerServiceImpl
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import util.application.ApplicationGenerator
+
+class CreateContainerServiceImplTest : BehaviorSpec({
+    val commandPort = mockk<CommandPort>(relaxed = true)
+    val createContainerService = CreateContainerServiceImpl(commandPort)
+
+    given("애플리케이션이 주어지고") {
+        val application = ApplicationGenerator.generateApplication()
+
+        `when`("service를 실행할때") {
+            createContainerService.createContainer(application, application.externalPort)
+
+            then("컨테이너를 실행하는 명령을 실행해야함") {
+                verify {
+                    commandPort.executeShellCommand(
+                        "docker create --network ${application.workspace.title.replace(' ', '_')} " +
+                        "--name ${application.name.lowercase()} " +
+                        "-p ${application.externalPort}:${application.port} ${application.name.lowercase()}:latest"
+                    )
+                }
+            }
+        }
+    }
+})
