d7290fbc219336ad708f91d5fc79d6e2bebad40b
dolong2 <k01066624566@gmail.com>
2024-03-13 16:50:28 +0900

    [add] StopContainerService를 실행할때 컨테이너 정지 명령이 실패하는 테스트 케이스 추가

--- src/test/kotlin/com/dcd/server/core/domain/application/service/StopContainerServiceImplTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/service/StopContainerServiceImplTest.kt
@@ -1,8 +1,11 @@
 package com.dcd.server.core.domain.application.service
 
 import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.exception.ContainerNotStoppedException
 import com.dcd.server.core.domain.application.service.impl.StopContainerServiceImpl
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import util.application.ApplicationGenerator
@@ -19,6 +22,16 @@ class StopContainerServiceImplTest : BehaviorSpec({
 
             then("컨테이너 정지 명령이 실행되야함") {
                 verify { commandPort.executeShellCommand("docker stop ${application.name.lowercase()}") }
+            }
+        }
+
+        `when`("컨테이너 정지 명령이 실패했을때") {
+            every { commandPort.executeShellCommand("docker stop ${application.name.lowercase()}") } returns 125
+
+            then("ContainerNotStoppedException이 발생해야함") {
+                shouldThrow<ContainerNotStoppedException> {
+                    stopContainerService.stopContainer(application)
+                }
             }
         }
     }
