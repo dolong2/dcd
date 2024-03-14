f5c67512e044762b868735bef6257207f8b962d5
dolong2 <k01066624566@gmail.com>
2024-03-14 17:40:53 +0900

    [add] WorkspaceVerification 어노테이션 어드바이스 추가

--- src/main/kotlin/com/dcd/server/core/common/aop/WorkspaceValidateAspect.kt
+++ src/main/kotlin/com/dcd/server/core/common/aop/WorkspaceValidateAspect.kt
@@ -1,13 +1,31 @@
 package com.dcd.server.core.common.aop
 
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
 
 @Aspect
 @Component
-class WorkspaceValidateAspect {
+class WorkspaceValidateAspect(
+    private val getCurrentUserService: GetCurrentUserService,
+    private val queryApplicationPort: QueryApplicationPort
+) {
     @Pointcut("@annotation(com.dcd.server.core.annotation.WorkspaceOwnerVerification)")
     fun verificationPointcut() {}
 
+    @Before("verificationPointcut() && args(id, ..)")
+    fun validWorkspaceOwner(id: String) {
+        val user = getCurrentUserService.getCurrentUser()
+
+        val application = queryApplicationPort.findById(id)
+            ?: throw ApplicationNotFoundException()
+
+        if (application.workspace.owner.equals(user))
+            throw WorkspaceOwnerNotSameException()
+    }
 }
