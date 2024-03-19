9c9f85f23ae5099e58ea7015d98472bb93a44dc6
dolong2 <k01066624566@gmail.com>
2024-03-19 19:33:33 +0900

    [add] WorkspaceOwnerVerification 적용

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/CreateApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/CreateApplicationUseCase.kt
@@ -1,20 +1,19 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.annotation.WorkspaceOwnerVerification
 import com.dcd.server.core.domain.application.dto.extenstion.toEntity
 import com.dcd.server.core.domain.application.dto.request.CreateApplicationReqDto
 import com.dcd.server.core.domain.application.model.enums.ApplicationType
 import com.dcd.server.core.domain.application.service.*
 import com.dcd.server.core.domain.application.spi.CommandApplicationPort
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
-import com.dcd.server.core.domain.workspace.service.ValidateWorkspaceOwnerService
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 
 @UseCase
 class CreateApplicationUseCase(
     private val commandApplicationPort: CommandApplicationPort,
     private val queryWorkspacePort: QueryWorkspacePort,
-    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService,
     private val cloneApplicationByUrlService: CloneApplicationByUrlService,
     private val modifyGradleService: ModifyGradleService,
     private val createDockerFileService: CreateDockerFileService,
@@ -23,10 +22,10 @@ class CreateApplicationUseCase(
     private val createContainerService: CreateContainerService,
     private val deleteApplicationDirectoryService: DeleteApplicationDirectoryService
 ) {
+    @WorkspaceOwnerVerification
     fun execute(workspaceId: String, createApplicationReqDto: CreateApplicationReqDto) {
         val workspace = queryWorkspacePort.findById(workspaceId)
             ?: throw WorkspaceNotFoundException()
-        validateWorkspaceOwnerService.validateOwner(workspace)
 
         val externalPort = getExternalPortService.getExternalPort(createApplicationReqDto.port)
 
--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCase.kt
@@ -1,26 +1,23 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.ReadOnlyUseCase
+import com.dcd.server.core.common.annotation.WorkspaceOwnerVerification
 import com.dcd.server.core.domain.application.dto.extenstion.toDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationListResDto
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
-import com.dcd.server.core.domain.user.service.GetCurrentUserService
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
-import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 
 @ReadOnlyUseCase
 class GetAllApplicationUseCase(
     private val queryApplicationPort: QueryApplicationPort,
-    private val getCurrentUserService: GetCurrentUserService,
     private val queryWorkspacePort: QueryWorkspacePort
 ) {
+    @WorkspaceOwnerVerification
     fun execute(workspaceId: String): ApplicationListResDto {
         val workspace = (queryWorkspacePort.findById(workspaceId)
             ?: throw WorkspaceNotFoundException())
-        val currentUser = getCurrentUserService.getCurrentUser()
-        if (workspace.owner.equals(currentUser).not())
-            throw WorkspaceOwnerNotSameException()
+
         return ApplicationListResDto(
             queryApplicationPort
                 .findAllByWorkspace(
