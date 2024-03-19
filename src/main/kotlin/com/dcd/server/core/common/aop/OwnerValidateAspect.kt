38932277a2acceeb4fa658d76195e0ad395b187f
dolong2 <k01066624566@gmail.com>
2024-03-19 18:10:53 +0900

    [add] 워크스페이스의 소유자 검증 조인트포인트 추가

--- src/main/kotlin/com/dcd/server/core/common/aop/OwnerValidateAspect.kt
+++ src/main/kotlin/com/dcd/server/core/common/aop/OwnerValidateAspect.kt
@@ -3,6 +3,7 @@ package com.dcd.server.core.common.aop
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 import org.aspectj.lang.annotation.Aspect
@@ -18,16 +19,31 @@ class OwnerValidateAspect(
     private val queryApplicationPort: QueryApplicationPort
 ) {
     @Pointcut("@annotation(com.dcd.server.core.common.annotation.ApplicationOwnerVerification)")
-    fun verificationPointcut() {}
+    fun applicationOwnerVerificationPointcut() {}
 
-    @Before("verificationPointcut() && args(id, ..)")
-    fun validWorkspaceOwner(id: String) {
+    @Pointcut("@annotation(com.dcd.server.core.common.annotation.WorkspaceOwnerVerification)")
+    fun workspaceOwnerVerificationPointcut() {}
+
+    @Before("applicationOwnerVerificationPointcut() && args(id, ..)")
+    fun validApplicationOwner(id: String) {
         val user = getCurrentUserService.getCurrentUser()
 
         val application = (queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException())
 
         val owner = application.workspace.owner
+        if (!owner.equals(user))
+            throw WorkspaceOwnerNotSameException()
+    }
+
+    @Before("workspaceOwnerVerificationPointcut() && args(id, ..)")
+    fun validWorkspaceOwner(id: String) {
+        val user = getCurrentUserService.getCurrentUser()
+
+        val workspace = (queryWorkspacePort.findById(id)
+            ?: throw WorkspaceNotFoundException())
+
+        val owner = workspace.owner
         if (!owner.equals(user))
             throw WorkspaceOwnerNotSameException()
     }
