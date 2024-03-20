666785193c248633b834eb0d66a6655c33ecdbf3
dolong2 <k01066624566@gmail.com>
2024-03-20 18:52:21 +0900

    [add] UpdateApplicationEnvUseCase 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/UpdateApplicationEnvUseCase.kt
@@ -0,0 +1,32 @@
+package com.dcd.server.core.domain.application.usecase
+
+import com.dcd.server.core.common.annotation.ApplicationOwnerVerification
+import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.domain.application.dto.request.UpdateApplicationEnvReqDto
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.spi.CommandApplicationPort
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+
+@UseCase
+class UpdateApplicationEnvUseCase(
+    private val queryApplicationPort: QueryApplicationPort,
+    private val commandApplicationPort: CommandApplicationPort
+) {
+    @ApplicationOwnerVerification
+    fun execute(applicationId: String, envKey: String, updateApplicationEnvReqDto: UpdateApplicationEnvReqDto) {
+        val application = (queryApplicationPort.findById(applicationId)
+            ?: throw ApplicationNotFoundException())
+
+        val env = application.env
+        if (env.containsKey(envKey).not())
+            throw RuntimeException()//해당 env를 찾을 수 없음
+
+        val mutableEnv = env.toMutableMap()
+        mutableEnv[envKey] = updateApplicationEnvReqDto.newValue
+        commandApplicationPort.save(
+            application.copy(
+                env = mutableEnv
+            )
+        )
+    }
+}
