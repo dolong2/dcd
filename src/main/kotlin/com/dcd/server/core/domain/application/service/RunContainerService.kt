515534e06040d1c6537cdd9266394ae482125381
dolong2 <k01066624566@gmail.com>
2024-03-26 18:03:52 +0900

    [fix] DockerRunService의 네이밍을 RunContainerService으로 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/service/RunContainerService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/RunContainerService.kt
@@ -2,7 +2,7 @@ package com.dcd.server.core.domain.application.service
 
 import com.dcd.server.core.domain.application.model.Application
 
-interface DockerRunService {
+interface RunContainerService {
     fun runApplication(id: String)
 
     fun runApplication(application: Application)
