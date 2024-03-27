e86aff879f7ad392b040d7fd170b90e55657a785
dolong2 <k01066624566@gmail.com>
2024-03-27 20:50:50 +0900

    [fix] Workspace의 toDto 메서드에서 toWorkspaceDto를 호출하도록 변경

--- src/main/kotlin/com/dcd/server/core/domain/workspace/dto/extension/WorkspaceDtoExtension.kt
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/dto/extension/WorkspaceDtoExtension.kt
@@ -1,6 +1,7 @@
 package com.dcd.server.core.domain.workspace.dto.extension
 
 import com.dcd.server.core.domain.application.dto.extenstion.toDto
+import com.dcd.server.core.domain.application.dto.extenstion.toWorkspaceDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationProfileResDto
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.user.dto.extension.toDto
@@ -16,7 +17,7 @@ fun Workspace.toDto(applicationList: List<Application>): WorkspaceResDto =
         title = this.title,
         description = this.description,
         owner = this.owner.toDto(),
-        applicationList = applicationList.map { it.toDto() }
+        applicationList = applicationList.map { it.toWorkspaceDto() }
     )
 
 fun CreateWorkspaceReqDto.toEntity(user: User): Workspace =
