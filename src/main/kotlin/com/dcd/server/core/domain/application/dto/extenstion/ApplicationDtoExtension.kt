0d5d799160af988cbd2896eca3c218be79059b46
dolong2 <k01066624566@gmail.com>
2024-03-27 20:44:04 +0900

    [add] Application 도메인에 toWorkspaceDto 확장함수 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/dto/extenstion/ApplicationDtoExtension.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/dto/extenstion/ApplicationDtoExtension.kt
@@ -5,6 +5,7 @@ import com.dcd.server.core.domain.application.dto.response.ApplicationProfileRes
 import com.dcd.server.core.domain.application.dto.response.ApplicationResDto
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
+import com.dcd.server.core.domain.workspace.dto.response.WorkspaceApplicationResDto
 import com.dcd.server.core.domain.workspace.model.Workspace
 
 fun CreateApplicationReqDto.toEntity(workspace: Workspace, externalPort: Int): Application =
@@ -40,4 +41,15 @@ fun Application.toProfileDto(): ApplicationProfileResDto =
         id = this.id,
         name = this.name,
         description = this.description
+    )
+
+fun Application.toWorkspaceDto(): WorkspaceApplicationResDto =
+    WorkspaceApplicationResDto(
+        id = this.id,
+        name = this.name,
+        description = this.description,
+        applicationType = this.applicationType,
+        port = this.port,
+        externalPort = this.externalPort,
+        status = this.status
     )
