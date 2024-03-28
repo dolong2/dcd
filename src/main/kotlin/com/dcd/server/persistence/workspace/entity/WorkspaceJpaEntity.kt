547a81d8698dc5755a53309dbc5254b57d6930f5
dolong2 <k01066624566@gmail.com>
2024-03-28 17:36:14 +0900

    [fix] WorkspaceJpaEntity의 테이블명을 workspace로 수정

--- src/main/kotlin/com/dcd/server/persistence/workspace/entity/WorkspaceJpaEntity.kt
+++ src/main/kotlin/com/dcd/server/persistence/workspace/entity/WorkspaceJpaEntity.kt
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
+@Table(name = "workspace")
 class WorkspaceJpaEntity(
     @Id
     val id: String = UUID.randomUUID().toString(),
