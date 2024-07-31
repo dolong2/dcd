db5a683ac9a7b538c1ccd20aa7b8a2c087a4c2bc
dolong2 <k01066624566@gmail.com>
2024-07-31 23:21:33 +0900

    [add] WorkspaceSimpleResponse 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/workspace/data/exetension/WorkspaceResponseDataExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/workspace/data/exetension/WorkspaceResponseDataExtension.kt
@@ -3,11 +3,13 @@ package com.dcd.server.presentation.domain.workspace.data.exetension
 import com.dcd.server.core.domain.workspace.dto.response.WorkspaceListResDto
 import com.dcd.server.core.domain.workspace.dto.response.WorkspaceProfileResDto
 import com.dcd.server.core.domain.workspace.dto.response.WorkspaceResDto
+import com.dcd.server.core.domain.workspace.dto.response.WorkspaceSimpleResDto
 import com.dcd.server.presentation.domain.application.data.exetension.toResponse
 import com.dcd.server.presentation.domain.user.data.exetension.toResponse
 import com.dcd.server.presentation.domain.workspace.data.response.WorkspaceListResponse
 import com.dcd.server.presentation.domain.workspace.data.response.WorkspaceProfileResponse
 import com.dcd.server.presentation.domain.workspace.data.response.WorkspaceResponse
+import com.dcd.server.presentation.domain.workspace.data.response.WorkspaceSimpleResponse
 
 fun WorkspaceResDto.toResponse(): WorkspaceResponse =
     WorkspaceResponse(
@@ -16,6 +18,13 @@ fun WorkspaceResDto.toResponse(): WorkspaceResponse =
         description = this.description,
         applicationList = this.applicationList.map { it.toResponse() },
         owner = this.owner.toResponse()
+    )
+
+fun WorkspaceSimpleResDto.toResponse(): WorkspaceSimpleResponse =
+    WorkspaceSimpleResponse(
+        id = this.id,
+        title = this.title,
+        description = this.description
     )
 
 fun WorkspaceListResDto.toResponse(): WorkspaceListResponse =
---
+++ src/main/kotlin/com/dcd/server/presentation/domain/workspace/data/response/WorkspaceSimpleResponse.kt
@@ -0,0 +1,7 @@
+package com.dcd.server.presentation.domain.workspace.data.response
+
+data class WorkspaceSimpleResponse(
+    val id: String,
+    val title: String,
+    val description: String
+)
