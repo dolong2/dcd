cc9e2a8c43fd32cb3330f20ea115b287fff87fdf
dolong2 <k01066624566@gmail.com>
2024-08-25 21:01:06 +0900

    [chore] SignUpReqDto의 toEntity 메서드에서 유저 상태를 PENDING으로 초기화 하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/auth/dto/extension/AuthDtoExtension.kt
+++ src/main/kotlin/com/dcd/server/core/domain/auth/dto/extension/AuthDtoExtension.kt
@@ -2,6 +2,7 @@ package com.dcd.server.core.domain.auth.dto.extension
 
 import com.dcd.server.core.domain.auth.dto.request.SignUpReqDto
 import com.dcd.server.core.domain.auth.model.Role
+import com.dcd.server.core.domain.user.model.Status
 import com.dcd.server.core.domain.user.model.User
 
 fun SignUpReqDto.toEntity(encodedPassword: String): User =
@@ -9,5 +10,6 @@ fun SignUpReqDto.toEntity(encodedPassword: String): User =
         email = this.email,
         password = encodedPassword,
         name = this.name,
-        roles = mutableListOf(Role.ROLE_USER)
+        roles = mutableListOf(Role.ROLE_USER),
+        status = Status.PENDING
     )
