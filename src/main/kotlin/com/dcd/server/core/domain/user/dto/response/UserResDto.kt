3966d02b3c51c0b09b1d7db65b0c3fb755620a4c
dolong2 <k01066624566@gmail.com>
2024-08-28 21:04:11 +0900

    [add] UserResDto에 status 필드 추가

--- src/main/kotlin/com/dcd/server/core/domain/user/dto/response/UserResDto.kt
+++ src/main/kotlin/com/dcd/server/core/domain/user/dto/response/UserResDto.kt
@@ -1,7 +1,10 @@
 package com.dcd.server.core.domain.user.dto.response
 
+import com.dcd.server.core.domain.user.model.Status
+
 data class UserResDto(
     val id: String,
     val email: String,
-    val name: String
+    val name: String,
+    val status: Status
 )
