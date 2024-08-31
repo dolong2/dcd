1f57f954850948e039bfd45690d47a62f86a8937
dolong2 <k01066624566@gmail.com>
2024-08-31 17:17:25 +0900

    [add] GetUserByStatusUseCase 테스트 코드 추가

---
+++ src/test/kotlin/com/dcd/server/core/domain/user/usecase/GetUserByStatusUseCaseTest.kt
@@ -0,0 +1,31 @@
+package com.dcd.server.core.domain.user.usecase
+
+import com.dcd.server.core.domain.user.dto.extension.toDto
+import com.dcd.server.core.domain.user.model.Status
+import com.dcd.server.core.domain.user.spi.QueryUserPort
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import util.user.UserGenerator
+
+class GetUserByStatusUseCaseTest : BehaviorSpec({
+    val queryUserPort = mockk<QueryUserPort>()
+    val getUserByStatusUseCase = GetUserByStatusUseCase(queryUserPort)
+
+    given("조회할 status가 주어지고") {
+        val status = Status.CREATED
+
+        `when`("execute 메서드를 실행할때") {
+            val userList = listOf(UserGenerator.generateUser())
+            every { queryUserPort.findByStatus(status) } returns userList
+            val result = getUserByStatusUseCase.execute(status)
+
+            then("result는 유저의 정보를 가지고 있어야함") {
+                verify { queryUserPort.findByStatus(status) }
+                result.list shouldBe userList.map { it.toDto() }
+            }
+        }
+    }
+})
