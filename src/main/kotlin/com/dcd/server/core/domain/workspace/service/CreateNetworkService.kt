7781b4287998dfea886486f3517810a3f9c972e4
dolong2 <k01066624566@gmail.com>
2024-09-03 23:06:22 +0900

    [chore] CreateNetworkService를 인터페이스로 수정

--- src/main/kotlin/com/dcd/server/core/domain/workspace/service/CreateNetworkService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/service/CreateNetworkService.kt
@@ -1,13 +1,5 @@
 package com.dcd.server.core.domain.workspace.service
 
-import com.dcd.server.core.common.command.CommandPort
-import org.springframework.stereotype.Service
-
-@Service
-class CreateNetworkService(
-    private val commandPort: CommandPort
-) {
-    fun createNetwork(networkTitle: String) {
-        commandPort.executeShellCommand("docker network create --driver bridge ${networkTitle.replace(' ', '_')}")
-    }
+interface CreateNetworkService {
+    fun createNetwork(networkTitle: String)
 }
