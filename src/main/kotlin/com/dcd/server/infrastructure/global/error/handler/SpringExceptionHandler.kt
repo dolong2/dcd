2c902042015f5a09413fa853540abd88a87d2601
dolong2 <k01066624566@gmail.com>
2024-09-04 23:21:14 +0900

    [add] SpringExceptionHandler 추가

---
+++ src/main/kotlin/com/dcd/server/infrastructure/global/error/handler/SpringExceptionHandler.kt
@@ -0,0 +1,29 @@
+package com.dcd.server.infrastructure.global.error.handler
+
+import com.dcd.server.core.common.error.ErrorCode
+import com.dcd.server.infrastructure.global.error.response.ErrorResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class SpringExceptionHandler{
+    private val log = LoggerFactory.getLogger(this::class.simpleName)
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(request: HttpServletRequest, ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        log.error(request.method)
+        log.error(request.requestURI)
+        val errorCode = ErrorCode.BAD_REQUEST
+        log.error(errorCode.msg)
+        log.error("${errorCode.code}")
+        return ResponseEntity(
+            ErrorResponse(errorCode),
+            HttpStatusCode.valueOf(errorCode.code)
+        )
+    }
+}
