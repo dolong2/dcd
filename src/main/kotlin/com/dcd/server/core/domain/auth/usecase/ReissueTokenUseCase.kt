39a4c1083aeb63f301a0d6672d8a5395a33c6b85
dolong2 <k01066624566@gmail.com>
2024-09-10 23:15:40 +0900

    [add] ReissueTokenUseCase에서 REFRESH 타입의 토큰인지 검증하는 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/auth/usecase/ReissueTokenUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/auth/usecase/ReissueTokenUseCase.kt
@@ -8,15 +8,22 @@ import com.dcd.server.core.domain.auth.spi.CommandRefreshTokenPort
 import com.dcd.server.core.domain.auth.spi.JwtPort
 import com.dcd.server.core.domain.auth.spi.QueryRefreshTokenPort
 import com.dcd.server.core.domain.user.spi.QueryUserPort
+import com.dcd.server.infrastructure.global.jwt.adapter.ParseTokenAdapter
+import com.dcd.server.infrastructure.global.jwt.exception.TokenTypeNotValidException
 
 @UseCase
 class ReissueTokenUseCase(
     private val queryRefreshTokenPort: QueryRefreshTokenPort,
     private val commandRefreshTokenPort: CommandRefreshTokenPort,
     private val jwtPort: JwtPort,
-    private val queryUserPort: QueryUserPort
+    private val queryUserPort: QueryUserPort,
+    private val parseTokenAdapter: ParseTokenAdapter
 ) {
     fun execute(token: String): TokenResDto {
+        val jwtType = parseTokenAdapter.getJwtType(token)
+        if (jwtType != "REFRESH")
+            throw TokenTypeNotValidException()
+
         val refreshToken = (queryRefreshTokenPort.findByToken(token)
             ?: throw ExpiredRefreshTokenException()) // 리프레시 토큰 만료
         val user = (queryUserPort.findById(refreshToken.userId)
