97160fea922de612ef215d22978e7ae6eedf6dbe
dolong2 <k01066624566@gmail.com>
2024-09-13 17:09:12 +0900

    [add] AuthDetails 추가

---
+++ src/main/kotlin/com/dcd/server/infrastructure/global/security/auth/AuthDetails.kt
@@ -0,0 +1,27 @@
+package com.dcd.server.infrastructure.global.security.auth
+
+import com.dcd.server.core.domain.user.model.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val user: User
+) : UserDetails{
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        user.roles
+            .map { SimpleGrantedAuthority(it.name) }
+            .toMutableList()
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = user.id
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
