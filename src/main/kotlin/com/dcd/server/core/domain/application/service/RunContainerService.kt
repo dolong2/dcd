6a68af43f900cf4158db4e827321987eed341ad9
dolong2 <k01066624566@gmail.com>
2024-09-17 15:48:22 +0900

    [chore] RunContainerService의 runApplication 메서드를 suspend 메서드로 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/service/RunContainerService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/RunContainerService.kt
@@ -3,7 +3,7 @@ package com.dcd.server.core.domain.application.service
 import com.dcd.server.core.domain.application.model.Application
 
 interface RunContainerService {
-    fun runApplication(id: String)
+    suspend fun runApplication(id: String)
 
-    fun runApplication(application: Application)
+   suspend fun runApplication(application: Application)
 }
