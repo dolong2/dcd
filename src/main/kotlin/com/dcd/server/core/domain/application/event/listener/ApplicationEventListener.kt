421fca51fbfb3fd7e5bc08fdedc91cfe7c004ad1
dolong2 <k01066624566@gmail.com>
2024-09-18 15:30:54 +0900

    [add] ChangeApplicationStatusEvent, ApplicationEventListener 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/event/listener/ApplicationEventListener.kt
@@ -0,0 +1,22 @@
+package com.dcd.server.core.domain.application.event.listener
+
+import com.dcd.server.core.domain.application.event.ChangeApplicationStatusEvent
+import com.dcd.server.core.domain.application.spi.CommandApplicationPort
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class ApplicationEventListener(
+    private val commandApplicationPort: CommandApplicationPort
+) {
+    @EventListener
+    @Transactional(rollbackFor = [Exception::class])
+    fun process(event: ChangeApplicationStatusEvent) {
+        val updatedApplication = event.application.copy(
+            status = event.status
+        )
+
+        commandApplicationPort.save(updatedApplication)
+    }
+}
