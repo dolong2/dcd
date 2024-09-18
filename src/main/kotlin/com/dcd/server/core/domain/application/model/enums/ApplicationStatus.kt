70162c1d8331d633f401316ada090f3d386d4e2c
dolong2 <k01066624566@gmail.com>
2024-09-18 15:33:16 +0900

    [add] ApplicationStatus에 CREATED, PENDING, FAILURE 상태 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/model/enums/ApplicationStatus.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/model/enums/ApplicationStatus.kt
@@ -1,6 +1,9 @@
 package com.dcd.server.core.domain.application.model.enums
 
-enum class ApplicationStatus {
-    RUNNING,
-    STOPPED
+enum class ApplicationStatus(description: String) {
+    CREATED("애플리케이션이 생성됨"),
+    PENDING("애플리케이션 상태 보류중 ex). 실행되기전, 정지되기전"),
+    RUNNING("애플리케이션 실행중"),
+    STOPPED("애플리케이션이 정지됨"),
+    FAILURE("애플리케이션의 플로우중 실패함")
 }
