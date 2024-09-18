3dd5fe6a36303b329ca1d93649844ca397743571
dolong2 <k01066624566@gmail.com>
2024-09-18 15:42:13 +0900

    [chore] 이벤트를 통해 애플리케이션의 상태를 이벤트를 통해서 수정하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/CreateContainerServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/CreateContainerServiceImpl.kt
@@ -1,17 +1,20 @@
 package com.dcd.server.core.domain.application.service.impl
 
 import com.dcd.server.core.common.command.CommandPort
-import com.dcd.server.core.domain.application.exception.ApplicationEnvNotFoundException
-import com.dcd.server.core.domain.application.exception.ContainerNotCreatedException
+import com.dcd.server.core.domain.application.event.ChangeApplicationStatusEvent
 import com.dcd.server.core.domain.application.model.Application
-import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
 import com.dcd.server.core.domain.application.service.CreateContainerService
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class CreateContainerServiceImpl(
-    private val commandPort: CommandPort
-) : CreateContainerService{
+    private val commandPort: CommandPort,
+    private val eventPublisher: ApplicationEventPublisher
+) : CreateContainerService {
+    private val log = LoggerFactory.getLogger(this::class.simpleName)
     override fun createContainer(application: Application, externalPort: Int) {
         val cmd =
             "docker create --network ${application.workspace.title.replace(' ', '_')} " +
@@ -19,6 +22,9 @@ class CreateContainerServiceImpl(
             "-p ${externalPort}:${application.port} ${application.name.lowercase()}:latest"
 
         val exitValue = commandPort.executeShellCommand(cmd)
-        if (exitValue != 0) throw ContainerNotCreatedException()
+        if (exitValue != 0) {
+            log.error("$exitValue")
+            eventPublisher.publishEvent(ChangeApplicationStatusEvent(ApplicationStatus.FAILURE, application))
+        }
     }
 }
--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/StopContainerServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/StopContainerServiceImpl.kt
@@ -1,17 +1,28 @@
 package com.dcd.server.core.domain.application.service.impl
 
 import com.dcd.server.core.common.command.CommandPort
-import com.dcd.server.core.domain.application.exception.ContainerNotStoppedException
+import com.dcd.server.core.domain.application.event.ChangeApplicationStatusEvent
 import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
 import com.dcd.server.core.domain.application.service.StopContainerService
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class StopContainerServiceImpl(
-    private val commandPort: CommandPort
+    private val commandPort: CommandPort,
+    private val eventPublisher: ApplicationEventPublisher
 ) : StopContainerService {
+    private val log = LoggerFactory.getLogger(this::class.simpleName)
+
     override fun stopContainer(application: Application) {
         val exitValue = commandPort.executeShellCommand("docker stop ${application.name.lowercase()}")
-        if (exitValue != 0) throw ContainerNotStoppedException()
+        if (exitValue != 0) {
+            log.error("$exitValue")
+            eventPublisher.publishEvent(ChangeApplicationStatusEvent(ApplicationStatus.FAILURE, application))
+        }
+
+        eventPublisher.publishEvent(ChangeApplicationStatusEvent(ApplicationStatus.STOPPED, application))
     }
 }
