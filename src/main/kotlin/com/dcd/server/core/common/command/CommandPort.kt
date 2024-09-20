606a03cde486846bdf59c16c71be17ad1f933980
dolong2 <k01066624566@gmail.com>
2024-09-20 23:36:49 +0900

    [add] executeShellCommandWithResult 메서드 추가

--- src/main/kotlin/com/dcd/server/core/common/command/CommandPort.kt
+++ src/main/kotlin/com/dcd/server/core/common/command/CommandPort.kt
@@ -2,4 +2,5 @@ package com.dcd.server.core.common.command
 
 interface CommandPort {
     fun executeShellCommand(cmd: String): Int
+    fun executeShellCommandWithResult(cmd: String): List<String>
 }
