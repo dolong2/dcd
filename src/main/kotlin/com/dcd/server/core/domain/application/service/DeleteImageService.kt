737861c219360b0fc645f7d41b42181315485437
dolong2 <k01066624566@gmail.com>
2024-09-25 23:07:22 +0900

    [chore] DeleteImageService에 비동기 적용

--- src/main/kotlin/com/dcd/server/core/domain/application/service/DeleteImageService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/DeleteImageService.kt
@@ -3,5 +3,5 @@ package com.dcd.server.core.domain.application.service
 import com.dcd.server.core.domain.application.model.Application
 
 interface DeleteImageService {
-    fun deleteImage(application: Application)
+    suspend fun deleteImage(application: Application)
 }
--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DeleteImageServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/DeleteImageServiceImpl.kt
@@ -3,13 +3,17 @@ package com.dcd.server.core.domain.application.service.impl
 import com.dcd.server.core.common.command.CommandPort
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.service.DeleteImageService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Service
 
 @Service
 class DeleteImageServiceImpl(
     private val commandPort: CommandPort
 ) : DeleteImageService {
-    override fun deleteImage(application: Application) {
-        commandPort.executeShellCommand("docker rmi ${application.name.lowercase()}")
+    override suspend fun deleteImage(application: Application) {
+        withContext(Dispatchers.IO) {
+            commandPort.executeShellCommand("docker rmi ${application.name.lowercase()}")
+        }
     }
 }
