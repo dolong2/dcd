e745af9225344f9a6725befdb89fbc0c7c3e04b5
dolong2 <k01066624566@gmail.com>
2024-09-25 23:06:54 +0900

    [chore] DeleteContainerService에 비동기 적용

--- src/main/kotlin/com/dcd/server/core/domain/application/service/DeleteContainerService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/DeleteContainerService.kt
@@ -3,5 +3,5 @@ package com.dcd.server.core.domain.application.service
 import com.dcd.server.core.domain.application.model.Application
 
 interface DeleteContainerService {
-    fun deleteContainer(application: Application)
+    suspend fun deleteContainer(application: Application)
 }
