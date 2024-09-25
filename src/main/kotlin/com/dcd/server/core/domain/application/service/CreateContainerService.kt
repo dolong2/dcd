ba4fe5ebff713a8c3842194be572eb175ae99459
dolong2 <k01066624566@gmail.com>
2024-09-25 23:05:35 +0900

    [chore] CreateContainerService에 비동기 적용

--- src/main/kotlin/com/dcd/server/core/domain/application/service/CreateContainerService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/CreateContainerService.kt
@@ -3,5 +3,5 @@ package com.dcd.server.core.domain.application.service
 import com.dcd.server.core.domain.application.model.Application
 
 interface CreateContainerService {
-    fun createContainer(application: Application, externalPort: Int)
+    suspend fun createContainer(application: Application, externalPort: Int)
 }
