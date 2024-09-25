0e9f73d6d93a3b2522e015b0cc19b2371a3e975f
dolong2 <k01066624566@gmail.com>
2024-09-25 23:41:12 +0900

    [chore] ModifyGradleService에 비동기 적용

--- src/main/kotlin/com/dcd/server/core/domain/application/service/ModifyGradleService.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/ModifyGradleService.kt
@@ -3,6 +3,6 @@ package com.dcd.server.core.domain.application.service
 import com.dcd.server.core.domain.application.model.Application
 
 interface ModifyGradleService {
-    fun modifyGradleByApplicationId(id: String)
-    fun modifyGradleByApplication(application: Application)
+    suspend fun modifyGradleByApplicationId(id: String)
+    suspend fun modifyGradleByApplication(application: Application)
 }
