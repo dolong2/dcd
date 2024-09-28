b567816ea4bb7acf9d3fcea3edbe230a1afcba31
dolong2 <k01066624566@gmail.com>
2024-09-29 00:25:50 +0900

    [add] EnableWebSocket 어노테이션 추가

--- src/main/kotlin/com/dcd/server/ServerApplication.kt
+++ src/main/kotlin/com/dcd/server/ServerApplication.kt
@@ -4,7 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.socket.config.annotation.EnableWebSocket
 
+@EnableWebSocket
 @EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
