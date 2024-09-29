5ee32cfa1a799d25ae0b85ebc7ca5bbfbfa968c9
dolong2 <k01066624566@gmail.com>
2024-09-29 22:48:48 +0900

    [add] 웹소켓 예외 핸들링 로직 추가

--- src/main/kotlin/com/dcd/server/infrastructure/global/config/WebSocketConfig.kt
+++ src/main/kotlin/com/dcd/server/infrastructure/global/config/WebSocketConfig.kt
@@ -1,6 +1,8 @@
 package com.dcd.server.infrastructure.global.config
 
-import com.dcd.server.presentation.domain.application.data.ApplicationSocketHandler
+import com.dcd.server.infrastructure.global.filter.WebSocketFilter
+import com.dcd.server.infrastructure.global.jwt.adapter.ParseTokenAdapter
+import com.dcd.server.presentation.domain.application.ApplicationSocketHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
@@ -8,11 +10,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 class WebSocketConfig(
-    private val dockerWebSocketHandler: ApplicationSocketHandler
+    private val dockerWebSocketHandler: ApplicationSocketHandler,
+    private val parseTokenAdapter: ParseTokenAdapter
 ) : WebSocketConfigurer {
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(dockerWebSocketHandler, "/application/exec")
+            .addInterceptors(WebSocketFilter(parseTokenAdapter))
             .setAllowedOrigins("*")
     }
 }
