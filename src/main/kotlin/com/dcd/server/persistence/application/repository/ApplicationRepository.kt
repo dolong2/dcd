35f1ae9525c941a064d935133cf67e8ba5593ede
dolong2 <k01066624566@gmail.com>
2024-10-14 23:36:28 +0900

    [add] label을 포함하는 애플리케이션을 조회하는 쿼리메서드 추가

--- src/main/kotlin/com/dcd/server/persistence/application/repository/ApplicationRepository.kt
+++ src/main/kotlin/com/dcd/server/persistence/application/repository/ApplicationRepository.kt
@@ -4,9 +4,16 @@ import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
 import com.dcd.server.persistence.application.entity.ApplicationJpaEntity
 import com.dcd.server.persistence.workspace.entity.WorkspaceJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ApplicationRepository : JpaRepository<ApplicationJpaEntity, String> {
     fun findAllByWorkspace(workspace: WorkspaceJpaEntity): List<ApplicationJpaEntity>
+    @Query("select app from ApplicationJpaEntity app where app.workspace = :workspace and app.labels IN :labels")
+    fun findAllByWorkspaceAndLabels(
+        @Param("workspace") workspace: WorkspaceJpaEntity,
+        @Param("labels") labels: List<String>
+    ): List<ApplicationJpaEntity>
     fun existsByExternalPort(externalPort: Int): Boolean
     fun findAllByStatus(status: ApplicationStatus): List<ApplicationJpaEntity>
 }
