9878755bdf1ececc4319c94d2e961f3fa548b907
dolong2 <k01066624566@gmail.com>
2024-10-26 11:16:18 +0900

    [add] 라벨로 애플리케이션을 정지 시키는 메서드 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/StopApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/StopApplicationUseCase.kt
@@ -1,21 +1,24 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.application.exception.AlreadyStoppedException
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.model.enums.ApplicationStatus
 import com.dcd.server.core.domain.application.service.ChangeApplicationStatusService
 import com.dcd.server.core.domain.application.service.StopContainerService
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 
 @UseCase
 class StopApplicationUseCase(
     private val queryApplicationPort: QueryApplicationPort,
     private val stopContainerService: StopContainerService,
-    private val changeApplicationStatusService: ChangeApplicationStatusService
+    private val changeApplicationStatusService: ChangeApplicationStatusService,
+    private val workspaceInfo: WorkspaceInfo
 ) : CoroutineScope by CoroutineScope(Dispatchers.IO) {
     fun execute(id: String) {
         val application = (queryApplicationPort.findById(id)
@@ -29,5 +32,38 @@ class StopApplicationUseCase(
         }
 
         changeApplicationStatusService.changeApplicationStatus(application, ApplicationStatus.PENDING)
+    }
+
+    fun execute(labels: List<String>) {
+        val workspace = (workspaceInfo.workspace
+            ?: throw WorkspaceNotFoundException())
+
+        val applicationList = queryApplicationPort.findAllByWorkspace(workspace, labels)
+
+        val runChannel = Channel<Application>(capacity = Channel.UNLIMITED)
+        val job = SupervisorJob()
+        val scope = this + job
+        applicationList.forEach {
+            if (it.status != ApplicationStatus.RUNNING)
+                return@forEach
+
+            runChannel.trySend(it).isSuccess
+            changeApplicationStatusService.changeApplicationStatus(it, ApplicationStatus.PENDING)
+        }
+
+        // 코루틴을 생성하여 작업 처리
+        repeat(3) {
+            scope.launch {
+                for (application in runChannel) {
+                    stopContainerService.stopContainer(application)
+                }
+            }
+        }
+
+        // 작업 완료 후 코루틴 스코프 종료
+        scope.launch {
+            runChannel.close()
+            job.complete()
+        }
     }
 }
