8eed3214f8159478122d21338377cb416fa685b9
dolong2 <k01066624566@gmail.com>
2024-10-28 23:47:38 +0900

    [add] 라벨로 애플리케이션 라벨을 추가하는 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/AddApplicationEnvUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/AddApplicationEnvUseCase.kt
@@ -1,16 +1,19 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.application.dto.request.AddApplicationEnvReqDto
 import com.dcd.server.core.domain.application.exception.AlreadyExistsEnvException
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.spi.CommandApplicationPort
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 
 @UseCase
 class AddApplicationEnvUseCase(
     private val queryApplicationPort: QueryApplicationPort,
-    private val commandApplicationPort: CommandApplicationPort
+    private val commandApplicationPort: CommandApplicationPort,
+    private val workspaceInfo: WorkspaceInfo
 ) {
     fun execute(id: String, addApplicationEnvReqDto: AddApplicationEnvReqDto) {
         val application = (queryApplicationPort.findById(id)
@@ -22,5 +25,23 @@ class AddApplicationEnvUseCase(
             envMutable[it.key] = it.value
         }
         commandApplicationPort.save(application.copy(env = envMutable))
+    }
+
+    fun execute(labels: List<String>, addApplicationEnvReqDto: AddApplicationEnvReqDto) {
+        val workspace = workspaceInfo.workspace
+            ?: throw WorkspaceNotFoundException()
+        val applicationList = queryApplicationPort.findAllByWorkspace(workspace, labels)
+
+        val updatedApplicationList = applicationList.map { application ->
+            val envMutable = application.env.toMutableMap()
+            addApplicationEnvReqDto.envList.forEach {
+                if (envMutable.containsKey(it.key)) throw AlreadyExistsEnvException()
+
+                envMutable[it.key] = it.value
+            }
+            application.copy(env = envMutable)
+        }
+
+        commandApplicationPort.saveAll(updatedApplicationList)
     }
 }
