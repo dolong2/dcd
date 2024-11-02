0e6dd1a3736cf7b95f4d4fbbf456924a3b62661a
dolong2 <k01066624566@gmail.com>
2024-11-02 19:09:47 +0900

    [add] 라벨로 애플리케이션의 환경변수를 제거하는 메서드 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/DeleteApplicationEnvUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/DeleteApplicationEnvUseCase.kt
@@ -1,15 +1,18 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.application.exception.ApplicationEnvNotFoundException
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.spi.CommandApplicationPort
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 
 @UseCase
 class DeleteApplicationEnvUseCase(
     private val queryApplicationPort: QueryApplicationPort,
-    private val commandApplicationPort: CommandApplicationPort
+    private val commandApplicationPort: CommandApplicationPort,
+    private val workspaceInfo: WorkspaceInfo
 ) {
     fun execute(id: String, key: String) {
         val application = (queryApplicationPort.findById(id)
@@ -18,5 +21,23 @@ class DeleteApplicationEnvUseCase(
         updatedEnv.remove(key)
             ?: throw ApplicationEnvNotFoundException()
         commandApplicationPort.save(application.copy(env = updatedEnv))
+    }
+
+    fun execute(labels: List<String>, key: String) {
+        val workspace = (workspaceInfo.workspace
+            ?: throw WorkspaceNotFoundException())
+
+        val applicationList = queryApplicationPort.findAllByWorkspace(workspace, labels)
+        val updatedApplicationList = applicationList.mapNotNull { application ->
+            val env = application.env
+
+            val mutableEnv = env.toMutableMap()
+            mutableEnv.remove(key)
+                ?: return@mapNotNull null
+
+            return@mapNotNull application.copy(env = mutableEnv)
+        }
+
+        commandApplicationPort.saveAll(updatedApplicationList)
     }
 }
