0c8a77273ac638d5de9120011d671ad08ee64d58
dolong2 <k01066624566@gmail.com>
2024-11-25 23:30:55 +0900

    [chore] HTTP 설정 생성시 실패하면 에러를 발생하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/GenerateHttpConfigServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/GenerateHttpConfigServiceImpl.kt
@@ -2,6 +2,7 @@ package com.dcd.server.core.domain.application.service.impl
 
 import com.dcd.server.core.common.command.CommandPort
 import com.dcd.server.core.common.file.FileContent
+import com.dcd.server.core.domain.application.exception.HttpConfigFailureException
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.service.GenerateHttpConfigService
 import org.springframework.stereotype.Service
@@ -12,6 +13,16 @@ class GenerateHttpConfigServiceImpl(
 ) : GenerateHttpConfigService {
     override fun generateWebServerConfig(application: Application, domain: String) {
         val webServerConfig = FileContent.getApplicationHttpConfig(application, domain)
-        commandPort.executeShellCommand("touch ./nginx/conf/${application.name.replace(" ", "-")}-http.conf >> EOF ${webServerConfig} >> EOF")
+        val exitValue = commandPort.executeShellCommand(
+            "touch ./nginx/conf/${
+                application.name.replace(
+                    " ",
+                    "-"
+                )
+            }-http.conf >> EOF ${webServerConfig} >> EOF"
+        )
+
+        if (exitValue != 0)
+            throw HttpConfigFailureException()
     }
 }
