531d6cd92340ae4015dceb2da5c6472b4d61bc43
dolong2 <k01066624566@gmail.com>
2024-11-25 22:59:18 +0900

    [add] SetApplicationDomainUseCase 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/SetApplicationDomainUseCase.kt
@@ -0,0 +1,27 @@
+package com.dcd.server.core.domain.application.usecase
+
+import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.domain.application.dto.request.SetDomainReqDto
+import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
+import com.dcd.server.core.domain.application.service.GenerateHttpConfigService
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.user.service.GetCurrentUserService
+import com.dcd.server.core.domain.workspace.exception.WorkspaceOwnerNotSameException
+
+@UseCase
+class SetApplicationDomainUseCase(
+    private val queryApplicationPort: QueryApplicationPort,
+    private val getCurrentUserService: GetCurrentUserService,
+    private val generateHttpConfigService: GenerateHttpConfigService
+) {
+    fun execute(applicationId: String, setDomainReqDto: SetDomainReqDto) {
+        val application = (queryApplicationPort.findById(applicationId)
+            ?: throw ApplicationNotFoundException())
+
+        val currentUser = getCurrentUserService.getCurrentUser()
+        if (currentUser.id != application.workspace.owner.id)
+            throw WorkspaceOwnerNotSameException()
+
+        generateHttpConfigService.generateWebServerConfig(application, setDomainReqDto.domain)
+    }
+}
