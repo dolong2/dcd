bbacaee235046502dbef077885f62bf6cd94e4f1
dolong2 <k01066624566@gmail.com>
2024-11-26 23:41:38 +0900

    [add] Nginx컨테이너 재실행 서비스 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/RebootNginxContainerServiceImpl.kt
@@ -0,0 +1,19 @@
+package com.dcd.server.core.domain.application.service.impl
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.service.RebootNginxService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class RebootNginxContainerServiceImpl(
+    private val commandPort: CommandPort
+) : RebootNginxService {
+    private val log = LoggerFactory.getLogger(this::class.simpleName)
+
+    override fun rebootNginx() {
+        val exitValue = commandPort.executeShellCommand("docker restart dcd-nginx")
+        if (exitValue != 0)
+            log.error("nginx restart failure")
+    }
+}
