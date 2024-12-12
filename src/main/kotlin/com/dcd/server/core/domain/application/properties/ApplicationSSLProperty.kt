a70c90ccbffa5a3a9f7d60915ba6367d983124d8
dolong2 <k01066624566@gmail.com>
2024-12-12 23:15:36 +0900

    [del] ApplicationSSLProperty 제거

--- src/main/kotlin/com/dcd/server/core/domain/application/properties/ApplicationSSLProperty.kt
+++
@@ -1,9 +0,0 @@
-package com.dcd.server.core.domain.application.properties
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties("application.ssl")
-class ApplicationSSLProperty(
-    val directory: String,
-    val password: String
-)
