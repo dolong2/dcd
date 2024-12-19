1cca93eb829a3ce4ffe0f0290ed50643ec8aadc0
dolong2 <k01066624566@gmail.com>
2024-12-19 23:17:11 +0900

    [chore] 이메일 인증 코드 검증 로직 수정

--- src/main/kotlin/com/dcd/server/core/domain/auth/service/impl/VerifyEmailAuthServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/auth/service/impl/VerifyEmailAuthServiceImpl.kt
@@ -2,6 +2,7 @@ package com.dcd.server.core.domain.auth.service.impl
 
 import com.dcd.server.core.domain.auth.exception.ExpiredCodeException
 import com.dcd.server.core.domain.auth.exception.InvalidAuthCodeException
+import com.dcd.server.core.domain.auth.exception.NotFoundAuthCodeException
 import com.dcd.server.core.domain.auth.service.VerifyEmailAuthService
 import com.dcd.server.core.domain.auth.spi.CommandEmailAuthPort
 import com.dcd.server.core.domain.auth.spi.QueryEmailAuthPort
@@ -16,6 +17,8 @@ class VerifyEmailAuthServiceImpl(
         if (!queryEmailAuthPort.existsByCodeAndEmail(email, code))
             if (queryEmailAuthPort.existsByEmail(email))
                 throw InvalidAuthCodeException()
+            else
+                throw NotFoundAuthCodeException()
         val emailAuth = (queryEmailAuthPort.findByCode(code)
             ?: throw ExpiredCodeException())
         commandEmailAuthPort.save(emailAuth.copy(certificate = true))
