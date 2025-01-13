100ba273711967596e81993cce529444963f6048
dolong2 <k01066624566@gmail.com>
2025-01-13 23:49:23 +0900

    [chore] 기존 테스트를 SpringBootTest를 사용하도록 수정

--- src/test/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCaseTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCaseTest.kt
@@ -3,19 +3,29 @@ package com.dcd.server.core.domain.application.usecase
 import com.dcd.server.core.domain.application.dto.extenstion.toDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationListResDto
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.user.spi.QueryUserPort
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
+import com.dcd.server.infrastructure.global.security.auth.AuthDetailsService
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import com.dcd.server.infrastructure.test.application.ApplicationGenerator
-import com.dcd.server.infrastructure.test.user.UserGenerator
-import com.dcd.server.infrastructure.test.workspace.WorkspaceGenerator
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
 
-class GetAllApplicationUseCaseTest : BehaviorSpec({
-    val queryApplicationPort = mockk<QueryApplicationPort>()
-    val queryWorkspacePort = mockk<QueryWorkspacePort>()
-    val getAllApplicationUseCase = GetAllApplicationUseCase(queryApplicationPort, queryWorkspacePort)
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+class GetAllApplicationUseCaseTest(
+    private val getAllApplicationUseCase: GetAllApplicationUseCase,
+    private val authDetailsService: AuthDetailsService,
+    private val queryWorkspacePort: QueryWorkspacePort,
+    private val queryUserPort: QueryUserPort,
+    private val queryApplicationPort: QueryApplicationPort
+) : BehaviorSpec({
+    val userId = "user1"
+
 
     given("applicationList가 주어지고") {
         val user = UserGenerator.generateUser()
