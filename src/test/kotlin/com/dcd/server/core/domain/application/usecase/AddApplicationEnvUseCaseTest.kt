32e5adae70d104efdd0ed1ad3f05a431b273745f
dolong2 <k01066624566@gmail.com>
2025-01-16 23:44:39 +0900

    [chore] 기존 테스트에서 스프링 부트 테스트를 사용하도록 수정

--- src/test/kotlin/com/dcd/server/core/domain/application/usecase/AddApplicationEnvUseCaseTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/AddApplicationEnvUseCaseTest.kt
@@ -1,24 +1,37 @@
 package com.dcd.server.core.domain.application.usecase
 
-import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.application.dto.request.AddApplicationEnvReqDto
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
-import com.dcd.server.core.domain.application.spi.CommandApplicationPort
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.user.spi.QueryUserPort
+import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
-import com.dcd.server.infrastructure.test.application.ApplicationGenerator
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
 
-class AddApplicationEnvUseCaseTest : BehaviorSpec({
-    val queryApplicationPort = mockk<QueryApplicationPort>()
-    val commandApplicationPort = mockk<CommandApplicationPort>()
-    val workspaceInfo = WorkspaceInfo()
-    val addApplicationEnvUseCase = AddApplicationEnvUseCase(queryApplicationPort, commandApplicationPort, workspaceInfo)
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+class AddApplicationEnvUseCaseTest(
+    private val addApplicationEnvUseCase: AddApplicationEnvUseCase,
+    private val queryUserPort: QueryUserPort,
+    private val queryWorkspacePort: QueryWorkspacePort,
+    private val queryApplicationPort: QueryApplicationPort
+) : BehaviorSpec({
+    var targetApplicationId = ""
+    beforeSpec {
+        val targetUser = queryUserPort.findById("user1")!!
 
     given("request가 주어지고") {
+        val workspace = queryWorkspacePort.findByUser(targetUser).first()
+        val application = queryApplicationPort.findAllByWorkspace(workspace).first()
+        targetApplicationId = application.id
+    }
+
         val request = AddApplicationEnvReqDto(
             envList = mapOf(Pair("testA", "testB"))
         )
