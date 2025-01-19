e5b533317d5b0f6fa0472d48c2c178bbb4ddfd25
dolong2 <k01066624566@gmail.com>
2025-01-19 21:24:16 +0900

    [chore] 환경변수가 수정되는지 검증하는 테스트 케이스 수정

--- src/test/kotlin/com/dcd/server/core/domain/application/usecase/UpdateApplicationEnvUseCaseTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/application/usecase/UpdateApplicationEnvUseCaseTest.kt
@@ -40,52 +40,17 @@ class UpdateApplicationEnvUseCaseTest(
     }
 
     given("애플리케이션 아이디와 수정할 환경변수값이 주어지고") {
-        val applicationId = "testId"
-        val envKey = "testKey"
         val request = UpdateApplicationEnvReqDto(newValue = "newValue")
 
         `when`("해당 애플리케이션이 존재할때") {
-            val env = mapOf(Pair(envKey, "testValue"))
-            val application = ApplicationGenerator.generateApplication(
-                id = applicationId,
-                env = env
-            )
-            every { queryApplicationPort.findById(applicationId) } returns application
 
-            updateApplicationEnvUseCase.execute(applicationId, envKey, request)
+            updateApplicationEnvUseCase.execute(applicationId, key, request)
 
             then("애플리케이션의 env를 변경해서 저장해야함") {
-                val updatedEnv = env.toMutableMap()
-                updatedEnv[envKey] = request.newValue
-                val updatedApplication = application.copy(env = updatedEnv)
+                val application = queryApplicationPort.findById(applicationId)
 
-                updatedApplication.env[envKey] shouldBe request.newValue
-                verify { commandApplicationPort.save(updatedApplication) }
-            }
-        }
-
-        `when`("해당 애플리케이션이 존재하지 않을때") {
-            every { queryApplicationPort.findById(applicationId) } returns null
-
-            then("ApplicationNotFoundException이 발생해야함") {
-                shouldThrow<ApplicationNotFoundException> {
-                    updateApplicationEnvUseCase.execute(applicationId, envKey, request)
-                }
-            }
-        }
-
-        `when`("해당 키를 가지는 환경변수가 존재하지 않을때") {
-            val env = mapOf(Pair("NotTestKey", "value"))
-            val application = ApplicationGenerator.generateApplication(
-                id = applicationId,
-                env = env
-            )
-            every { queryApplicationPort.findById(applicationId) } returns application
-
-            then("ApplicationEnvNotFoundException가 발생해야함") {
-                shouldThrow<ApplicationEnvNotFoundException> {
-                    updateApplicationEnvUseCase.execute(applicationId, envKey, request)
-                }
+                application shouldNotBe null
+                application!!.env[key] shouldBe request.newValue
             }
         }
     }
