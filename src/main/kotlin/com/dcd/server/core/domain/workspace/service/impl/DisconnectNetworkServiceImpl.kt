209bded3029ad4644d696acca68f03a7a5055a9d
dolong2 <k01066624566@gmail.com>
2025-02-04 23:20:01 +0900

    [chore] DisconnectNetworkService의 구현을 workspace를 가진 애플리케이션의 네트워크 연결을 끊도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/workspace/service/impl/DisconnectNetworkServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/service/impl/DisconnectNetworkServiceImpl.kt
@@ -1,18 +1,20 @@
 package com.dcd.server.core.domain.workspace.service.impl
 
 import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.workspace.model.Workspace
 import com.dcd.server.core.domain.workspace.service.DisconnectNetworkService
 import org.springframework.stereotype.Service
 
 @Service
 class DisconnectNetworkServiceImpl(
-    private val commandPort: CommandPort
+    private val commandPort: CommandPort,
+    private val queryApplicationPort: QueryApplicationPort
 ) : DisconnectNetworkService {
-    override fun disconnectNetwork(networkName: String) {
-        commandPort.executeShellCommand(
-            "for container in \$(docker network inspect $networkName -f '{{range .Containers}}{{.Name}} {{end}}'); do" +
-                " docker network disconnect $networkName \$container;" +
-                " done"
-        )
+    override fun disconnectNetwork(workspace: Workspace) {
+        val applicationList = queryApplicationPort.findAllByWorkspace(workspace)
+        applicationList.forEach {
+            commandPort.executeShellCommand("docker network disconnect ${workspace.networkName} ${it.containerName}")
+        }
     }
 }
