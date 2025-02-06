0cf8b3a60c7728f6b30038181c069783c19780fd
dolong2 <k01066624566@gmail.com>
2025-02-06 23:46:31 +0900

    [del] ModifyGradleService 제거

--- src/main/kotlin/com/dcd/server/core/domain/application/service/ModifyGradleService.kt
+++
@@ -1,8 +0,0 @@
-package com.dcd.server.core.domain.application.service
-
-import com.dcd.server.core.domain.application.model.Application
-
-interface ModifyGradleService {
-    suspend fun modifyGradleByApplicationId(id: String)
-    suspend fun modifyGradleByApplication(application: Application)
-}
