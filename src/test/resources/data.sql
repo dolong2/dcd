43a97646eb10200055cf4743c9fb38f064f85fb4
dolong2 <k01066624566@gmail.com>
2025-02-19 23:48:16 +0900

    [chore] 문법에 맞게 sql 구문 수정

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,33 +1,33 @@
 -- 테이블 생성
-drop table if exists application_entity cascade
-drop table if exists application_env_entity cascade
-drop table if exists application_label_entity cascade
-drop table if exists global_env_entity cascade
-drop table if exists role_entity cascade
-drop table if exists user_entity cascade
-drop table if exists workspace_entity cascade
-create table application_entity (external_port integer not null, port integer not null, application_type varchar(255) check (application_type in ('SPRING_BOOT','NEST_JS','MYSQL','MARIA_DB','REDIS')), description varchar(255), failure_reason varchar(255), github_url varchar(255), id varchar(255) not null, name varchar(255), status varchar(255) check (status in ('CREATED','PENDING','RUNNING','STOPPED','FAILURE')), version varchar(255), workspace_id varchar(255), primary key (id))
-create table application_env_entity (application_id varchar(255) not null, env_key varchar(255) not null, env_value varchar(255), primary key (application_id, env_key))
-create table application_label_entity (application_id varchar(255) not null, label varchar(255))
-create table global_env_entity (env_key varchar(255) not null, env_value varchar(255), workspace_id varchar(255) not null, primary key (env_key, workspace_id))
-create table role_entity (roles varchar(255) check (roles in ('ROLE_ADMIN','ROLE_DEVELOPER','ROLE_USER')), user_id varchar(255) not null)
-create table user_entity (email varchar(255), id varchar(255) not null, name varchar(255), password varchar(255), status varchar(255) check (status in ('PENDING','CREATED')), primary key (id))
-create table workspace_entity (description varchar(255), id varchar(255) not null, owner_id varchar(255), title varchar(255), primary key (id))
-alter table if exists application_entity add constraint FKn9drxkrx2h6wlorxfy00mr45h foreign key (workspace_id) references workspace_entity
-alter table if exists application_env_entity add constraint FKb8anxni720qr9mk9neafwm7v foreign key (application_id) references application_entity
-alter table if exists application_label_entity add constraint FKq6iovxq5tdx2i1lwrx34kg0b9 foreign key (application_id) references application_entity
-alter table if exists global_env_entity add constraint FK694b15y5r7k158yn9c2rnftte foreign key (workspace_id) references workspace_entity
-alter table if exists role_entity add constraint FKrot6fehcor0f3sux5s6kgl0a4 foreign key (user_id) references user_entity
-alter table if exists workspace_entity add constraint FKlfxk1bhw5knckt8vv28xvx5g2 foreign key (owner_id) references user_entity
+drop table if exists application_entity cascade;
+drop table if exists application_env_entity cascade;
+drop table if exists application_label_entity cascade;
+drop table if exists global_env_entity cascade;
+drop table if exists role_entity cascade;
+drop table if exists user_entity cascade;
+drop table if exists workspace_entity cascade;
+create table application_entity (external_port integer not null, port integer not null, application_type varchar(255) check (application_type in ('SPRING_BOOT','NEST_JS','MYSQL','MARIA_DB','REDIS')), description varchar(255), failure_reason varchar(255), github_url varchar(255), id varchar(255) not null, name varchar(255), status varchar(255) check (status in ('CREATED','PENDING','RUNNING','STOPPED','FAILURE')), version varchar(255), workspace_id varchar(255), primary key (id));
+create table application_env_entity (application_id varchar(255) not null, env_key varchar(255) not null, env_value varchar(255), primary key (application_id, env_key));
+create table application_label_entity (application_id varchar(255) not null, label varchar(255));
+create table global_env_entity (env_key varchar(255) not null, env_value varchar(255), workspace_id varchar(255) not null, primary key (env_key, workspace_id));
+create table role_entity (roles varchar(255) check (roles in ('ROLE_ADMIN','ROLE_DEVELOPER','ROLE_USER')), user_id varchar(255) not null);
+create table user_entity (email varchar(255), id varchar(255) not null, name varchar(255), password varchar(255), status varchar(255) check (status in ('PENDING','CREATED')), primary key (id));
+create table workspace_entity (description varchar(255), id varchar(255) not null, owner_id varchar(255), title varchar(255), primary key (id));
+alter table if exists application_entity add constraint FKn9drxkrx2h6wlorxfy00mr45h foreign key (workspace_id) references workspace_entity;
+alter table if exists application_env_entity add constraint FKb8anxni720qr9mk9neafwm7v foreign key (application_id) references application_entity;
+alter table if exists application_label_entity add constraint FKq6iovxq5tdx2i1lwrx34kg0b9 foreign key (application_id) references application_entity;
+alter table if exists global_env_entity add constraint FK694b15y5r7k158yn9c2rnftte foreign key (workspace_id) references workspace_entity;
+alter table if exists role_entity add constraint FKrot6fehcor0f3sux5s6kgl0a4 foreign key (user_id) references user_entity;
+alter table if exists workspace_entity add constraint FKlfxk1bhw5knckt8vv28xvx5g2 foreign key (owner_id) references user_entity;
 
 -- 유저 생성
-    insert into user_entity (email,name,password,status,id) values ('ownerEmail','applicationOwner' 'encoded_testPassword', 'CREATED','user1');
+insert into user_entity (email,name,password,status,id) values ('ownerEmail','applicationOwner', 'encoded_testPassword', 'CREATED','user1');
 insert into role_entity (user_id,roles) values ('user1', 'ROLE_USER');
-insert into user_entity (email,name,password,status,id) values ('testEmail','applicationOwner' 'encoded_testPassword', 'CREATED','user2');
+insert into user_entity (email,name,password,status,id) values ('testEmail','applicationOwner', 'encoded_testPassword', 'CREATED','user2');
 insert into role_entity (user_id,roles) values ('user2', 'ROLE_USER');
 
 -- 워크스페이스 생성
 insert into workspace_entity (description,owner_id,title,id) values ('testDescription', 'user1', 'testTitle', 'workspace1');
 
 -- 애플리케이션 생성
-insert into application_entity (application_type,description,external_port,failure_reason,github_url,name,port,status,version,workspace_id,id) values ('SPRING_BOOT','testDescription', 8080, NULL, 'testUrl', 'testName', 8080, 'STOPPED','17','workspace1','testId')
+insert into application_entity (application_type,description,external_port,failure_reason,github_url,name,port,status,version,workspace_id,id) values ('SPRING_BOOT','testDescription', 8080, NULL, 'testUrl', 'testName', 8080, 'STOPPED','17','workspace1','testId');
