08f6b72cb0abe178a0eb002079b564d0ef426a4b
dolong2 <k01066624566@gmail.com>
2025-02-21 23:38:28 +0900

    [del] 커맨드 실행 요청 객체에서 정규식 검증 제거

--- src/main/kotlin/com/dcd/server/presentation/domain/application/data/request/ExecuteCommandRequest.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/data/request/ExecuteCommandRequest.kt
@@ -1,10 +1,8 @@
 package com.dcd.server.presentation.domain.application.data.request
 
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Pattern
 
 data class ExecuteCommandRequest(
     @field:NotBlank
-    @field:Pattern(regexp = "^(?:(?!(;|\\|\\||&&)|rm\\s+-rf\\s+\\/|(wget|curl)\\s+.*\\|\\s*(sh|bash|zsh|ksh)|cat\\s+/etc/passwd|(cat|grep|awk|sed)\\s+/.*ssh/.*(id_rsa|authorized_keys|known_hosts)).)*$")
     val command: String
 )
