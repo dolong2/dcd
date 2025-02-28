55ab818328f6339c1962255554c6282caaaba21d
dolong2 <k01066624566@gmail.com>
2025-03-01 00:26:07 +0900

    [add] 애플리케이션 타입 조회 엔드포인트 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/application/ApplicationStaticWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/ApplicationStaticWebAdapter.kt
@@ -1,9 +1,11 @@
 package com.dcd.server.presentation.domain.application
 
 import com.dcd.server.core.domain.application.model.enums.ApplicationType
+import com.dcd.server.core.domain.application.usecase.GetApplicationTypeUseCase
 import com.dcd.server.core.domain.application.usecase.GetAvailableVersionUseCase
 import com.dcd.server.presentation.common.annotation.WebAdapter
 import com.dcd.server.presentation.domain.application.data.exetension.toResponse
+import com.dcd.server.presentation.domain.application.data.response.ApplicationTypeListResponse
 import com.dcd.server.presentation.domain.application.data.response.AvailableVersionResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable
 
 @WebAdapter("/application")
 class ApplicationStaticWebAdapter(
-    private val getAvailableVersionUseCase: GetAvailableVersionUseCase
+    private val getAvailableVersionUseCase: GetAvailableVersionUseCase,
+    private val getApplicationTypeUseCase: GetApplicationTypeUseCase
 ) {
     @GetMapping("/{applicationType}/version")
     fun getAvailableVersion(@PathVariable applicationType: ApplicationType): ResponseEntity<AvailableVersionResponse> =
         getAvailableVersionUseCase.execute(applicationType)
+            .let { ResponseEntity.ok(it.toResponse()) }
+
+    @GetMapping("/types")
+    fun getApplicationTypes(): ResponseEntity<ApplicationTypeListResponse> =
+        getApplicationTypeUseCase.execute()
             .let { ResponseEntity.ok(it.toResponse()) }
 }
