f0dce858db09c7392d0090d2675b4ec5385adbbf
dolong2 <k01066624566@gmail.com>
2025-03-06 23:39:47 +0900

    [chore] 애플리케이션 전체 조회 유스케이스에서 WorkspaceInfo를 통해 워크스페이스를 조회하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetAllApplicationUseCase.kt
@@ -1,20 +1,20 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.ReadOnlyUseCase
+import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.application.dto.extenstion.toDto
 import com.dcd.server.core.domain.application.dto.response.ApplicationListResDto
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
-import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 
 @ReadOnlyUseCase
 class GetAllApplicationUseCase(
     private val queryApplicationPort: QueryApplicationPort,
-    private val queryWorkspacePort: QueryWorkspacePort
+    private val workspaceInfo: WorkspaceInfo
 ) {
     fun execute(workspaceId: String, labels: List<String>?): ApplicationListResDto {
-        val workspace = (queryWorkspacePort.findById(workspaceId)
-            ?: throw WorkspaceNotFoundException())
+        val workspace = workspaceInfo.workspace
+            ?: throw WorkspaceNotFoundException()
 
         return ApplicationListResDto(
             queryApplicationPort
