a59e721ba35031540f2c814ed5d91d6806d169e8
dolong2 <k01066624566@gmail.com>
2025-03-12 23:45:09 +0900

    [chore] ErrorResponse 임포트 경로 수정

--- src/main/kotlin/com/dcd/server/infrastructure/global/filter/ExceptionFilter.kt
+++ src/main/kotlin/com/dcd/server/infrastructure/global/filter/ExceptionFilter.kt
@@ -2,8 +2,7 @@ package com.dcd.server.infrastructure.global.filter
 
 import com.dcd.server.core.common.error.BasicException
 import com.dcd.server.core.common.error.ErrorCode
-import com.dcd.server.infrastructure.global.error.response.ErrorResponse
-import com.dcd.server.presentation.domain.application.exception.InvalidConnectionInfoException
+import com.dcd.server.presentation.common.error.response.ErrorResponse
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletException
@@ -11,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.web.filter.OncePerRequestFilter
-import org.springframework.web.socket.server.HandshakeFailureException
 
 class ExceptionFilter(
     private val objectMapper: ObjectMapper
