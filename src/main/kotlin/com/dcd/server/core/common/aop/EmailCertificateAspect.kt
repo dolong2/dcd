4c9f52ef93ae70b37178c43dc125f710e7f33030
dolong2 <k01066624566@gmail.com>
2025-03-19 23:46:59 +0900

    [del] 이메일 확인 어드바이스에서 유저 존재여부 확인 로직 제거

--- src/main/kotlin/com/dcd/server/core/common/aop/EmailCertificateAspect.kt
+++ src/main/kotlin/com/dcd/server/core/common/aop/EmailCertificateAspect.kt
@@ -6,10 +6,8 @@ import com.dcd.server.core.common.aop.exception.NotCertificateEmailException
 import com.dcd.server.core.common.aop.util.CustomExpressionParser
 import com.dcd.server.core.common.error.BasicException
 import com.dcd.server.core.common.error.ErrorCode
-import com.dcd.server.core.domain.auth.exception.UserNotFoundException
 import com.dcd.server.core.domain.auth.spi.CommandEmailAuthPort
 import com.dcd.server.core.domain.auth.spi.QueryEmailAuthPort
-import com.dcd.server.core.domain.user.spi.QueryUserPort
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
@@ -22,8 +20,7 @@ import kotlin.reflect.full.memberProperties
 @Component
 class EmailCertificateAspect(
     private val queryEmailAuthPort: QueryEmailAuthPort,
-    private val commandEmailAuthPort: CommandEmailAuthPort,
-    private val queryUserPort: QueryUserPort
+    private val commandEmailAuthPort: CommandEmailAuthPort
 ) {
     @Pointcut("@annotation(com.dcd.server.core.common.annotation.CheckEmailCertificate)")
     fun checkEmailCertificatePointcut() {}
@@ -41,8 +38,6 @@ class EmailCertificateAspect(
         val email = getFieldValue(parameterObject, "email")
             ?: throw InvalidParsingObjectFieldException()
 
-        if (queryUserPort.existsByEmail(email).not())
-            throw UserNotFoundException()
         val emailAuthList = queryEmailAuthPort.findByEmail(email)
             .filter { it.certificate }
         if (emailAuthList.isEmpty())
