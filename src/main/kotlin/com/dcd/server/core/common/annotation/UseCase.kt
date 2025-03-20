d1902bd8ba5997de7507c24ac796f4100899cc82
dolong2 <k01066624566@gmail.com>
2025-03-20 23:38:26 +0900

    [add] UseCase에 rollbackFor, noRollbackFor 필드 추가

--- src/main/kotlin/com/dcd/server/core/common/annotation/UseCase.kt
+++ src/main/kotlin/com/dcd/server/core/common/annotation/UseCase.kt
@@ -1,8 +1,16 @@
 package com.dcd.server.core.common.annotation
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import kotlin.reflect.KClass
 
 @Component
 @Transactional(rollbackFor = [Exception::class])
-annotation class UseCase()
+annotation class UseCase(
+    @get:AliasFor(annotation = Transactional::class, attribute = "rollbackFor")
+    val rollbackFor: Array<KClass<out Throwable>> = [Exception::class],
+
+    @get:AliasFor(annotation = Transactional::class, attribute = "noRollbackFor")
+    val noRollbackFor: Array<KClass<out Throwable>> = []
+)
