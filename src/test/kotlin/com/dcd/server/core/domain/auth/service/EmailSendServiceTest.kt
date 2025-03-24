779213ace35d73d882ed2b77c50a97de429655c3
dolong2 <k01066624566@gmail.com>
2025-03-24 23:09:45 +0900

    [chore] 테스트 수정

--- src/test/kotlin/com/dcd/server/core/domain/auth/service/EmailSendServiceTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/auth/service/EmailSendServiceTest.kt
@@ -1,5 +1,6 @@
 package com.dcd.server.core.domain.auth.service
 
+import com.dcd.server.core.domain.auth.model.enums.EmailAuthUsage
 import com.dcd.server.core.domain.auth.service.impl.EmailSendServiceImpl
 import com.dcd.server.core.domain.auth.spi.CommandEmailAuthPort
 import io.kotest.core.spec.style.BehaviorSpec
@@ -17,7 +18,7 @@ class EmailSendServiceTest : BehaviorSpec({
     given("이메일이 주어지고") {
         val testEmail = "testEmail"
         `when`("이메일을 보낼때") {
-            emailSendService.sendEmail(testEmail)
+            emailSendService.sendEmail(testEmail, EmailAuthUsage.SIGNUP)
             every { commandEmailAuthPort.save(any()) } returns Unit
             then("메일을 보내고 mailAuth를 save해야함") {
                 verify { mailSender.send(any<MimeMessage>()) }
