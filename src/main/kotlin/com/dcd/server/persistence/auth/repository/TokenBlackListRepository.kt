95a6f75d5434be24ba3276af1794397935241183
dolong2 <k01066624566@gmail.com>
2025-04-21 23:34:30 +0900

    [del] 사용되지 않는 쿼리 메서드 제거

--- src/main/kotlin/com/dcd/server/persistence/auth/repository/TokenBlackListRepository.kt
+++ src/main/kotlin/com/dcd/server/persistence/auth/repository/TokenBlackListRepository.kt
@@ -4,5 +4,4 @@ import com.dcd.server.persistence.auth.entity.TokenBlackListEntity
 import org.springframework.data.repository.CrudRepository
 
 interface TokenBlackListRepository : CrudRepository<TokenBlackListEntity, String> {
-    fun existsByToken(token: String): Boolean
 }
