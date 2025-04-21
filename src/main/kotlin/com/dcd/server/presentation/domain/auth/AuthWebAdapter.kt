46bb52c380cfff95631834102059024cf4f35a9c
dolong2 <k01066624566@gmail.com>
2025-04-21 23:34:00 +0900

    [del] presentation 계층에서 사용되지 않는 임포트 제거

--- src/main/kotlin/com/dcd/server/presentation/domain/auth/AuthWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/auth/AuthWebAdapter.kt
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @WebAdapter("/auth")
 class AuthWebAdapter(
