fcbf7c53ef38b1fd1b3a0cfb428d3d8050631d89
dolong2 <k01066624566@gmail.com>
2025-04-21 23:34:45 +0900

    [del] persistence 계층에서 사용되지 않는 임포트 제거

--- src/main/kotlin/com/dcd/server/persistence/auth/entity/RefreshTokenEntity.kt
+++ src/main/kotlin/com/dcd/server/persistence/auth/entity/RefreshTokenEntity.kt
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
-import java.util.concurrent.TimeUnit
 
 @RedisHash(value = "RefreshToken")
 class RefreshTokenEntity(
