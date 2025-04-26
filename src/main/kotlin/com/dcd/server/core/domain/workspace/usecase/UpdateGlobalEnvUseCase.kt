0558870ebbc8bccc5e83b0d8e7df2daba08f0100
dolong2 <k01066624566@gmail.com>
2025-04-26 23:57:46 +0900

    [del] 디버깅 코드 제거

--- src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/UpdateGlobalEnvUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/UpdateGlobalEnvUseCase.kt
@@ -9,15 +9,13 @@ import com.dcd.server.core.domain.workspace.exception.GlobalEnvNotFoundException
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 import com.dcd.server.core.domain.workspace.service.ValidateWorkspaceOwnerService
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
-import com.dcd.server.persistence.env.repository.GlobalEnvRepository
 
 @UseCase
 class UpdateGlobalEnvUseCase(
     private val queryWorkspacePort: QueryWorkspacePort,
     private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService,
     private val commandGlobalEnvPort: CommandGlobalEnvPort,
-    private val queryGlobalEnvPort: QueryGlobalEnvPort,
-    private val globalEnvRepository: GlobalEnvRepository
+    private val queryGlobalEnvPort: QueryGlobalEnvPort
 ) {
     @Lock("#workspaceId+#envKey")
     fun execute(workspaceId: String, envKey: String, updateGlobalEnvReqDto: UpdateGlobalEnvReqDto) {
@@ -25,11 +23,6 @@ class UpdateGlobalEnvUseCase(
             ?: throw WorkspaceNotFoundException())
 
         validateWorkspaceOwnerService.validateOwner(workspace)
-
-        val findAll = globalEnvRepository.findAll()
-        findAll.forEach {
-            println("it = ${it.key} ${it.value}")
-        }
 
         val globalEnv = (queryGlobalEnvPort.findByKeyAndWorkspace(envKey, workspace)
             ?: throw GlobalEnvNotFoundException())
