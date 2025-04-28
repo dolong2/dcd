2941b6a9cc5287e80a64069c04e4b18836f878f1
dolong2 <k01066624566@gmail.com>
2025-04-28 23:56:56 +0900

    [add] EncryptServiceImpl 구현 내용 추가

--- src/main/kotlin/com/dcd/server/core/common/service/impl/EncryptServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/common/service/impl/EncryptServiceImpl.kt
@@ -1,17 +1,16 @@
 package com.dcd.server.core.common.service.impl
 
 import com.dcd.server.core.common.service.EncryptService
+import com.dcd.server.core.common.spi.EncryptPort
 import org.springframework.stereotype.Service
 
 @Service
 class EncryptServiceImpl(
-
+    private val encryptPort: EncryptPort
 ) : EncryptService {
-    override fun encryptData(rawData: String): String {
-        TODO("Not yet implemented")
-    }
+    override fun encryptData(rawData: String): String =
+        encryptPort.encrypt(rawData)
 
-    override fun decryptData(encodedData: String): String {
-        TODO("Not yet implemented")
-    }
+    override fun decryptData(encodedData: String): String =
+        encryptPort.decrypt(encodedData)
 }
