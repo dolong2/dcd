813c4d99be3c43d8bf011568accd4a7c82c17c2d
dolong2 <k01066624566@gmail.com>
2025-05-03 21:14:12 +0900

    [add] 워크스페이스 환경변수 주입시 암호화가 정상적으로 적용되는지 검증하는 테스트 케이스 추가

--- src/test/kotlin/com/dcd/server/core/domain/workspace/usecase/PutGlobalEnvUseCaseTest.kt
+++ src/test/kotlin/com/dcd/server/core/domain/workspace/usecase/PutGlobalEnvUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.dcd.server.core.domain.workspace.usecase
 
+import com.dcd.server.core.common.spi.EncryptPort
 import com.dcd.server.core.domain.env.dto.request.PutEnvReqDto
 import com.dcd.server.core.domain.env.spi.QueryGlobalEnvPort
 import com.dcd.server.core.domain.user.spi.CommandUserPort
@@ -33,7 +34,8 @@ class PutGlobalEnvUseCaseTest(
     private val queryWorkspacePort: QueryWorkspacePort,
     private val commandWorkspacePort: CommandWorkspacePort,
     private val commandUserPort: CommandUserPort,
-    private val queryGlobalEnvPort: QueryGlobalEnvPort
+    private val queryGlobalEnvPort: QueryGlobalEnvPort,
+    private val encryptPort: EncryptPort
 ) : BehaviorSpec({
     val userId = "1e1973eb-3fb9-47ac-9342-c16cd63ffc6f"
     val targetWorkspaceId = "d57b42f5-5cc4-440b-8dce-b4fc2e372eff"
@@ -89,6 +91,29 @@ class PutGlobalEnvUseCaseTest(
                 shouldThrow<WorkspaceNotFoundException> {
                     putGlobalEnvUseCase.execute(givenWorkspaceId, putGlobalEnvReqDto)
                 }
+            }
+        }
+    }
+
+    given("암호화된 환경변수 등록 요청이 주어지고") {
+        val envKey = "testA"
+        val envValue = "testB"
+        val request = PutGlobalEnvReqDto(
+            envList = listOf(PutEnvReqDto(envKey, envValue, true))
+        )
+
+        `when`("usecase를 실행하면") {
+            putGlobalEnvUseCase.execute(targetWorkspaceId, request)
+
+            then("암호화된 value가 저장되어야함") {
+                val targetWorkspace = queryWorkspacePort.findById(targetWorkspaceId)!!
+                val env = queryGlobalEnvPort.findByKeyAndWorkspace(
+                    key = envKey,
+                    workspace = targetWorkspace
+                )!!
+
+                env.value shouldNotBe envValue
+                encryptPort.decrypt(env.value) shouldBe envValue
             }
         }
     }
