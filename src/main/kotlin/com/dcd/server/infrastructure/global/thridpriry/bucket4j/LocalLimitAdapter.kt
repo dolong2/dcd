7abadce2eaa85dee3ce1a244d8e51f0920babd5e
dolong2 <k01066624566@gmail.com>
2025-05-04 21:40:24 +0900

    [add] LimitPort 구현 어뎁터 추가

---
+++ src/main/kotlin/com/dcd/server/infrastructure/global/thridpriry/bucket4j/LocalLimitAdapter.kt
@@ -0,0 +1,28 @@
+package com.dcd.server.infrastructure.global.thridpriry.bucket4j
+
+import com.dcd.server.core.common.annotation.Limit
+import com.dcd.server.core.common.spi.LimitPort
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+class LocalLimitAdapter : LimitPort {
+    private val bucketMap: MutableMap<String, Bucket> = ConcurrentHashMap()
+
+    override fun consumer(key: String, limit: Limit): Boolean =
+        resolveToken(key, limit).tryConsume(1L)
+
+    private fun resolveToken(key: String, limit: Limit): Bucket =
+        bucketMap.computeIfAbsent(key) {
+            val bandwidth = Bandwidth.builder()
+                .capacity(limit.capacity)
+                .refillGreedy(limit.refillAmount, Duration.ofMinutes(limit.refillDurationMinute))
+                .initialTokens(limit.capacity)
+                .build()
+
+            Bucket.builder().addLimit(bandwidth).build()
+        }
+}
