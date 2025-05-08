d697f318fdf97689697dc177d9bb87aa231610a4
dolong2 <k01066624566@gmail.com>
2025-05-08 23:09:21 +0900

    [fix] 이메일 전송시 Limit의 표현식 수정

--- src/main/kotlin/com/dcd/server/presentation/domain/auth/AuthWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/auth/AuthWebAdapter.kt
@@ -29,7 +29,7 @@ class AuthWebAdapter(
     private val nonAuthChangePasswordUseCase: NonAuthChangePasswordUseCase
 ) {
     @PostMapping("/email")
-    @Limit(target = "#emailSendRequest.email+#email", capacity = 5)
+    @Limit(target = "#emailSendRequest.email", capacity = 5)
     fun sendAuthEmail(
         @Validated
         @RequestBody
