952449fff7a0db60519b08d69257773163f40dd0
dolong2 <k01066624566@gmail.com>
2025-05-26 21:16:52 +0900

    [add] 배포 스크립트에서 절대 경로를 사용하도록 수정

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-source dcd.env
-nohup java -jar server-0.0.1-SNAPSHOT.jar > dcd.log 2>&1 &
+source /home/dolong2/dcd/dcd.env
+nohup java -jar /home/dolong2/dcd/server-0.0.1-SNAPSHOT.jar > /home/dolong2/dcd/dcd.log 2>&1 &
