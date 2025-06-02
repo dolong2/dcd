94531989a5751c625c051b3033d8feead3012dde
dolong2 <k01066624566@gmail.com>
2025-06-02 21:29:05 +0900

    [del] 기존에 워크스페이스 소유주를 검증하던 로직 제거

--- src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/DeleteGlobalEnvUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/workspace/usecase/DeleteGlobalEnvUseCase.kt
@@ -6,22 +6,18 @@ import com.dcd.server.core.domain.env.spi.CommandGlobalEnvPort
 import com.dcd.server.core.domain.env.spi.QueryGlobalEnvPort
 import com.dcd.server.core.domain.workspace.exception.GlobalEnvNotFoundException
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
-import com.dcd.server.core.domain.workspace.service.ValidateWorkspaceOwnerService
 import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
 
 @UseCase
 class DeleteGlobalEnvUseCase(
     private val queryWorkspacePort: QueryWorkspacePort,
     private val commandGlobalEnvPort: CommandGlobalEnvPort,
-    private val validateWorkspaceOwnerService: ValidateWorkspaceOwnerService,
     private val queryGlobalEnvPort: QueryGlobalEnvPort
 ) {
     @Lock("#workspaceId+#key")
     fun execute(workspaceId: String, key: String) {
         val workspace = (queryWorkspacePort.findById(workspaceId)
             ?: throw WorkspaceNotFoundException())
-
-        validateWorkspaceOwnerService.validateOwner(workspace)
 
         val globalEnv = (queryGlobalEnvPort.findByKeyAndWorkspace(key, workspace)
             ?: throw GlobalEnvNotFoundException())
