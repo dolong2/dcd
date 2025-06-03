ef8f1bfd68555a64374a253ed6f19b387e96026f
dolong2 <k01066624566@gmail.com>
2025-06-03 18:06:31 +0900

    [chore] 로그인시 토큰관련 port를 사용하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/auth/usecase/SignInUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/auth/usecase/SignInUseCase.kt
@@ -5,19 +5,19 @@ import com.dcd.server.core.common.service.SecurityService
 import com.dcd.server.core.domain.auth.dto.request.SignInReqDto
 import com.dcd.server.core.domain.auth.dto.response.TokenResDto
 import com.dcd.server.core.domain.auth.exception.UserNotFoundException
-import com.dcd.server.core.domain.auth.spi.JwtPort
+import com.dcd.server.core.domain.auth.spi.GenerateTokenPort
 import com.dcd.server.core.domain.user.spi.QueryUserPort
 
 @UseCase
 class SignInUseCase(
     private val queryUserPort: QueryUserPort,
     private val securityService: SecurityService,
-    private val jwtPort: JwtPort
+    private val generateTokenPort: GenerateTokenPort
 ) {
     fun execute(signInReqDto: SignInReqDto): TokenResDto {
         val user = (queryUserPort.findByEmail(signInReqDto.email)
             ?: throw UserNotFoundException()) // 해당 유저를 찾을 수 없음
         securityService.matchPassword(signInReqDto.password, user.password)
-        return jwtPort.generateToken(user.id)
+        return generateTokenPort.generateToken(user.id)
     }
 }
