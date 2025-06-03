8c4e94cb22153b61b8eb0827a2d4d200aa3c7da6
dolong2 <k01066624566@gmail.com>
2025-06-03 18:06:19 +0900

    [chore] 토큰 재발급시 토큰관련 port를 사용하도록 수정

--- src/main/kotlin/com/dcd/server/core/domain/auth/usecase/ReissueTokenUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/auth/usecase/ReissueTokenUseCase.kt
@@ -4,24 +4,21 @@ import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.domain.auth.dto.response.TokenResDto
 import com.dcd.server.core.domain.auth.exception.ExpiredRefreshTokenException
 import com.dcd.server.core.domain.auth.exception.UserNotFoundException
-import com.dcd.server.core.domain.auth.spi.CommandRefreshTokenPort
-import com.dcd.server.core.domain.auth.spi.JwtPort
-import com.dcd.server.core.domain.auth.spi.QueryRefreshTokenPort
+import com.dcd.server.core.domain.auth.spi.*
 import com.dcd.server.core.domain.user.spi.QueryUserPort
 import com.dcd.server.infrastructure.global.jwt.adapter.JwtPrefix
-import com.dcd.server.infrastructure.global.jwt.adapter.ParseTokenAdapter
 import com.dcd.server.infrastructure.global.jwt.exception.TokenTypeNotValidException
 
 @UseCase
 class ReissueTokenUseCase(
     private val queryRefreshTokenPort: QueryRefreshTokenPort,
     private val commandRefreshTokenPort: CommandRefreshTokenPort,
-    private val jwtPort: JwtPort,
-    private val queryUserPort: QueryUserPort,
-    private val parseTokenAdapter: ParseTokenAdapter
+    private val generateTokenPort: GenerateTokenPort,
+    private val parseTokenPort: ParseTokenPort,
+    private val queryUserPort: QueryUserPort
 ) {
     fun execute(token: String): TokenResDto {
-        val jwtType = parseTokenAdapter.getJwtType(token)
+        val jwtType = parseTokenPort.getJwtType(token)
         if (jwtType != JwtPrefix.REFRESH)
             throw TokenTypeNotValidException()
 
@@ -30,6 +27,6 @@ class ReissueTokenUseCase(
         val user = (queryUserPort.findById(refreshToken.userId)
             ?: throw UserNotFoundException())
         commandRefreshTokenPort.delete(refreshToken)
-        return jwtPort.generateToken(user.id)
+        return generateTokenPort.generateToken(user.id)
     }
 }
