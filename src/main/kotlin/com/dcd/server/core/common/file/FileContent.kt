8e981802877c4aabdaf6473d9531040e213e22ed
dolong2 <k01066624566@gmail.com>
2025-06-11 23:54:21 +0900

    [fix] 애플리케이션을 http로 외부에 개방할때 생성되는 nginx 문법 오류 수정

--- src/main/kotlin/com/dcd/server/core/common/file/FileContent.kt
+++ src/main/kotlin/com/dcd/server/core/common/file/FileContent.kt
@@ -91,14 +91,14 @@ object FileContent {
 
     fun getApplicationHttpConfig(application: Application, domain: String): String =
         "server {\n" +
-            "\tlisten: 443;\n" +
+            "\tlisten: 443 ssl;\n" +
             "\tserver-name: $domain;\n\n" +
 
             "\tssl_certificate /etc/nginx/conf.d/ssl/certificate/fullchain.pem;\n" +
             "\tssl_certificate_key /etc/nginx/conf.d/ssl/certificate/privkey.pem;\n\n" +
 
             "\tlocation / {\n" +
-                "\t\tproxy_pass: http://host.docker.internal:${application.externalPort};\n" +
+                "\t\tproxy_pass http://host.docker.internal:${application.externalPort};\n" +
             "\t}\n" +
         "}\n"
 
