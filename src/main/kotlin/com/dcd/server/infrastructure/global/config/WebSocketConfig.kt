54436d8c155c1bae3f92392ebd0615e097997bd1
dolong2 <k01066624566@gmail.com>
2025-06-13 23:44:58 +0900

    [chore] WebSocketConfig에서 SocketHandler를 주입받도록 수정

--- src/main/kotlin/com/dcd/server/infrastructure/global/config/WebSocketConfig.kt
+++ src/main/kotlin/com/dcd/server/infrastructure/global/config/WebSocketConfig.kt
@@ -1,8 +1,7 @@
 package com.dcd.server.infrastructure.global.config
 
+import com.dcd.server.core.common.socket.SocketHandler
 import com.dcd.server.infrastructure.global.socket.interceptor.WebSocketInterceptor
-import com.dcd.server.infrastructure.global.jwt.adapter.ParseTokenAdapter
-import com.dcd.server.infrastructure.global.socket.ApplicationSocketHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
@@ -10,11 +9,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 class WebSocketConfig(
-    private val dockerWebSocketHandler: ApplicationSocketHandler,
+    private val socketHAndler: SocketHandler,
     private val webSocketInterceptor: WebSocketInterceptor
 ) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(dockerWebSocketHandler, "/application/exec")
+        registry.addHandler(socketHAndler, "/application/exec")
             .addInterceptors(webSocketInterceptor)
             .setAllowedOrigins("*")
     }
