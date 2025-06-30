bab74a566fdc7893d55e9e28664a9895f84789be
dolong2 <k01066624566@gmail.com>
2025-06-30 23:59:44 +0900

    [chore] nginx 설정만 리로드하도록 수정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/dolong2/dcd/dcd.env
 cd /home/dolong2/dcd && nohup java -jar /home/dolong2/dcd/server-0.0.1-SNAPSHOT.jar > /home/dolong2/dcd/dcd.log 2>&1 &
-docker restart dcd-nginx
+docker exec dcd-nginx nginx -s reload
