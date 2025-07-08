e56f765b6c171e31703868e51fa0db448297d5e2
dolong2 <k01066624566@gmail.com>
2025-07-08 23:25:56 +0900

    [add] 도메인 생성시 이름 검증 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/domain/usecase/CreateDomainUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/domain/usecase/CreateDomainUseCase.kt
@@ -5,15 +5,22 @@ import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.domain.dto.extension.toEntity
 import com.dcd.server.core.domain.domain.dto.request.CreateDomainReqDto
 import com.dcd.server.core.domain.domain.dto.respone.CreateDomainResDto
+import com.dcd.server.core.domain.domain.exception.AlreadyExistsDomainException
 import com.dcd.server.core.domain.domain.spi.CommandDomainPort
+import com.dcd.server.core.domain.domain.spi.QueryDomainPort
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 
 @UseCase
 class CreateDomainUseCase(
     private val commandDomainPort: CommandDomainPort,
+    private val queryDomainPort: QueryDomainPort,
     private val workspaceInfo: WorkspaceInfo
 ) {
     fun execute(createDomainReqDto: CreateDomainReqDto): CreateDomainResDto {
+        //도메인은 워크스페이스에 관계없이 전역적으로 관리되어야함
+        if (queryDomainPort.existsByName(createDomainReqDto.name))
+            throw AlreadyExistsDomainException()
+
         val workspace = (workspaceInfo.workspace
             ?: throw WorkspaceNotFoundException())
 
