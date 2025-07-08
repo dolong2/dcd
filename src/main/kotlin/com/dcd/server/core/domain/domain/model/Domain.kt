15fdb1b76bc9dcd6dae23a459adfe22495c7f168
dolong2 <k01066624566@gmail.com>
2025-07-08 22:55:57 +0900

    [add] 도메인 모델에 workspace 추가

--- src/main/kotlin/com/dcd/server/core/domain/domain/model/Domain.kt
+++ src/main/kotlin/com/dcd/server/core/domain/domain/model/Domain.kt
@@ -1,10 +1,12 @@
 package com.dcd.server.core.domain.domain.model
 
 import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.workspace.model.Workspace
 
 data class Domain(
     val id: String,
     val name: String,
     val description: String,
-    val application: Application?
+    val application: Application?,
+    val workspace: Workspace,
 )
--- src/main/kotlin/com/dcd/server/persistence/domain/adapter/DomainAdapter.kt
+++ src/main/kotlin/com/dcd/server/persistence/domain/adapter/DomainAdapter.kt
@@ -4,6 +4,10 @@ import com.dcd.server.core.domain.domain.model.Domain
 import com.dcd.server.persistence.application.adapter.toDomain
 import com.dcd.server.persistence.application.adapter.toEntity
 import com.dcd.server.persistence.domain.entity.DomainJpaEntity
+import com.dcd.server.persistence.user.adapter.toDomain
+import com.dcd.server.persistence.user.adapter.toEntity
+import com.dcd.server.persistence.workspace.adapter.toDomain
+import com.dcd.server.persistence.workspace.adapter.toEntity
 import java.util.UUID
 
 fun Domain.toEntity(): DomainJpaEntity =
@@ -11,7 +15,8 @@ fun Domain.toEntity(): DomainJpaEntity =
         id = UUID.fromString(this.id),
         name = this.name,
         description = this.description,
-        application = this.application?.toEntity()
+        application = this.application?.toEntity(),
+        workspace = this.workspace.toEntity()
     )
 
 fun DomainJpaEntity.toDomain(): Domain =
@@ -19,5 +24,6 @@ fun DomainJpaEntity.toDomain(): Domain =
         id = this.id.toString(),
         name = this.name,
         description = this.description,
-        application = this.application?.toDomain()
+        application = this.application?.toDomain(),
+        workspace = this.workspace.toDomain()
     )
