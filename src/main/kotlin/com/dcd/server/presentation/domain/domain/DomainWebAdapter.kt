e3e99d26396c14c3569527385ebd46cb5b321f4f
dolong2 <k01066624566@gmail.com>
2025-07-13 20:26:25 +0900

    [add] 도메인 삭제 엔드포인트 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/domain/DomainWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/domain/DomainWebAdapter.kt
@@ -2,12 +2,14 @@ package com.dcd.server.presentation.domain.domain
 
 import com.dcd.server.core.common.annotation.WorkspaceOwnerVerification
 import com.dcd.server.core.domain.domain.usecase.CreateDomainUseCase
+import com.dcd.server.core.domain.domain.usecase.DeleteDomainUseCase
 import com.dcd.server.presentation.domain.domain.data.dto.toDto
 import com.dcd.server.presentation.domain.domain.data.dto.toResponse
 import com.dcd.server.presentation.domain.domain.data.request.CreateDomainRequest
 import com.dcd.server.presentation.domain.domain.data.response.CreateDomainResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -17,7 +19,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/{workspaceId}/domain")
 class DomainWebAdapter(
-    private val createDomainUseCase: CreateDomainUseCase
+    private val createDomainUseCase: CreateDomainUseCase,
+    private val deleteDomainUseCase: DeleteDomainUseCase
 ) {
     @PostMapping
     @WorkspaceOwnerVerification("#workspaceId")
@@ -27,4 +30,13 @@ class DomainWebAdapter(
     ): ResponseEntity<CreateDomainResponse> =
         createDomainUseCase.execute(createDomainRequest.toDto())
             .let { ResponseEntity.ok(it.toResponse()) }
+
+    @DeleteMapping("/{domainId}")
+    @WorkspaceOwnerVerification("#workspaceId")
+    fun deleteDomain(
+        @PathVariable workspaceId: String,
+        @PathVariable domainId: String
+    ): ResponseEntity<Void> =
+        deleteDomainUseCase.execute(domainId)
+            .run { ResponseEntity.ok().build() }
 }
