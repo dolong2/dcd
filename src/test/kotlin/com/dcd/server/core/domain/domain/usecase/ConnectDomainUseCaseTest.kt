937cdccdb37da765c1181d9cb7459fe012730136
dolong2 <k01066624566@gmail.com>
2025-07-15 23:49:53 +0900

    [add] 도메인 연결 유스케이스 테스트 케이스 추가

---
+++ src/test/kotlin/com/dcd/server/core/domain/domain/usecase/ConnectDomainUseCaseTest.kt
@@ -0,0 +1,65 @@
+package com.dcd.server.core.domain.domain.usecase
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.common.data.WorkspaceInfo
+import com.dcd.server.core.common.file.FileContent
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.domain.dto.request.ConnectDomainReqDto
+import com.dcd.server.core.domain.domain.spi.CommandDomainPort
+import com.dcd.server.core.domain.domain.spi.QueryDomainPort
+import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.verify
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import util.domain.DomainGenerator
+import java.util.*
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+class ConnectDomainUseCaseTest(
+    private val connectDomainUseCase: ConnectDomainUseCase,
+    private val commandDomainPort: CommandDomainPort,
+    private val queryDomainPort: QueryDomainPort,
+    private val queryWorkspacePort: QueryWorkspacePort,
+    private val queryApplicationPort: QueryApplicationPort,
+    private val workspaceInfo: WorkspaceInfo,
+    @MockkBean(relaxed = true)
+    private val commandPort: CommandPort,
+) : BehaviorSpec({
+    val domainId = UUID.randomUUID().toString()
+    val applicationId = "2fb0f315-8272-422f-8e9f-c4f765c022b2"
+    beforeContainer {
+        val workspace = queryWorkspacePort.findById("d57b42f5-5cc4-440b-8dce-b4fc2e372eff")!!
+        workspaceInfo.workspace = workspace
+    }
+
+    given("연결할 도메인이 주어지고") {
+        val domain = DomainGenerator.generateDomain(id = domainId, workspace = workspaceInfo.workspace!!)
+        commandDomainPort.save(domain)
+
+        `when`("도메인에 애플리케이션을 연결할때") {
+            val request = ConnectDomainReqDto(applicationId)
+
+            connectDomainUseCase.execute(domainId, request)
+
+            then("Nginx 설정 파일을 생성해야함") {
+                val application = queryApplicationPort.findById(applicationId)!!
+                val webServerConfig = FileContent.getApplicationHttpConfig(application, domain.getDomainName())
+                val httpConfigDirectory = "./nginx/conf/${domain.id}"
+                verify { commandPort.executeShellCommand("mkdir -p $httpConfigDirectory && cat <<EOF > ${httpConfigDirectory}/${application.name.replace(" ", "-")}-http.conf \n ${webServerConfig}EOF") }
+            }
+            then("도메인에 해당 애플리케이션이 연결되어야함") {
+                val domain = queryDomainPort.findById(domainId)!!
+                domain.application?.id shouldBe applicationId
+            }
+            then("nginx 재실행이 명령되어야함") {
+                verify { commandPort.executeShellCommand("docker restart dcd-nginx") }
+            }
+        }
+    }
+})
