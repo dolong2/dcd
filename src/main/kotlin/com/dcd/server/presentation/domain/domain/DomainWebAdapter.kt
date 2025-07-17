2ddd094173efd5b5019b9ae6e690d0ac8f1ab30d
dolong2 <k01066624566@gmail.com>
2025-07-17 22:20:59 +0900

    [feat] 도메인 조회 엔드포인트 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/domain/DomainWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/domain/DomainWebAdapter.kt
@@ -1,23 +1,18 @@
 package com.dcd.server.presentation.domain.domain
 
 import com.dcd.server.core.common.annotation.WorkspaceOwnerVerification
-import com.dcd.server.core.domain.domain.usecase.ConnectDomainUseCase
-import com.dcd.server.core.domain.domain.usecase.CreateDomainUseCase
-import com.dcd.server.core.domain.domain.usecase.DeleteDomainUseCase
-import com.dcd.server.core.domain.domain.usecase.DisconnectDomainUseCase
+import com.dcd.server.core.domain.domain.usecase.*
+import com.dcd.server.presentation.common.data.extension.toResponse
+import com.dcd.server.presentation.common.data.response.ListResponse
 import com.dcd.server.presentation.domain.domain.data.extension.toDto
 import com.dcd.server.presentation.domain.domain.data.extension.toResponse
 import com.dcd.server.presentation.domain.domain.data.request.ConnectDomainRequest
 import com.dcd.server.presentation.domain.domain.data.request.CreateDomainRequest
 import com.dcd.server.presentation.domain.domain.data.response.CreateDomainResponse
+import com.dcd.server.presentation.domain.domain.data.response.DomainResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/{workspaceId}/domain")
@@ -25,8 +20,15 @@ class DomainWebAdapter(
     private val createDomainUseCase: CreateDomainUseCase,
     private val deleteDomainUseCase: DeleteDomainUseCase,
     private val connectDomainUseCase: ConnectDomainUseCase,
-    private val disconnectDomainUseCase: DisconnectDomainUseCase
+    private val disconnectDomainUseCase: DisconnectDomainUseCase,
+    private val getDomainUseCase: GetDomainUseCase
 ) {
+    @GetMapping
+    @WorkspaceOwnerVerification("#workspaceId")
+    fun getDomainList(@PathVariable workspaceId: String): ResponseEntity<ListResponse<DomainResponse>> =
+        getDomainUseCase.execute()
+            .let { ResponseEntity.ok(it.toResponse { resDto -> resDto.toResponse() }) }
+
     @PostMapping
     @WorkspaceOwnerVerification("#workspaceId")
     fun createDomain(
