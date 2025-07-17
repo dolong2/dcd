0f67a4b33a8ed4a4f48dc01181441d5f69fb179b
dolong2 <k01066624566@gmail.com>
2025-07-17 22:40:37 +0900

    [test] 도메인 조회 웹어뎁터 테스트 추가

--- src/test/kotlin/com/dcd/server/presentation/domain/domain/DomainWebAdapterTest.kt
+++ src/test/kotlin/com/dcd/server/presentation/domain/domain/DomainWebAdapterTest.kt
@@ -1,17 +1,18 @@
 package com.dcd.server.presentation.domain.domain
 
+import com.dcd.server.core.common.data.dto.response.ListResDto
 import com.dcd.server.core.domain.domain.dto.request.CreateDomainReqDto
 import com.dcd.server.core.domain.domain.dto.response.CreateDomainResDto
-import com.dcd.server.core.domain.domain.usecase.ConnectDomainUseCase
-import com.dcd.server.core.domain.domain.usecase.CreateDomainUseCase
-import com.dcd.server.core.domain.domain.usecase.DeleteDomainUseCase
-import com.dcd.server.core.domain.domain.usecase.DisconnectDomainUseCase
+import com.dcd.server.core.domain.domain.dto.response.DomainResDto
+import com.dcd.server.core.domain.domain.usecase.*
 import com.dcd.server.presentation.domain.domain.data.request.ConnectDomainRequest
 import com.dcd.server.presentation.domain.domain.data.request.CreateDomainRequest
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.springframework.http.HttpStatus
 import java.util.UUID
 
@@ -21,12 +22,14 @@ class DomainWebAdapterTest : BehaviorSpec({
     val deleteDomainUseCase = mockk<DeleteDomainUseCase>(relaxUnitFun = true)
     val connectDomainUseCase = mockk<ConnectDomainUseCase>(relaxUnitFun = true)
     val disconnectDomainUseCase = mockk<DisconnectDomainUseCase>(relaxUnitFun = true)
+    val getDomainUseCase = mockk<GetDomainUseCase>()
 
     val domainWebAdapter = DomainWebAdapter(
         createDomainUseCase = createDomainUseCase,
         deleteDomainUseCase = deleteDomainUseCase,
         connectDomainUseCase = connectDomainUseCase,
         disconnectDomainUseCase = disconnectDomainUseCase,
+        getDomainUseCase = getDomainUseCase
     )
 
     given("CreateDomainRequest가 주어지고") {
@@ -74,6 +77,32 @@ class DomainWebAdapterTest : BehaviorSpec({
             then("200으로 응답되어야함") {
                 result.statusCode shouldBe HttpStatus.OK
                 result.body shouldBe null
+            }
+        }
+    }
+
+    given("아무것도 주어지지 않고") {
+
+        `when`("도메인을 조회할때") {
+            val domainResDto = DomainResDto(id = UUID.randomUUID().toString(), name = "testDomain", description = "test", application = null)
+            every { getDomainUseCase.execute() } returns ListResDto(listOf(domainResDto))
+
+            val result = domainWebAdapter.getDomainList(workspaceId)
+
+            then("유스케이스에서 반환된 값의 정보를 가지고 있어야함") {
+                val list = result.body?.list
+                list shouldNotBe null
+                list!!.size shouldBe 1
+
+                val domainResponse = list[0]
+                domainResponse.id shouldBe domainResDto.id
+                domainResponse.name shouldBe domainResDto.name
+                domainResponse.description shouldBe domainResDto.description
+                domainResponse.application shouldBe domainResDto.application
+            }
+
+            then("응답 상태는 OK여야함") {
+                result.statusCode shouldBe HttpStatus.OK
             }
         }
     }
