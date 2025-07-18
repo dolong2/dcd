0127ae57e6f079a4139cb6a8722cf7838be45f0d
dolong2 <k01066624566@gmail.com>
2025-07-18 23:24:07 +0900

    [feat] 도메인 삭제 유스케이스에 도메인 워크스페이스 정보 검증 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/domain/usecase/DeleteDomainUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/domain/usecase/DeleteDomainUseCase.kt
@@ -1,6 +1,7 @@
 package com.dcd.server.core.domain.domain.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.domain.exception.AlreadyConnectedDomainException
 import com.dcd.server.core.domain.domain.exception.DomainNotFoundException
 import com.dcd.server.core.domain.domain.spi.CommandDomainPort
@@ -9,11 +10,15 @@ import com.dcd.server.core.domain.domain.spi.QueryDomainPort
 @UseCase
 class DeleteDomainUseCase(
     private val queryDomainPort: QueryDomainPort,
-    private val commandDomainPort: CommandDomainPort
+    private val commandDomainPort: CommandDomainPort,
+    private val workspaceInfo: WorkspaceInfo
 ) {
     fun execute(id: String) {
         val domain = (queryDomainPort.findById(id)
             ?: throw DomainNotFoundException())
+
+        if (domain.workspace != workspaceInfo.workspace)
+            throw DomainNotFoundException()
 
         if (domain.application != null)
             throw AlreadyConnectedDomainException()
