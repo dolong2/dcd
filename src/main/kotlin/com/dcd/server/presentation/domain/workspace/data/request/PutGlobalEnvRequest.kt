6d9670fb5e7b48836b344b9786189eb0d783228e
dolong2 <k01066624566@gmail.com>
2025-08-04 22:58:52 +0900

    [del] GlobalEnv를 사용하던 핵심 메서드및 유스케이스 제거

--- src/main/kotlin/com/dcd/server/presentation/domain/workspace/data/request/PutGlobalEnvRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.dcd.server.presentation.domain.workspace.data.request
-
-import com.dcd.server.presentation.domain.env.data.request.PutEnvRequest
-
-data class PutGlobalEnvRequest(
-    val name: String,
-    val description: String,
-    val envList: List<PutEnvRequest>
-)
