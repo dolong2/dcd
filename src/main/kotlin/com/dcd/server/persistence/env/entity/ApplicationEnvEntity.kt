d2972fbb4bed40c8c6844922f3cb48f0737bc9b4
dolong2 <k01066624566@gmail.com>
2025-08-05 23:14:49 +0900

    [feat] 애플리케이션 환경변수 엔티티에 workspace 필드 추가

--- src/main/kotlin/com/dcd/server/persistence/env/entity/ApplicationEnvEntity.kt
+++ src/main/kotlin/com/dcd/server/persistence/env/entity/ApplicationEnvEntity.kt
@@ -1,6 +1,7 @@
 package com.dcd.server.persistence.env.entity
 
 import com.dcd.server.persistence.env.entity.common.Env
+import com.dcd.server.persistence.workspace.entity.WorkspaceJpaEntity
 import jakarta.persistence.*
 import java.util.*
 import kotlin.collections.List
@@ -11,5 +12,8 @@ class ApplicationEnvEntity(
     name: String,
     description: String,
     @OneToMany(mappedBy = "applicationEnv", cascade = [CascadeType.REMOVE])
-    val details: List<ApplicationEnvDetailEntity>
+    val details: List<ApplicationEnvDetailEntity>,
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "workspace_id")
+    val workspace: WorkspaceJpaEntity,
 ) : Env(id, name, description)
