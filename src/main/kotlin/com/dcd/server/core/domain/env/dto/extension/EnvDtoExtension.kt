6cf85dfbcc7426c361fee3e76e2a35b4c89938e2
dolong2 <k01066624566@gmail.com>
2025-08-06 23:46:50 +0900

    [feat] 환경변수 삽입 요청 dto 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/env/dto/extension/EnvDtoExtension.kt
@@ -0,0 +1,24 @@
+package com.dcd.server.core.domain.env.dto.extension
+
+import com.dcd.server.core.domain.env.dto.request.PutApplicationReqDto
+import com.dcd.server.core.domain.env.dto.request.PutEnvReqDto
+import com.dcd.server.core.domain.env.model.ApplicationEnv
+import com.dcd.server.core.domain.env.model.ApplicationEnvDetail
+import com.dcd.server.core.domain.workspace.model.Workspace
+import java.util.UUID
+
+fun PutApplicationReqDto.toModel(workspace: Workspace): ApplicationEnv =
+    ApplicationEnv(
+        name = this.name,
+        description = this.description,
+        details = this.details.map { it.toModel() },
+        workspace = workspace
+    )
+
+fun PutEnvReqDto.toModel(): ApplicationEnvDetail =
+    ApplicationEnvDetail(
+        id = UUID.randomUUID(),
+        key = this.key,
+        value = this.value,
+        encryption = this.encryption,
+    )
