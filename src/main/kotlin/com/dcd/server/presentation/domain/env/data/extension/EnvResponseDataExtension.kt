4271b624c4e10832bd7e685e11456d323cf14b03
dolong2 <k01066624566@gmail.com>
2025-08-09 22:18:31 +0900

    [feat] 환경변수 상세 조회 응답 객체 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/env/data/extension/EnvResponseDataExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/env/data/extension/EnvResponseDataExtension.kt
@@ -1,8 +1,12 @@
 package com.dcd.server.presentation.domain.env.data.extension
 
+import com.dcd.server.core.domain.env.dto.response.ApplicationEnvDetailResDto
 import com.dcd.server.core.domain.env.dto.response.ApplicationEnvListResDto
+import com.dcd.server.core.domain.env.dto.response.ApplicationEnvResDto
 import com.dcd.server.core.domain.env.dto.response.ApplicationEnvSimpleResDto
+import com.dcd.server.presentation.domain.env.data.response.ApplicationEnvDetailResponse
 import com.dcd.server.presentation.domain.env.data.response.ApplicationEnvListResponse
+import com.dcd.server.presentation.domain.env.data.response.ApplicationEnvResponse
 import com.dcd.server.presentation.domain.env.data.response.ApplicationEnvSimpleResponse
 
 fun ApplicationEnvSimpleResDto.toResponse(): ApplicationEnvSimpleResponse =
@@ -15,4 +19,19 @@ fun ApplicationEnvSimpleResDto.toResponse(): ApplicationEnvSimpleResponse =
 fun ApplicationEnvListResDto.toResponse(): ApplicationEnvListResponse =
     ApplicationEnvListResponse(
         list = this.list.map { it.toResponse() }
+    )
+
+fun ApplicationEnvResDto.toResponse(): ApplicationEnvResponse =
+    ApplicationEnvResponse(
+        id = this.id,
+        name = this.name,
+        description = this.description,
+        details = this.details.map { it.toResponse() }
+    )
+
+fun ApplicationEnvDetailResDto.toResponse(): ApplicationEnvDetailResponse =
+    ApplicationEnvDetailResponse(
+        key = this.key,
+        value = this.value,
+        encryption = this.encryption
     )
