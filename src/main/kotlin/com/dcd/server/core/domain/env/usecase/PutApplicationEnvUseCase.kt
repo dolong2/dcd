e6fcf10d391161a76bd8ee1afe80f010d9a1a48c
dolong2 <k01066624566@gmail.com>
2025-08-10 22:28:57 +0900

    [feat] 환경변수 정보를 수정하는 유스케이스 메서드 추가

--- src/main/kotlin/com/dcd/server/core/domain/env/usecase/PutApplicationEnvUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/env/usecase/PutApplicationEnvUseCase.kt
@@ -3,12 +3,15 @@ package com.dcd.server.core.domain.env.usecase
 import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.common.service.EncryptService
+import com.dcd.server.core.domain.application.exception.ApplicationEnvNotFoundException
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
 import com.dcd.server.core.domain.env.dto.extension.toModel
 import com.dcd.server.core.domain.env.dto.request.PutApplicationEnvReqDto
 import com.dcd.server.core.domain.env.model.ApplicationEnvMatcher
 import com.dcd.server.core.domain.env.spi.CommandApplicationEnvPort
+import com.dcd.server.core.domain.env.spi.QueryApplicationEnvPort
 import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
+import java.util.UUID
 
 @UseCase
 class PutApplicationEnvUseCase(
@@ -16,6 +19,7 @@ class PutApplicationEnvUseCase(
     private val commandApplicationEnvPort: CommandApplicationEnvPort,
     private val queryApplicationPort: QueryApplicationPort,
     private val encryptService: EncryptService,
+    private val queryApplicationEnvPort: QueryApplicationEnvPort,
 ) {
 
     fun execute(putApplicationEnvReqDto: PutApplicationEnvReqDto) {
@@ -38,6 +42,45 @@ class PutApplicationEnvUseCase(
             ApplicationEnvMatcher(
                 application = it,
                 applicationEnv = applicationEnv
+            )
+        }
+        commandApplicationEnvPort.saveAllMatcher(envMatcherList)
+    }
+
+    fun execute(id: UUID, putApplicationEnvReqDto: PutApplicationEnvReqDto) {
+        val workspace = (workspaceInfo.workspace
+            ?: throw WorkspaceNotFoundException())
+
+        val applicationEnv = (queryApplicationEnvPort.findById(id)
+            ?: throw ApplicationEnvNotFoundException())
+
+        if (workspace != applicationEnv.workspace)
+            throw ApplicationEnvNotFoundException()
+
+        val requestKeys = putApplicationEnvReqDto.details.map { it.key }.toSet()
+        val mergedApplicationEnv = applicationEnv.details.filter { it.key !in requestKeys } + applicationEnv.details
+
+        val updatedEnv = applicationEnv.copy(
+            name = putApplicationEnvReqDto.name,
+            description = putApplicationEnvReqDto.description,
+            details = mergedApplicationEnv
+        )
+        commandApplicationEnvPort.save(updatedEnv)
+
+        commandApplicationEnvPort.deleteAllMatcherByEnv(applicationEnv)
+        val applicationListByLabel = putApplicationEnvReqDto.applicationLabelList
+            ?.let { queryApplicationPort.findAllByWorkspace(workspace, it) }
+            ?: emptyList()
+        val applicationListByIds = (putApplicationEnvReqDto.applicationIdList
+            ?.let { queryApplicationPort.findByIds(it) }
+            ?: emptyList())
+
+        val applicationSet = (applicationListByLabel + applicationListByIds).toSet()
+
+        val envMatcherList = applicationSet.map {
+            ApplicationEnvMatcher(
+                application = it,
+                applicationEnv = updatedEnv
             )
         }
         commandApplicationEnvPort.saveAllMatcher(envMatcherList)
