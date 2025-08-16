eff26c7967e24c62d38205041d42d028b992e52d
dolong2 <k01066624566@gmail.com>
2025-08-16 23:08:55 +0900

    [feat] 환경변수 매칭 관계 조회 메서드 추가

--- src/main/kotlin/com/dcd/server/core/domain/env/spi/QueryApplicationEnvPort.kt
+++ src/main/kotlin/com/dcd/server/core/domain/env/spi/QueryApplicationEnvPort.kt
@@ -3,6 +3,7 @@ package com.dcd.server.core.domain.env.spi
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.env.model.ApplicationEnv
 import com.dcd.server.core.domain.env.model.ApplicationEnvDetail
+import com.dcd.server.core.domain.env.model.ApplicationEnvMatcher
 import com.dcd.server.core.domain.workspace.model.Workspace
 import java.util.UUID
 
@@ -16,4 +17,6 @@ interface QueryApplicationEnvPort {
     fun findAllByWorkspace(workspace: Workspace): List<ApplicationEnv>
 
     fun findAllByLabelsIn(workspace: Workspace, labels: List<String>): List<ApplicationEnv>
+
+    fun findAllMatcherByEnv(applicationEnv: ApplicationEnv): List<ApplicationEnvMatcher>
 }
