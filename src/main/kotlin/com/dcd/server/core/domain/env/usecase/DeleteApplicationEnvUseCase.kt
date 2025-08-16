01d8fb698daae00630992626891f5fbe1e0b3f0e
dolong2 <k01066624566@gmail.com>
2025-08-16 23:09:17 +0900

    [feat] 환경변수 삭제시 애플리케이션 배포 이벤트를 발행하는 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/env/usecase/DeleteApplicationEnvUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/env/usecase/DeleteApplicationEnvUseCase.kt
@@ -1,19 +1,28 @@
 package com.dcd.server.core.domain.env.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.domain.application.event.DeployApplicationEvent
 import com.dcd.server.core.domain.env.exception.ApplicationEnvNotFoundException
 import com.dcd.server.core.domain.env.spi.CommandApplicationEnvPort
 import com.dcd.server.core.domain.env.spi.QueryApplicationEnvPort
+import org.springframework.context.ApplicationEventPublisher
 import java.util.UUID
 
 @UseCase
 class DeleteApplicationEnvUseCase(
     private val queryApplicationEnvPort: QueryApplicationEnvPort,
-    private val commandApplicationEnvPort: CommandApplicationEnvPort
+    private val commandApplicationEnvPort: CommandApplicationEnvPort,
+    private val eventPublisher: ApplicationEventPublisher
 ) {
     fun execute(envId: UUID) {
         val applicationEnv = queryApplicationEnvPort.findById(envId)
             ?: throw ApplicationEnvNotFoundException()
         commandApplicationEnvPort.delete(applicationEnv)
+
+        val envMatcher = queryApplicationEnvPort.findAllMatcherByEnv(applicationEnv)
+        val applicationSet = envMatcher.map { it.application }.toSet()
+        if (applicationSet.isNotEmpty()) {
+            eventPublisher.publishEvent(DeployApplicationEvent(applicationSet.map { it.id }))
+        }
     }
 }
