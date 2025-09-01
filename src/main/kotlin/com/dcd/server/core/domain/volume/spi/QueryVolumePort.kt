02d364ebaf502c810cf96a602f65ab331f28efb5
dolong2 <k01066624566@gmail.com>
2025-09-01 23:34:54 +0900

    [feat] 볼륨 관련 영속성 포트 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/volume/spi/QueryVolumePort.kt
@@ -0,0 +1,14 @@
+package com.dcd.server.core.domain.volume.spi
+
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.volume.model.Volume
+import com.dcd.server.core.domain.volume.model.VolumeMount
+import java.util.UUID
+
+interface QueryVolumePort {
+    fun findById(id: UUID): Volume?
+
+    fun findAllMountByApplication(application: Application): List<VolumeMount>
+
+    fun findAllMountByVolume(volume: Volume): List<VolumeMount>
+}
