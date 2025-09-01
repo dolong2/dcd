dd758b3e260a06b64c14e5d4975ab03a375a83c8
dolong2 <k01066624566@gmail.com>
2025-09-01 23:34:31 +0900

    [feat] 볼륨관련 모델 엔티티 래핑 메서드 추가

---
+++ src/main/kotlin/com/dcd/server/persistence/volume/adapter/VolumeAdapter.kt
@@ -0,0 +1,44 @@
+package com.dcd.server.persistence.volume.adapter
+
+import com.dcd.server.core.domain.volume.model.Volume
+import com.dcd.server.core.domain.volume.model.VolumeMount
+import com.dcd.server.persistence.application.adapter.toDomain
+import com.dcd.server.persistence.application.adapter.toEntity
+import com.dcd.server.persistence.volume.entity.VolumeJpaEntity
+import com.dcd.server.persistence.volume.entity.VolumeMountJpaEntity
+import com.dcd.server.persistence.workspace.adapter.toDomain
+import com.dcd.server.persistence.workspace.adapter.toEntity
+
+fun Volume.toEntity(): VolumeJpaEntity =
+    VolumeJpaEntity(
+        id = this.id,
+        name = this.name,
+        description = this.description,
+        physicalPath = this.physicalPath,
+        workspace = this.workspace.toEntity()
+    )
+
+fun VolumeJpaEntity.toDomain(): Volume =
+    Volume(
+        id = this.id,
+        name = this.name,
+        description = this.description,
+        physicalPath = this.physicalPath,
+        workspace = this.workspace.toDomain()
+    )
+
+fun VolumeMount.toEntity(): VolumeMountJpaEntity =
+    VolumeMountJpaEntity(
+        id = this.id,
+        application = this.application.toEntity(),
+        volume = this.volume.toEntity(),
+        mountPath = this.mountPath
+    )
+
+fun VolumeMountJpaEntity.toDomain(): VolumeMount =
+    VolumeMount(
+        id = this.id,
+        application = this.application.toDomain(),
+        volume = this.volume.toDomain(),
+        mountPath = this.mountPath
+    )
