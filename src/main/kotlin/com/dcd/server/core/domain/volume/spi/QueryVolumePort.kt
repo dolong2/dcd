b689e27712e62456ab36a7f7e3f9780b73c43501
dolong2 <k01066624566@gmail.com>
2025-09-03 15:38:13 +0900

    [feat] 중복 볼륨 존재여부 확인 메서드 추가

--- src/main/kotlin/com/dcd/server/core/domain/volume/spi/QueryVolumePort.kt
+++ src/main/kotlin/com/dcd/server/core/domain/volume/spi/QueryVolumePort.kt
@@ -3,10 +3,13 @@ package com.dcd.server.core.domain.volume.spi
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.volume.model.Volume
 import com.dcd.server.core.domain.volume.model.VolumeMount
+import com.dcd.server.core.domain.workspace.model.Workspace
 import java.util.UUID
 
 interface QueryVolumePort {
     fun findById(id: UUID): Volume?
+
+    fun existsVolumeByNameAndWorkspace(name: String, workspace: Workspace): Boolean
 
     fun findAllMountByApplication(application: Application): List<VolumeMount>
 
