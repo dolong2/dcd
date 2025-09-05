ee11b1bdf72b58a86851d1b02fb6bd30702f0682
dolong2 <k01066624566@gmail.com>
2025-09-05 23:31:31 +0900

    [feat] 볼륨 수정 엔드포인트 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/volume/VolumeWebAdapter.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/volume/VolumeWebAdapter.kt
@@ -3,21 +3,25 @@ package com.dcd.server.presentation.domain.volume
 import com.dcd.server.core.common.annotation.WorkspaceOwnerVerification
 import com.dcd.server.core.domain.volume.usecase.CreateVolumeUseCase
 import com.dcd.server.core.domain.volume.usecase.DeleteVolumeUseCase
+import com.dcd.server.core.domain.volume.usecase.UpdateVolumeUseCase
 import com.dcd.server.presentation.common.annotation.WebAdapter
 import com.dcd.server.presentation.domain.volume.data.extension.toDto
 import com.dcd.server.presentation.domain.volume.data.request.CreateVolumeRequest
+import com.dcd.server.presentation.domain.volume.data.request.UpdateVolumeRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import java.util.UUID
 
 @WebAdapter("/{workspaceId}/volume")
 class VolumeWebAdapter(
     private val createVolumeUseCase: CreateVolumeUseCase,
-    private val deleteVolumeUseCase: DeleteVolumeUseCase
+    private val deleteVolumeUseCase: DeleteVolumeUseCase,
+    private val updateVolumeUseCase: UpdateVolumeUseCase
 ) {
     @PostMapping
     @WorkspaceOwnerVerification("#workspaceId")
@@ -35,5 +39,15 @@ class VolumeWebAdapter(
         @PathVariable volumeId: UUID,
     ): ResponseEntity<Void> =
         deleteVolumeUseCase.execute(volumeId)
+            .run { ResponseEntity.ok().build() }
+
+    @PutMapping("/{volumeId}")
+    @WorkspaceOwnerVerification("#workspaceId")
+    fun updateVolume(
+        @PathVariable workspaceId: String,
+        @PathVariable volumeId: UUID,
+        @Validated @RequestBody updateVolumeRequest: UpdateVolumeRequest
+    ): ResponseEntity<Void> =
+        updateVolumeUseCase.execute(volumeId, updateVolumeRequest.toDto())
             .run { ResponseEntity.ok().build() }
 }
