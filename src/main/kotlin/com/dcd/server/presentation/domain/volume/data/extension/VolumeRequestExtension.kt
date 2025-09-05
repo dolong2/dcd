6b3bf5999ef84681a641cbf1dd6cfb91c7f49037
dolong2 <k01066624566@gmail.com>
2025-09-05 23:30:47 +0900

    [feat] 볼륨 수정 요청 객체 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/volume/data/extension/VolumeRequestExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/volume/data/extension/VolumeRequestExtension.kt
@@ -1,10 +1,18 @@
 package com.dcd.server.presentation.domain.volume.data.extension
 
 import com.dcd.server.core.domain.volume.dto.request.CreateVolumeReqDto
+import com.dcd.server.core.domain.volume.dto.request.UpdateVolumeReqDto
 import com.dcd.server.presentation.domain.volume.data.request.CreateVolumeRequest
+import com.dcd.server.presentation.domain.volume.data.request.UpdateVolumeRequest
 
 fun CreateVolumeRequest.toDto(): CreateVolumeReqDto =
     CreateVolumeReqDto(
+        name = this.name,
+        description = this.description
+    )
+
+fun UpdateVolumeRequest.toDto(): UpdateVolumeReqDto =
+    UpdateVolumeReqDto(
         name = this.name,
         description = this.description
     )
