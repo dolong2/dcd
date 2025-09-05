0f7e35bfd06d8bd498736b2d30242d71d9d0718f
dolong2 <k01066624566@gmail.com>
2025-09-05 23:46:25 +0900

    [feat] 볼륨 수정 테스트 케이스 추가

---
+++ src/test/kotlin/com/dcd/server/core/domain/volume/usecase/UpdateVolumeUseCaseTest.kt
@@ -0,0 +1,108 @@
+package com.dcd.server.core.domain.volume.usecase
+
+import com.dcd.server.core.common.command.CommandPort
+import com.dcd.server.core.common.data.WorkspaceInfo
+import com.dcd.server.core.domain.application.spi.QueryApplicationPort
+import com.dcd.server.core.domain.volume.dto.request.UpdateVolumeReqDto
+import com.dcd.server.core.domain.volume.exception.AlreadyExistsVolumeMountException
+import com.dcd.server.core.domain.volume.exception.VolumeNotFoundException
+import com.dcd.server.core.domain.volume.model.Volume
+import com.dcd.server.core.domain.volume.model.VolumeMount
+import com.dcd.server.core.domain.workspace.spi.QueryWorkspacePort
+import com.dcd.server.persistence.volume.adapter.toDomain
+import com.dcd.server.persistence.volume.adapter.toEntity
+import com.dcd.server.persistence.volume.repository.VolumeMountRepository
+import com.dcd.server.persistence.volume.repository.VolumeRepository
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+class UpdateVolumeUseCaseTest(
+    private val updateVolumeUseCase: UpdateVolumeUseCase,
+    @MockkBean(relaxed = true)
+    private val commandPort: CommandPort,
+    private val workspaceInfo: WorkspaceInfo,
+    private val volumeRepository: VolumeRepository,
+    private val volumeMountRepository: VolumeMountRepository,
+    private val queryWorkspacePort: QueryWorkspacePort,
+    private val queryApplicationPort: QueryApplicationPort,
+) : BehaviorSpec({
+    val targetVolumeId = UUID.randomUUID()
+
+    beforeSpec {
+        val targetWorkspace = queryWorkspacePort.findById("d57b42f5-5cc4-440b-8dce-b4fc2e372eff")!!
+        workspaceInfo.workspace = targetWorkspace
+
+        val volume = Volume(
+            id = targetVolumeId,
+            name = "testVolume",
+            description = "testDescription",
+            workspace = targetWorkspace
+        )
+        volumeRepository.save(volume.toEntity())
+    }
+
+    given("타겟 볼륨 아이디와 수정할 볼륨 요청 dto가 주어지고") {
+        val request = UpdateVolumeReqDto(name = "updateVolume", description = "updateDescription")
+
+        `when`("유스케이스를 실행할때") {
+            updateVolumeUseCase.execute(targetVolumeId, request)
+
+            then("볼륨 정보가 수정되어야함") {
+                val targetVolume = volumeRepository.findByIdOrNull(targetVolumeId)
+
+                targetVolume shouldNotBe null
+                targetVolume!!.name shouldBe request.name
+                targetVolume.description shouldBe request.description
+            }
+        }
+    }
+
+    given("볼륨이 존재하지 않고") {
+        volumeRepository.deleteAll()
+        val request = UpdateVolumeReqDto(name = "updateVolume", description = "updateDescription")
+
+        `when`("유스케이스를 실행할때") {
+
+            then("예외가 발생해야함") {
+                shouldThrow<VolumeNotFoundException> {
+                    updateVolumeUseCase.execute(targetVolumeId, request)
+                }
+            }
+        }
+    }
+
+    given("볼륨 마운트가 존재하고") {
+        val application = queryApplicationPort.findById("2fb0f315-8272-422f-8e9f-c4f765c022b2")!!
+        val volume = volumeRepository.findByIdOrNull(targetVolumeId)!!.toDomain()
+        val volumeMount = VolumeMount(
+            id = UUID.randomUUID(),
+            application = application,
+            volume = volume,
+            mountPath = "/test/volume",
+            readOnly = false
+        )
+        volumeMountRepository.save(volumeMount.toEntity())
+
+        val request = UpdateVolumeReqDto(name = "updateVolume", description = "updateDescription")
+
+        `when`("유스케이스를 실행하면") {
+
+            then("에러가 발생해야함") {
+                shouldThrow<AlreadyExistsVolumeMountException> {
+                    updateVolumeUseCase.execute(targetVolumeId, request)
+                }
+            }
+        }
+    }
+})
--- src/test/kotlin/com/dcd/server/presentation/domain/volume/VolumeWebAdapterTest.kt
+++ src/test/kotlin/com/dcd/server/presentation/domain/volume/VolumeWebAdapterTest.kt
@@ -1,9 +1,12 @@
 package com.dcd.server.presentation.domain.volume
 
 import com.dcd.server.core.domain.volume.dto.request.CreateVolumeReqDto
+import com.dcd.server.core.domain.volume.dto.request.UpdateVolumeReqDto
 import com.dcd.server.core.domain.volume.usecase.CreateVolumeUseCase
 import com.dcd.server.core.domain.volume.usecase.DeleteVolumeUseCase
+import com.dcd.server.core.domain.volume.usecase.UpdateVolumeUseCase
 import com.dcd.server.presentation.domain.volume.data.request.CreateVolumeRequest
+import com.dcd.server.presentation.domain.volume.data.request.UpdateVolumeRequest
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.mockk
@@ -14,8 +17,9 @@ import java.util.UUID
 class VolumeWebAdapterTest : BehaviorSpec({
     val createVolumeUseCase = mockk<CreateVolumeUseCase>(relaxUnitFun = true)
     val deleteVolumeUseCase = mockk<DeleteVolumeUseCase>(relaxUnitFun = true)
+    val updateVolumeUseCase = mockk<UpdateVolumeUseCase>(relaxUnitFun = true)
 
-    val volumeWebAdapter = VolumeWebAdapter(createVolumeUseCase, deleteVolumeUseCase)
+    val volumeWebAdapter = VolumeWebAdapter(createVolumeUseCase, deleteVolumeUseCase, updateVolumeUseCase)
 
     given("워크스페이스 아이디와 볼륨 생성 요청이 주어지고") {
         val testWorkspaceId = UUID.randomUUID().toString()
@@ -47,6 +51,24 @@ class VolumeWebAdapterTest : BehaviorSpec({
 
             then("볼륨 삭제 유스케이스를 실행해야함") {
                 verify { deleteVolumeUseCase.execute(testVolumeId) }
+            }
+        }
+    }
+
+    given("워크스페이스 아이디, 볼륨 아이디, 볼륨 수정 요청이 주어지고") {
+        val testWorkspaceId = UUID.randomUUID().toString()
+        val testVolumeId = UUID.randomUUID()
+        val request = UpdateVolumeRequest(name = "testVolume", description = "testDescription")
+
+        `when`("볼륨 수정 메서드를 실행하면") {
+            val result = volumeWebAdapter.updateVolume(testWorkspaceId, testVolumeId, request)
+
+            then("상태코드 OK가 응답되어야함") {
+                result.statusCode shouldBe HttpStatus.OK
+            }
+
+            then("볼륨 수정 유스케이스를 실행해야함") {
+                verify { updateVolumeUseCase.execute(testVolumeId, any() as UpdateVolumeReqDto) }
             }
         }
     }
