cf34758c291e4cdb5fe61987dd283cc91e1a1451
dolong2 <k01066624566@gmail.com>
2025-09-06 23:57:04 +0900

    [feat] 볼륨 삭제 유스케이스에 볼륨의 워크스페이스 소속 검증 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/volume/usecase/DeleteVolumeUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/volume/usecase/DeleteVolumeUseCase.kt
@@ -1,22 +1,31 @@
 package com.dcd.server.core.domain.volume.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
+import com.dcd.server.core.common.data.WorkspaceInfo
 import com.dcd.server.core.domain.volume.exception.AlreadyExistsVolumeMountException
 import com.dcd.server.core.domain.volume.exception.VolumeNotFoundException
 import com.dcd.server.core.domain.volume.service.DeleteVolumeService
 import com.dcd.server.core.domain.volume.spi.CommandVolumePort
 import com.dcd.server.core.domain.volume.spi.QueryVolumePort
+import com.dcd.server.core.domain.workspace.exception.WorkspaceNotFoundException
 import java.util.UUID
 
 @UseCase
 class DeleteVolumeUseCase(
     private val queryVolumePort: QueryVolumePort,
     private val commandVolumePort: CommandVolumePort,
-    private val deleteVolumeService: DeleteVolumeService
+    private val deleteVolumeService: DeleteVolumeService,
+    private val workspaceInfo: WorkspaceInfo
 ) {
     fun execute(volumeId: UUID) {
+        val workspace = (workspaceInfo.workspace
+            ?: throw WorkspaceNotFoundException())
+
         val volume = (queryVolumePort.findById(volumeId)
             ?: throw VolumeNotFoundException())
+
+        if (workspace != volume.workspace)
+            throw VolumeNotFoundException()
 
         val volumeMountList = queryVolumePort.findAllMountByVolume(volume)
         if (volumeMountList.isNotEmpty())
