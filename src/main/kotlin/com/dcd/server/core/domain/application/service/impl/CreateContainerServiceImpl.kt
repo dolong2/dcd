44de2433589e1f0aa4d25e43e4bcb4012eb84c54
dolong2 <k01066624566@gmail.com>
2025-09-12 23:21:26 +0900

    [feat] 애플리케이션 컨테이너 생성시 마운트된 볼륨을 반영해서 생성하는 로직 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/CreateContainerServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/CreateContainerServiceImpl.kt
@@ -5,6 +5,7 @@ import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.service.CreateContainerService
 import com.dcd.server.core.domain.application.spi.CheckExitValuePort
 import com.dcd.server.core.domain.application.util.FailureCase
+import com.dcd.server.core.domain.volume.spi.QueryVolumePort
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Service
@@ -12,13 +13,26 @@ import org.springframework.stereotype.Service
 @Service
 class CreateContainerServiceImpl(
     private val commandPort: CommandPort,
+    private val queryVolumePort: QueryVolumePort,
     private val checkExitValuePort: CheckExitValuePort
 ) : CreateContainerService {
     override suspend fun createContainer(application: Application, externalPort: Int) {
+        val volumeMountBuilder = StringBuilder()
+        queryVolumePort.findAllMountByApplication(application)
+            .forEach {
+                val volume = it.volume
+                volumeMountBuilder.append("-v ${volume.name}:${it.mountPath}")
+                if (it.readOnly)
+                    volumeMountBuilder.append(":ro")
+                volumeMountBuilder.append(" ")
+            }
+        val volumeMountFlags = volumeMountBuilder.toString()
+
         withContext(Dispatchers.IO) {
             val cmd =
                 "docker create --network ${application.workspace.networkName} " +
                         "--name ${application.containerName} " +
+                        volumeMountFlags +
                         "-p ${externalPort}:${application.port} ${application.containerName}:latest"
 
             commandPort.executeShellCommand(cmd)
