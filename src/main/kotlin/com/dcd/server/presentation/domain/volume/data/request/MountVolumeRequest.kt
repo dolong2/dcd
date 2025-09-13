3b12c5ded6d464f2006ccda68088391148a51bbb
dolong2 <k01066624566@gmail.com>
2025-09-13 23:32:20 +0900

    [feat] 마운트 볼륨 요청시 마운트 경로에 정규식 적용

--- src/main/kotlin/com/dcd/server/presentation/domain/volume/data/request/MountVolumeRequest.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/volume/data/request/MountVolumeRequest.kt
@@ -1,9 +1,11 @@
 package com.dcd.server.presentation.domain.volume.data.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 data class MountVolumeRequest(
     @field:NotBlank
+    @field:Pattern(regexp = "^(?!(?:/(?:proc|sys|dev|etc)?$))/(?:(?:[A-Za-z0-9._-]+/)*[A-Za-z0-9._-]+)?$")
     val mountPath: String,
     val readOnly: Boolean,
 )
