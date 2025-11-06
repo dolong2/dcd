c3775fe7b2993b43da70e9a62fcf8ec185a51bb6
dolong2 <k01066624566@gmail.com>
2025-11-06 17:27:42 +0900

    [feat] 애플리케이션에 초기화 스크립트 작성 서비스 추가

---
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/InitialScriptServiceImpl.kt
@@ -0,0 +1,27 @@
+package com.dcd.server.core.domain.application.service.impl
+
+import com.dcd.server.core.domain.application.model.Application
+import com.dcd.server.core.domain.application.model.ApplicationInitialScript
+import com.dcd.server.core.domain.application.service.InitialScriptService
+import com.dcd.server.core.domain.application.spi.CommandApplicationInitialScriptPort
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class InitialScriptServiceImpl(
+    private val commandApplicationInitialScriptPort: CommandApplicationInitialScriptPort
+) : InitialScriptService {
+    override fun write(
+        application: Application,
+        initialScripts: List<String>,
+    ) {
+        val initialScriptList = initialScripts.map { script ->
+            ApplicationInitialScript(
+                id = UUID.randomUUID(),
+                script = script,
+                application = application,
+            )
+        }
+        commandApplicationInitialScriptPort.saveAll(initialScriptList)
+    }
+}
