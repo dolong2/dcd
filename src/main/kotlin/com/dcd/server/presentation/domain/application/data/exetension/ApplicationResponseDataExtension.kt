c56010166dbb13d81b63b4b98574157acced273f
조재영 <80161826+dolong2@users.noreply.github.com>
2026-03-25 00:25:43 +0000

    [feat] 애플리케이션 응답 객체에 failureReasonDetail 필드 추가

--- src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/ApplicationResponseDataExtension.kt
+++ src/main/kotlin/com/dcd/server/presentation/domain/application/data/exetension/ApplicationResponseDataExtension.kt
@@ -18,6 +18,7 @@ fun ApplicationResDto.toResponse(): ApplicationResponse =
         version = this.version,
         status = this.status,
         failureReason = this.failureReason,
+        failureReasonDetail = this.failureReasonDetail,
         initialScripts = this.initialScripts,
         labels = this.labels
     )
