697ed397bf27e7fb986105d2bdf1d12468e515c3
조재영 <80161826+dolong2@users.noreply.github.com>
2026-03-25 08:01:11 +0000

    [refac] 애플리케이션 단일 조회 유스케이스에서 ApplicationResDto를 사용하도록 리펙토링

--- src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetOneApplicationUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/usecase/GetOneApplicationUseCase.kt
@@ -1,8 +1,8 @@
 package com.dcd.server.core.domain.application.usecase
 
 import com.dcd.server.core.common.annotation.UseCase
-import com.dcd.server.core.domain.application.dto.extenstion.toDto
-import com.dcd.server.core.domain.application.dto.response.ApplicationDetailResDto
+import com.dcd.server.core.domain.application.dto.extenstion.toResDto
+import com.dcd.server.core.domain.application.dto.response.ApplicationResDto
 import com.dcd.server.core.domain.application.exception.ApplicationNotFoundException
 import com.dcd.server.core.domain.application.spi.QueryApplicationInitialScriptPort
 import com.dcd.server.core.domain.application.spi.QueryApplicationPort
@@ -14,11 +14,9 @@ class GetOneApplicationUseCase(
     private val queryApplicationEnvPort: QueryApplicationEnvPort,
     private val queryApplicationInitialScriptPort: QueryApplicationInitialScriptPort
 ) {
-    fun execute(id: String): ApplicationDetailResDto {
+    fun execute(id: String): ApplicationResDto {
         val application = (queryApplicationPort.findById(id)
             ?: throw ApplicationNotFoundException())
-        val applicationEnvList = queryApplicationEnvPort.findByApplication(application)
-        val initialScripts = queryApplicationInitialScriptPort.findAllByApplication(application)
-        return application.toDto(applicationEnvList, initialScripts)
+        return application.toResDto()
     }
 }
