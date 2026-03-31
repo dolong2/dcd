99c0f1a1dad55f947b731223727951ed2ef072f2
dolong2 <k01066624566@gmail.com>
2026-04-01 01:14:22 +0900

    [refac] 테스트와 맞지 않는 부분 수정

--- src/main/kotlin/com/dcd/server/infrastructure/global/adapter/CuckooFilterServiceAdapter.kt
+++ src/main/kotlin/com/dcd/server/infrastructure/global/adapter/CuckooFilterServiceAdapter.kt
@@ -3,8 +3,6 @@ package com.dcd.server.infrastructure.global.adapter
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.redisson.api.RedissonClient
-import org.redisson.client.RedisException
-import org.redisson.client.codec.StringCodec
 import com.dcd.server.core.common.spi.CountBloomFilterPort
 import com.dcd.server.core.common.service.exception.BloomFilterReservationException
 
@@ -14,18 +12,18 @@ class CuckooFilterServiceAdapter(
 ) : CountBloomFilterPort {
     private val log = LoggerFactory.getLogger(this::class.simpleName)
 
-    private val DEFAULT_CAPACITY = 10000L
+    private val DEFAULT_CAPACITY = 100000L
 
     override fun add(filterName: String, item: String): Boolean {
         return try {
             val cuckooFilter = redissonClient.getCuckooFilter<String>(filterName)
-            
+
             // CuckooFilter가 없으면 생성
-            if (!cuckooFilter.exists()) {
+            if (!cuckooFilter.isExists) {
                 cuckooFilter.init(DEFAULT_CAPACITY)
             }
-            
-            cuckooFilter.add(item)
+
+            cuckooFilter.addIfAbsent(item)
         } catch (e: Exception) {
             if (e is BloomFilterReservationException) throw e
             log.error("Error adding to Cuckoo Filter: {}", filterName, e)
@@ -39,7 +37,7 @@ class CuckooFilterServiceAdapter(
             cuckooFilter.remove(item)
         } catch (e: Exception) {
             log.error("Error removing from Cuckoo Filter: {}", filterName, e)
-            throw BloomFilterReservationException()
+            false
         }
     }
 
