9891154d2316446816d232567e1b342560dd6dad
dolong2 <k01066624566@gmail.com>
2026-04-01 10:42:33 +0900

    [refac] 회원가입시 bloomFilter를 검증하도록 리펙토링

--- src/main/kotlin/com/dcd/server/core/domain/auth/usecase/SignUpUseCase.kt
+++ src/main/kotlin/com/dcd/server/core/domain/auth/usecase/SignUpUseCase.kt
@@ -3,10 +3,12 @@ package com.dcd.server.core.domain.auth.usecase
 import com.dcd.server.core.common.annotation.CheckEmailCertificate
 import com.dcd.server.core.common.annotation.UseCase
 import com.dcd.server.core.common.service.SecurityService
+import com.dcd.server.core.common.spi.CountBloomFilterPort
 import com.dcd.server.core.domain.auth.dto.extension.toEntity
 import com.dcd.server.core.domain.auth.dto.request.SignUpReqDto
 import com.dcd.server.core.domain.auth.exception.AlreadyExistsUserException
 import com.dcd.server.core.domain.auth.model.enums.EmailAuthUsage
+import com.dcd.server.core.domain.user.model.User
 import com.dcd.server.core.domain.user.spi.CommandUserPort
 import com.dcd.server.core.domain.user.spi.QueryUserPort
 
@@ -14,11 +16,16 @@ import com.dcd.server.core.domain.user.spi.QueryUserPort
 class SignUpUseCase(
     private val securityService: SecurityService,
     private val commandUserPort: CommandUserPort,
-    private val queryUserPort: QueryUserPort
+    private val queryUserPort: QueryUserPort,
+    private val bloomFilterPort: CountBloomFilterPort
 ) {
     @CheckEmailCertificate("#signUpReqDto.email", EmailAuthUsage.SIGNUP)
     fun execute(signUpReqDto: SignUpReqDto) {
-        if(queryUserPort.existsByEmail(signUpReqDto.email))
+        val email = signUpReqDto.email
+
+        //유저 존재 여부 검사시 bloomFilter의 존재여부 판단을 우선적으로 판단함
+        val bloomFilterQueryResult = bloomFilterPort.exists(User.USER_INFO_BLOOM_FILTER, email)
+        if(bloomFilterQueryResult && queryUserPort.existsByEmail(email))
             throw AlreadyExistsUserException()
         val encodePassword = securityService.encodePassword(signUpReqDto.password)
         val user = signUpReqDto.toEntity(encodePassword)
