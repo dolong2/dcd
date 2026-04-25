4de71fc2f21c1b2922bb7ac51a0900f3eba2644e
조재영 <80161826+dolong2@users.noreply.github.com>
2026-04-25 06:25:34 +0000

    [feat] ContainerPort 추가

---
+++ src/main/kotlin/com/dcd/server/core/common/spi/ContainerActions.kt
@@ -0,0 +1,16 @@
+package com.dcd.server.core.common.spi
+
+import com.dcd.server.core.domain.application.scheduler.enums.ContainerStatus
+import com.dcd.server.core.domain.application.model.Application
+
+interface ContainerActions {
+    fun createContainer(application: Application): String
+    fun startContainer(application: Application): Boolean
+    fun stopContainer(application: Application): Boolean
+    fun deleteContainer(application: Application): Boolean
+    fun deleteImage(application: Application): Boolean
+    fun getContainerStatus(application: Application): String
+    fun getContainer(status: ContainerStatus): List<String>
+    fun getContainerLogs(application: Application): List<String>
+    fun buildImage(application: Application, dockerfilePath: String): String
+}
