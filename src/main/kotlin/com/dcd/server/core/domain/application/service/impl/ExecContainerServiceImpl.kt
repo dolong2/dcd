3743764760b5c010afcc0cdace4e382d9526fe9d
조재영 <80161826+dolong2@users.noreply.github.com>
2026-05-20 05:11:59 +0000

    [refac] 웹소켓으로 애플리케이션에 명령실행 로직 수정

--- src/main/kotlin/com/dcd/server/core/domain/application/service/impl/ExecContainerServiceImpl.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/service/impl/ExecContainerServiceImpl.kt
@@ -2,19 +2,22 @@ package com.dcd.server.core.domain.application.service.impl
 
 import com.dcd.server.core.common.command.CommandPort
 import com.dcd.server.core.common.spi.ContainerPort
+import com.dcd.server.core.domain.application.exception.ContainerNotConnectedException
 import com.dcd.server.core.domain.application.model.Application
 import com.dcd.server.core.domain.application.service.ExecContainerService
 import org.springframework.stereotype.Service
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import java.io.*
-import java.util.Stack
+import java.util.concurrent.ConcurrentHashMap
 
 @Service
 class ExecContainerServiceImpl(
     private val containerPort: ContainerPort,
     private val commandPort: CommandPort
 ) : ExecContainerService {
+    private val containerInputStreams = ConcurrentHashMap<String, OutputStream>()
+
     override fun execCmd(application: Application, cmd: String): List<String> {
         val result = mutableListOf<String>()
         containerPort.execute {
@@ -26,15 +29,20 @@ class ExecContainerServiceImpl(
     }
 
     override fun execCmd(application: Application, session: WebSocketSession, cmd: String) {
+        containerInputStreams[session.id]?.let {
+            it.write("${cmd}\n".toByteArray())
+            it.flush()
+        } ?: throw ContainerNotConnectedException()
+    }
+
     override fun initContainerTty(application: Application, session: WebSocketSession) =
         containerPort.execute {
             attachContainer(application) { response ->
                 if (session.isOpen) {
-                    println("ws test1: ${response}")
                     session.sendMessage(TextMessage(response))
-    }
+                }
             }
         }?.let {
             containerInputStreams[session.id] = it
-        } ?: throw RuntimeException("컨테이너에 연결되지 않았습니다.")
+        } ?: throw ContainerNotConnectedException()
 }
