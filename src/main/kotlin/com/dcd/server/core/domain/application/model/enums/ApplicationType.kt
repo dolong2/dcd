7ea33ae7435a6f22200056accf409019d730d9d8
조재영 <80161826+dolong2@users.noreply.github.com>
2026-06-02 00:09:51 +0000

    [feat] ApplicationType에 GIN 추가

--- src/main/kotlin/com/dcd/server/core/domain/application/model/enums/ApplicationType.kt
+++ src/main/kotlin/com/dcd/server/core/domain/application/model/enums/ApplicationType.kt
@@ -3,8 +3,11 @@ package com.dcd.server.core.domain.application.model.enums
 enum class ApplicationType {
     SPRING_BOOT,
     NEST_JS,
+    GIN,
+
     MYSQL,
     MARIA_DB,
+    H2_DB,
+
     REDIS,
-    H2_DB
 }
